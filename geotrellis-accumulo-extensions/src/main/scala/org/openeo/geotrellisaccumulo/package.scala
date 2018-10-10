5d7d94aeec7894134ab155a1beabc6f4d7944cf1
Jeroen Dries <jeroen.dries@vito.be>
2018-10-10 21:34:07 +0200

    add utility method to convert an rdd map to a scala Geotrellis Pyramid

--- geotrellis-accumulo-extensions/src/main/scala/org/openeo/geotrellisaccumulo/package.scala
+++ geotrellis-accumulo-extensions/src/main/scala/org/openeo/geotrellisaccumulo/package.scala
@@ -1,5 +1,16 @@
 package org.openeo
 
+import geotrellis.raster.MultibandTile
+import geotrellis.spark.pyramid.Pyramid
+import geotrellis.spark.{Metadata, SpaceTimeKey, TileLayerMetadata}
+import org.apache.spark.rdd.RDD
+
+import scala.collection.JavaConverters.mapAsScalaMapConverter
+
 package object geotrellisaccumulo {
 
+    def createGeotrellisPyramid(levels: java.util.Map[Integer,RDD[(SpaceTimeKey,MultibandTile)]  with Metadata[TileLayerMetadata[SpaceTimeKey]]]): Pyramid[SpaceTimeKey,MultibandTile,TileLayerMetadata[SpaceTimeKey]] ={
+      val map: Map[Int, RDD[(SpaceTimeKey, MultibandTile)] with Metadata[TileLayerMetadata[SpaceTimeKey]]] = levels.asScala.toMap.map(t=>(t._1.toInt,t._2))
+      return new Pyramid(map)
+    }
 }
