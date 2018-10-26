a69edddb18bb965405f32280098773478c5123ed
Jeroen Dries <jeroen.dries@vito.be>
2018-10-26 15:34:15 +0200

    add some local tests, used for debugging

---
+++ geotrellis-accumulo-extensions/src/test/java/org/openeo/geotrellisaccumulo/GeotrellisAccumuloRDDTest.scala
@@ -0,0 +1,52 @@
+package org.openeo.geotrellisaccumulo
+
+import geotrellis.raster.MultibandTile
+import geotrellis.spark.SpaceTimeKey
+import geotrellis.vector.Extent
+import org.apache.spark.{SparkConf, SparkContext}
+import org.scalatest.{FlatSpec, Matchers}
+
+class GeotrellisAccumuloRDDTest  extends FlatSpec with Matchers{
+
+  def isSorted[T](s: Seq[T])(implicit ord: Ordering[T]): Boolean = s match {
+    case Seq() => true
+    case Seq(_) => true
+    case _ => s.sliding(2).forall { case List(x, y) => ord.lteq(x, y) }
+  }
+
+
+  it should "load data from accumulo" in {
+    val conf = new SparkConf
+    conf.setAppName("IngestS2")
+    conf.setMaster("local[4]")
+    conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    conf.set("spark.hadoop.fs.permissions.umask-mode", "000")
+    val sc = new SparkContext(conf)
+
+    val layername = "CGS_SENTINEL2_RADIOMETRY_V102"
+    val bbox = new Extent(3, 51, 3.5, 52)
+
+    val accumuloTileLayerRepository = new PyramidFactory("hdp-accumulo-instance", "epod6.vgt.vito.be:2181,epod17.vgt.vito.be:2181,epod1.vgt.vito.be:2181")
+    accumuloTileLayerRepository.pyramid(layername,bbox,"EPSG:4326")
+    val rdd = accumuloTileLayerRepository.rdd[MultibandTile](layername)
+
+    println(rdd)
+    val partitions = rdd.partitions
+
+    //rdd.foreach(v => println(v))
+    //val startkeys = partitions.flatMap(p => p.asInstanceOf[NewHadoopPartition].serializableHadoopSplit.t.asInstanceOf[BatchInputSplit].getRanges().asScala).map(r => r.getStartKey)
+    //isSorted(startkeys) shouldEqual true
+    /*partitions.foreach(p => p.asInstanceOf[NewHadoopPartition].serializableHadoopSplit.t.asInstanceOf[BatchInputSplit].getRanges().stream().forEach(new Consumer[data.Range] {
+      override def accept(t: data.Range): Unit = {
+        if(t!=null) {
+          print(t.getStartKey.getRow())
+          println(" " + t.getStartKey.getTimestamp)
+        }
+      }
+    }))*/
+    //println(partitions)
+    val tile = rdd.lookup(new SpaceTimeKey(0,0,1518134400000L))
+    println(tile)
+
+  }
+}
---
+++ geotrellis-accumulo-extensions/src/test/java/org/openeo/geotrellisaccumulo/PyramidFactoryTest.java
@@ -0,0 +1,49 @@
+package org.openeo.geotrellisaccumulo;
+
+import geotrellis.raster.MultibandTile;
+import geotrellis.spark.SpaceTimeKey;
+import geotrellis.vector.Extent;
+import org.apache.hadoop.hdfs.HdfsConfiguration;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.apache.spark.rdd.RDD;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import scala.Tuple2;
+import scala.collection.immutable.Seq;
+
+import static org.junit.Assert.assertFalse;
+
+public class PyramidFactoryTest {
+
+    @BeforeClass
+    public static void sparkContext() {
+        HdfsConfiguration config = new HdfsConfiguration();
+        config.set("hadoop.security.authentication", "kerberos");
+        UserGroupInformation.setConfiguration(config);
+        SparkConf conf = new SparkConf();
+        conf.setAppName("PyramidFactoryTest");
+        conf.setMaster("local[4]");
+        conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+
+        SparkContext sc =SparkContext.getOrCreate(conf);
+        //creating context may have screwed up security settings
+        UserGroupInformation.setConfiguration(config);
+    }
+    @Test
+    public void createPyramid() {
+
+
+        PyramidFactory pyramidFactory = new PyramidFactory("hdp-accumulo-instance", "epod6.vgt.vito.be:2181,epod17.vgt.vito.be:2181,epod1.vgt.vito.be:2181");
+        Extent bbox = new Extent(652000, 5161000, 672000, 5181000);
+        String srs = "EPSG:32632";
+
+        //bbox = new Extent(10.5, 46.5, 11.4, 46.9);
+        //srs = "EPSG:4326";
+        Seq<Tuple2<Object, RDD<Tuple2<SpaceTimeKey, MultibandTile>>>> pyramid = pyramidFactory.pyramid_seq("PROBAV_L3_S10_TOC_NDVI_333M", bbox, srs, "2016-12-31T00:00:00Z", "2018-01-01T02:00:00Z");
+        System.out.println("pyramid = " + pyramid);
+        assertFalse(pyramid.apply(0)._2.isEmpty());
+
+    }
+}
