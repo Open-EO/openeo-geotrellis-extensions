4953800de63f928198b27da0c0f9f7b49934ec37
Jeroen Dries <jeroen.dries@vito.be>
2019-01-21 16:02:52 +0100

    add function to map the dates in a spacetime datacube to new values

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/OpenEOProcesses.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/OpenEOProcesses.scala
@@ -1,9 +1,14 @@
 package org.openeo.geotrellis
 
+import java.time.{Instant, ZonedDateTime}
+
 import geotrellis.raster.mapalgebra.local._
 import geotrellis.raster.{MultibandTile, Tile}
-import geotrellis.spark.{ContextRDD, Metadata, MultibandTileLayerRDD, TileLayerMetadata}
+import geotrellis.spark.{ContextRDD, Metadata, MultibandTileLayerRDD, SpaceTimeKey, TemporalKey, TileLayerMetadata}
 import org.apache.spark.rdd.RDD
+
+import scala.collection.JavaConverters
+import scala.collection.JavaConverters._
 
 
 class OpenEOProcesses extends Serializable {
@@ -33,6 +38,26 @@ class OpenEOProcesses extends Serializable {
 
   def applyProcess[K](datacube:MultibandTileLayerRDD[K], process:String): RDD[(K, MultibandTile)] with Metadata[TileLayerMetadata[K]]= {
     return ContextRDD(datacube.map(multibandtile => (multibandtile._1,multibandtile._2.mapBands((b,t) => unaryProcesses.get(process).get(t) ))),datacube.metadata)
+  }
+
+  def mapInstantToInterval(datacube:MultibandTileLayerRDD[SpaceTimeKey], intervals:java.lang.Iterable[String],labels:java.lang.Iterable[String]) :MultibandTileLayerRDD[SpaceTimeKey] = {
+    val timePeriods: Seq[Iterable[Instant]] = JavaConverters.iterableAsScalaIterableConverter(intervals).asScala.map(s => Instant.parse(s)).grouped(2).toList
+    val periodsToLabels: Seq[(Iterable[Instant], String)] = timePeriods.zip(labels.asScala)
+    val tilesByInterval: RDD[(SpaceTimeKey, MultibandTile)] = datacube.flatMap(tuple => {
+      val instant = tuple._1.time.toInstant
+      val spatialKey = tuple._1.spatialKey
+      val labelsForKey = periodsToLabels.filter(p => {
+        val interval = p._1
+        val iterator = interval.toIterator
+        val leftBound = iterator.next()
+        val rightBound = iterator.next()
+        (leftBound.isBefore(instant) && rightBound.isAfter(instant)) || leftBound.equals(instant)
+      }).map(t => t._2).map(ZonedDateTime.parse(_))
+
+      labelsForKey.map(l => (SpaceTimeKey(spatialKey,TemporalKey(l)),tuple._2))
+    })
+    return ContextRDD(tilesByInterval, datacube.metadata)
+
   }
 
 
--- openeo-geotrellis/src/test/java/org/openeo/geotrellis/TestOpenEOProcesses.java
+++ openeo-geotrellis/src/test/java/org/openeo/geotrellis/TestOpenEOProcesses.java
@@ -1,15 +1,19 @@
 package org.openeo.geotrellis;
 
 import geotrellis.raster.*;
-import geotrellis.spark.SpatialKey;
+import geotrellis.spark.*;
 import geotrellis.spark.testkit.TileLayerRDDBuilders$;
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaPairRDD$;
 import org.apache.spark.rdd.RDD;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import scala.Tuple2;
 
+import java.time.ZonedDateTime;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -42,4 +46,25 @@ public class TestOpenEOProcesses {
 
         assertEquals(1.0,doubles[0],0.0);
     }
-}
+
+    @Test
+    public void testMapToIntervals() {
+        Tile zeroTile = new ByteConstantTile((byte)0,256,256, (ByteCells) CellType$.MODULE$.fromName("int8raw"));
+        ContextRDD<SpatialKey, MultibandTile, TileLayerMetadata<SpaceTimeKey>> datacube = (ContextRDD<SpatialKey, MultibandTile, TileLayerMetadata<SpaceTimeKey>>) TileLayerRDDBuilders$.MODULE$.createMultibandTileLayerRDD(SparkContext.getOrCreate(), new ArrayMultibandTile(new Tile[]{zeroTile}), new TileLayout(1, 1, 256, 256));
+        JavaPairRDD<SpaceTimeKey, MultibandTile> spacetimeDataCube = JavaPairRDD$.MODULE$.fromJavaRDD(datacube.toJavaRDD()).flatMapToPair(spatialKeyMultibandTileTuple2 -> Arrays.asList(
+                Tuple2.apply(SpaceTimeKey.apply(spatialKeyMultibandTileTuple2._1, TemporalKey.apply(ZonedDateTime.parse("2017-01-01T00:00:00Z"))), spatialKeyMultibandTileTuple2._2),
+                Tuple2.apply(SpaceTimeKey.apply(spatialKeyMultibandTileTuple2._1, TemporalKey.apply(ZonedDateTime.parse("2017-01-15T00:00:00Z"))), spatialKeyMultibandTileTuple2._2),
+                Tuple2.apply(SpaceTimeKey.apply(spatialKeyMultibandTileTuple2._1, TemporalKey.apply(ZonedDateTime.parse("2017-02-01T00:00:00Z"))), spatialKeyMultibandTileTuple2._2),
+                Tuple2.apply(SpaceTimeKey.apply(spatialKeyMultibandTileTuple2._1, TemporalKey.apply(ZonedDateTime.parse("2018-01-15T00:00:00Z"))), spatialKeyMultibandTileTuple2._2)
+        ).iterator());
+        //JavaConverters.
+        RDD<Tuple2<SpaceTimeKey, MultibandTile>> mappedRDD = new OpenEOProcesses().mapInstantToInterval(new ContextRDD<SpaceTimeKey, MultibandTile, TileLayerMetadata<SpaceTimeKey>>(spacetimeDataCube.rdd(), datacube.metadata()), Arrays.asList("2017-01-01T00:00:00Z", "2017-01-30T00:00:00Z","2017-01-30T00:00:00Z", "2017-03-30T00:00:00Z"), Arrays.asList("2017-01-01T00:00:00Z","2017-01-15T00:00:00Z"));
+        List<Tuple2<SpaceTimeKey, MultibandTile>> map = JavaPairRDD$.MODULE$.fromJavaRDD(mappedRDD.toJavaRDD()).collect();
+        map.forEach(spaceTimeKey -> System.out.println("spaceTimeKey = " + spaceTimeKey._1.time()));
+        System.out.println("map = " + map);
+        assertEquals(2, map.stream().filter(tuple -> tuple._1().time().equals(ZonedDateTime.parse("2017-01-01T00:00:00Z"))).count());
+        assertEquals(1, map.stream().filter(tuple -> tuple._1().time().equals(ZonedDateTime.parse("2017-01-15T00:00:00Z"))).count());
+    }
+
+
+    }
