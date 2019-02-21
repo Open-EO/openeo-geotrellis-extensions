32bfe037920cb7ef9da6edfd6fb64b708c054ab9
Niels Hamers <nielshamers@hotmail.com>
2019-02-21 08:49:15 +0100

    interpolate colors for missing values in colormap file

--- geotrellis-vlm/src/main/scala/org/openeo/geotrellisvlm/ColorMapParser.scala
+++ geotrellis-vlm/src/main/scala/org/openeo/geotrellisvlm/ColorMapParser.scala
@@ -1,6 +1,8 @@
 package org.openeo.geotrellisvlm
 
-import geotrellis.raster.render.{ColorMap, RGBA}
+import java.lang.Integer.parseInt
+
+import geotrellis.raster.render.{ColorMap, ColorRamp, RGBA}
 
 import scala.collection.mutable.ListBuffer
 import scala.io.Source
@@ -8,27 +10,48 @@ import scala.io.Source
 object ColorMapParser {
 
   def parse(fileName: String): ColorMap = {
-    val elemList = new ListBuffer[(Int, Int)]
+    val colorBuffer = new ListBuffer[(Double, Int)]
     
     var opacity = "1.0"
 
     val opacityRegex = """<Opacity>(.*)</Opacity>""".r
     val colorRegex = """<ColorMapEntry color="#([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})" quantity="(\d*)"(?:.*opacity="(.*)")?.*""".r
     
+    var previousVal: Option[Int] = None
+    var previousColor: Option[Int] = None
+    
     for (line <- Source.fromFile(fileName).getLines()) {
       line.trim match {
         case opacityRegex(o) => opacity = o  
-        case colorRegex(r, g, b, value, o) => 
-          val a = Option(o).getOrElse(opacity).toDouble * 100
-          elemList += ((value.toInt.toByte, RGBA(hexToInt(r), hexToInt(g), hexToInt(b), a)))
+        case colorRegex(r, g, b, q, o) => addColorsToBuffer(r, g, b, q, o)
         case _ =>
       }
     }
 
-    def hexToInt(hex: String): Int = {
-      Integer.parseInt(hex, 16)
+    def addColorsToBuffer(r: String, g: String, b: String, q: String, o: String) {
+      val value = q.toInt
+      val a = Option(o).getOrElse(opacity).toDouble * 100
+      val color = RGBA(hexToInt(r), hexToInt(g), hexToInt(b), a)
+      
+      previousVal match {
+        case None => colorBuffer += ((value, color))
+        case Some(v) =>
+          val stops = value - v + 1
+          val colors = ColorRamp(previousColor.get, color).stops(stops).colors
+
+          for ((c, i) <- colors.zipWithIndex if i > 0) {
+            colorBuffer += ((v + i, c))
+          }
+      }
+
+      previousVal = Some(value)
+      previousColor = Some(color)
     }
 
-    ColorMap(Map(elemList:_*))
+    def hexToInt(hex: String): Int = {
+      parseInt(hex, 16)
+    }
+    
+    ColorMap(colorBuffer:_*)
   }
 }
