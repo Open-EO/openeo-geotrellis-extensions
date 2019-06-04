ec9c3a20f862ed3f10145279be0e1d3a1346130a
Jan Van den bosch <jan.vandenbosch@hotmail.com>
2019-06-04 09:07:09 +0200

    limit tiles to bbox

--- geotrellis-seeder/src/test/scala/Sentinel2FileDataCubeTest.scala
+++ geotrellis-seeder/src/test/scala/Sentinel2FileDataCubeTest.scala
@@ -1,23 +1,45 @@
-import geotrellis.contrib.vlm.ReadingSource
 import geotrellis.contrib.vlm.geotiff.GeoTiffRasterSource
-import geotrellis.contrib.vlm.spark.RasterSourceRDD
 import geotrellis.proj4.LatLng
-import geotrellis.raster.{GridExtent, MultibandTile, Raster}
-import geotrellis.raster.io.geotiff.{MultibandGeoTiff, SinglebandGeoTiff}
-import geotrellis.spark.tiling.LayoutDefinition
+import geotrellis.raster.io.geotiff.MultibandGeoTiff
+import geotrellis.raster.{MultibandTile, Raster}
+import geotrellis.spark._
+import geotrellis.spark.tiling.FloatingLayoutScheme
 import geotrellis.spark.util.SparkUtils
-import geotrellis.vector.Extent
+import geotrellis.vector.{Extent, ProjectedExtent}
+import org.apache.spark.SparkContext
 import org.junit.Test
 
 class Sentinel2FileDataCubeTest {
 
   @Test
   def test(): Unit = {
-    val geoTiff10m = GeoTiffRasterSource("/data/MTDA/CGS_S2/CGS_S2_RADIOMETRY/2019/03/25/S2B_20190325T105709Z_31UDS_CGS_V102_000/S2B_20190325T105709Z_31UDS_TOC_V102/S2B_20190325T105709Z_31UDS_TOC-B02_10M_V102.tif")
-    val geoTiff20m = GeoTiffRasterSource("/data/MTDA/CGS_S2/CGS_S2_RADIOMETRY/2019/03/25/S2B_20190325T105709Z_31UDS_CGS_V102_000/S2B_20190325T105709Z_31UDS_TOC_V102/S2B_20190325T105709Z_31UDS_TOC-B05_20M_V102.tif")
-    val geoTiff60m = GeoTiffRasterSource("/data/MTDA/CGS_S2/CGS_S2_RADIOMETRY/2019/03/25/S2B_20190325T105709Z_31UDS_CGS_V102_000/S2B_20190325T105709Z_31UDS_TOC_V102/S2B_20190325T105709Z_31UDS_TOC-B01_60M_V102.tif")
+    val geoTiff10m = "/data/MTDA/CGS_S2/CGS_S2_RADIOMETRY/2019/03/25/S2B_20190325T105709Z_31UDS_CGS_V102_000/S2B_20190325T105709Z_31UDS_TOC_V102/S2B_20190325T105709Z_31UDS_TOC-B02_10M_V102.tif"
+    val geoTiff20m = "/data/MTDA/CGS_S2/CGS_S2_RADIOMETRY/2019/03/25/S2B_20190325T105709Z_31UDS_CGS_V102_000/S2B_20190325T105709Z_31UDS_TOC_V102/S2B_20190325T105709Z_31UDS_TOC-B05_20M_V102.tif"
+    val geoTiff60m = "/data/MTDA/CGS_S2/CGS_S2_RADIOMETRY/2019/03/25/S2B_20190325T105709Z_31UDS_CGS_V102_000/S2B_20190325T105709Z_31UDS_TOC_V102/S2B_20190325T105709Z_31UDS_TOC-B01_60M_V102.tif"
 
-    val bands = Seq(geoTiff10m, geoTiff20m, geoTiff60m)
+    val bandGeoTiffs = Seq(geoTiff10m, geoTiff20m, geoTiff60m)
+    val bbox = ProjectedExtent(Extent(xmin = 2.59003, ymin = 51.069, xmax = 2.8949, ymax = 51.2206), LatLng)
+
+    implicit val sc = SparkUtils.createLocalSparkContext("local[*]", getClass.getSimpleName)
+
+    try {
+      val rasterSourceRDD = tileLayerRdd(bandGeoTiffs, bbox)
+      val tileCount = rasterSourceRDD.count()
+
+      println(s"got $tileCount tiles")
+
+      val Raster(multibandTile, extent) = rasterSourceRDD
+          .crop(bbox.reproject(rasterSourceRDD.metadata.crs))
+          .stitch()
+
+      MultibandGeoTiff(multibandTile, extent, rasterSourceRDD.metadata.crs).write("/tmp/stitched.geotiff")
+    } finally {
+      sc.stop()
+    }
+  }
+
+  private def tileLayerRdd(geoTiffBandUris: Iterable[String], bbox: ProjectedExtent)(implicit sc: SparkContext): MultibandTileLayerRDD[SpatialKey] = {
+    val bands = geoTiffBandUris.map(GeoTiffRasterSource(_))
 
     bands
       .map(band => band.cellSize)
@@ -25,41 +47,31 @@ class Sentinel2FileDataCubeTest {
 
     val maxResolutionBand = bands.minBy(_.cellSize.resolution)
 
-    val upsampledBands = bands
-      .map { band =>
-        if (band.cellSize == maxResolutionBand.cellSize) band else band.resampleToGrid(maxResolutionBand.rasterExtent)
+    val reprojectedBbox = bbox.reproject(maxResolutionBand.crs)
+
+    val layout = FloatingLayoutScheme(tileSize = 256).levelFor(maxResolutionBand.extent, maxResolutionBand.cellSize).layout
+    val overlappingKeys = layout.mapTransform.keysForGeometry(reprojectedBbox.toPolygon())
+
+    val tiles = bands
+      .map { band  => band.tileToLayout(layout) }
+      .flatMap { layoutTileSource => layoutTileSource.readAll(overlappingKeys.toIterator).map { case (key, multibandTile) => (key, multibandTile.band(0)) } }
+      .groupBy { case (key, _) => key }
+      .map { case (key, tilesPerKey) =>
+        val tiles = tilesPerKey.map { case (_, tile) => tile }
+        (key, MultibandTile(tiles))
       }
+      .toSeq
 
-    val bbox = Extent(xmin = 2.59003, ymin = 51.069, xmax = 2.8949, ymax = 51.2206).reproject(LatLng, maxResolutionBand.crs)
+    val tilesRdd = sc.parallelize(tiles, tiles.size)
 
-    /*val singleGeotiffs = for {
-      (upsampledBand, i) <- upsampledBands.zipWithIndex
-      Some(Raster(tile, extent)) = upsampledBand.read(bbox)
-    } yield (SinglebandGeoTiff(tile.band(0), extent, upsampledBand.crs), i)
+    val metadata = TileLayerMetadata[SpatialKey](
+      maxResolutionBand.cellType,
+      layout,
+      reprojectedBbox,
+      maxResolutionBand.crs,
+      KeyBounds(layout.mapTransform(reprojectedBbox))
+    )
 
-    singleGeotiffs.foreach { case (geotiff, i) => geotiff.write(s"/tmp/${i}_upsampled.geotiff") }
-
-    val multibandTile = MultibandTile(singleGeotiffs.map { case (geotiff, _) => geotiff.tile })
-    MultibandGeoTiff(multibandTile, singleGeotiffs.head._1.extent, maxResolutionBand.crs).write("/tmp/multi_upsampled.geotiff")*/
-
-    val gridExtent = GridExtent(maxResolutionBand.extent, maxResolutionBand.cellSize)
-    val layout = LayoutDefinition(gridExtent, 256)
-
-    implicit val sc = SparkUtils.createLocalSparkContext("local[1]", getClass.getSimpleName)
-
-    try {
-      /*val rasterSourceRDD = RasterSourceRDD(upsampledBands, layout)
-        .crop(bbox)*/
-      val readingSources = upsampledBands.zipWithIndex
-        .map { case (upsampledBand, i) => ReadingSource(upsampledBand, i) }
-
-      val rasterSourceRDD = RasterSourceRDD.read(readingSources, layout)
-        .crop(bbox)
-
-      val Raster(multibandTile, extent) = rasterSourceRDD.stitch()
-      MultibandGeoTiff(multibandTile, extent, rasterSourceRDD.metadata.crs).write("/tmp/stitched.geotiff")
-    } finally {
-      sc.stop()
-    }
+    ContextRDD(tilesRdd, metadata)
   }
 }
