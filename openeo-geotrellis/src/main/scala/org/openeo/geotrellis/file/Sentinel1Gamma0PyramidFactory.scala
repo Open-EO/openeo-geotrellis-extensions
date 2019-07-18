1367c52358119779faae506593cb217d4a84c2a2
Niels Hamers <niels.hamers@assign.be>
2019-07-18 11:24:50 +0200

    add Sentinel1 Gamma0 layer based on sentinel hub

---
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/file/Sentinel1Gamma0PyramidFactory.scala
@@ -0,0 +1,76 @@
+package org.openeo.geotrellis.file
+
+import java.time.ZonedDateTime
+
+import geotrellis.proj4.{CRS, WebMercator}
+import geotrellis.raster.{FloatCellType, MultibandTile}
+import geotrellis.spark.pyramid.Pyramid
+import geotrellis.spark.tiling._
+import geotrellis.spark.{ContextRDD, KeyBounds, MultibandTileLayerRDD, SpaceTimeKey, TileLayerMetadata}
+import geotrellis.vector.{Extent, ProjectedExtent}
+import org.apache.spark.SparkContext
+import org.openeo.geotrellissentinelhub.Gamma0Bands._
+import org.openeo.geotrellissentinelhub._
+
+object Sentinel1Gamma0PyramidFactory {
+  private val maxZoom = 14
+}
+
+class Sentinel1Gamma0PyramidFactory {
+  import Sentinel1Gamma0PyramidFactory._
+
+  private def sequentialDates(from: ZonedDateTime): Stream[ZonedDateTime] = from #:: sequentialDates(from plusDays 1)
+  
+  def layer(boundingBox: ProjectedExtent, from: ZonedDateTime, to: ZonedDateTime, zoom: Int = maxZoom, bands: Seq[Band] = gamma0Bands)(implicit sc: SparkContext): MultibandTileLayerRDD[SpaceTimeKey] = {
+    require(zoom >= 0)
+    require(zoom <= maxZoom)
+
+    val targetCrs: CRS = WebMercator
+    val reprojectedBoundingBox = boundingBox.reproject(targetCrs)
+    
+    val layout = ZoomedLayoutScheme(targetCrs).levelForZoom(targetCrs.worldExtent, zoom).layout
+
+    val dates = sequentialDates(from)
+      .takeWhile(date => !(date isAfter to))
+
+    val tilesRdd = sc.parallelize(dates, dates.size)
+      .flatMap(date => layout.mapTransform.keysForGeometry(reprojectedBoundingBox.toPolygon()).map(key => SpaceTimeKey(key, date)))
+      .map(key => (key, retrieveS1Gamma0TileFromSentinelHub(key.spatialKey.extent(layout), key.temporalKey, layout.tileLayout.tileCols, layout.tileLayout.tileRows, bands)))
+
+    val metadata: TileLayerMetadata[SpaceTimeKey] = {
+      val gridBounds = layout.mapTransform.extentToBounds(reprojectedBoundingBox)
+
+      TileLayerMetadata(
+        cellType = FloatCellType,
+        layout = layout,
+        extent = reprojectedBoundingBox,
+        crs = targetCrs,
+        KeyBounds(SpaceTimeKey(gridBounds.colMin, gridBounds.rowMin, from), SpaceTimeKey(gridBounds.colMax, gridBounds.rowMax, to))
+      )
+    }
+
+    ContextRDD(tilesRdd, metadata)
+  }
+  
+  def pyramid(boundingBox: ProjectedExtent, from: ZonedDateTime, to: ZonedDateTime, bands: Seq[Band] = gamma0Bands)(implicit sc: SparkContext): Pyramid[SpaceTimeKey, MultibandTile, TileLayerMetadata[SpaceTimeKey]] = {
+    val layers = for (zoom <- maxZoom to 0 by -1) yield zoom -> layer(boundingBox, from, to, zoom, bands)
+    Pyramid(layers.toMap)
+  }
+  
+  def pyramid_seq(bbox: Extent, bbox_srs: String, from_date: String, to_date: String, band_indices: java.util.List[Int]): Seq[(Int, MultibandTileLayerRDD[SpaceTimeKey])] = {
+    implicit val sc: SparkContext = SparkContext.getOrCreate()
+    
+    val projectedExtent = ProjectedExtent(bbox, CRS.fromName(bbox_srs))
+    val from = ZonedDateTime.parse(from_date)
+    val to = ZonedDateTime.parse(to_date)
+
+    val bands: Seq[Band] =
+      if (band_indices == null) gamma0Bands
+      else gamma0Bands.filter(b => band_indices.contains(gamma0Bands.indexOf(b)))
+
+    pyramid(projectedExtent, from, to, bands).levels.toSeq
+      .sortBy { case (zoom, _) => zoom }
+      .reverse
+  }
+
+}
---
+++ openeo-geotrellis/src/test/scala/org/openeo/geotrellis/file/Sentinel1Gamma0PyramidFactoryTest.scala
@@ -0,0 +1,70 @@
+package org.openeo.geotrellis.file
+
+import java.time.format.DateTimeFormatter
+import java.time.{LocalDate, LocalTime, ZoneOffset, ZonedDateTime}
+
+import geotrellis.proj4.LatLng
+import geotrellis.raster.Raster
+import geotrellis.raster.io.geotiff.MultibandGeoTiff
+import geotrellis.spark.util.SparkUtils
+import geotrellis.vector.{Extent, ProjectedExtent}
+import org.apache.spark.SparkConf
+import org.junit.{Ignore, Test}
+import org.openeo.geotrellissentinelhub.Gamma0Bands.{IW_VH, IW_VV, gamma0Bands}
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable.ArrayBuffer
+
+class Sentinel1Gamma0PyramidFactoryTest {
+
+  private val pyramidFactory = new Sentinel1Gamma0PyramidFactory
+
+  @Ignore
+  @Test
+  def writeGeoTiffs(): Unit = {
+    val boundingBox = ProjectedExtent(Extent(xmin = 2.59003, ymin = 51.069, xmax = 2.8949, ymax = 51.2206), LatLng)
+    val from = ZonedDateTime.of(LocalDate.of(2019, 3, 25), LocalTime.MIDNIGHT, ZoneOffset.UTC)
+    val to = from// plusWeeks 1
+
+    val sparkConf = new SparkConf()
+      .set("spark.kryoserializer.buffer.max", "512m")
+      .set("spark.rdd.compress","true")
+
+    val sc = SparkUtils.createLocalSparkContext(sparkMaster = "local[*]", appName = getClass.getSimpleName, sparkConf)
+
+    try {
+      val srs = s"EPSG:${boundingBox.crs.epsgCode.get}"
+      val bandIndices = ArrayBuffer(IW_VH, IW_VV).map(gamma0Bands.indexOf(_)).asJava
+
+      val pyramid = pyramidFactory.pyramid_seq(boundingBox.extent, srs,
+        DateTimeFormatter.ISO_OFFSET_DATE_TIME format from, DateTimeFormatter.ISO_OFFSET_DATE_TIME format to,
+        bandIndices)
+      
+      val zoom = 14
+
+      val baseLayer = pyramid
+        .find { case (index, _) => index == zoom }
+        .map { case (_, layer) => layer }
+        .get.cache()
+
+      println(s"got ${baseLayer.count()} tiles")
+
+      val timestamps = baseLayer.keys
+        .map(_.time)
+        .distinct()
+        .collect()
+        .sortWith(_ isBefore _)
+
+      for (timestamp <- timestamps) {
+        val Raster(multibandTile, extent) = baseLayer
+          .toSpatial(timestamp)
+          .stitch()
+
+        MultibandGeoTiff(multibandTile, extent, baseLayer.metadata.crs)
+          .write(s"/home/niels/gamma0/${zoom}_${DateTimeFormatter.ISO_LOCAL_DATE format timestamp}.tif")
+      }
+    } finally {
+      sc.stop()
+    }
+  }
+}
