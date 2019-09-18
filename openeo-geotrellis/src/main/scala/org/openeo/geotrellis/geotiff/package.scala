a923ee07edfa9ed808a844726f9f75f8b532d3a5
Jan Van den bosch <jan.vandenbosch@hotmail.com>
2019-09-18 13:29:11 +0200

    support compressed geotiffs (not in GeoPySpark API)

---
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/geotiff/package.scala
@@ -0,0 +1,58 @@
+package org.openeo.geotrellis
+
+import java.util
+
+import geotrellis.raster.io.geotiff._
+import geotrellis.raster.io.geotiff.compression.Compression
+import geotrellis.raster.{MultibandTile, Raster}
+import geotrellis.spark.{ContextRDD, Metadata, SpatialKey, TileLayerMetadata}
+import geotrellis.vector.Extent
+import org.apache.spark.rdd.RDD
+import scala.collection.JavaConverters._
+
+package object geotiff {
+  type SRDD = RDD[(SpatialKey, MultibandTile)] with Metadata[TileLayerMetadata[SpatialKey]]
+
+  private def toExtent(extent: util.Map[String, Double]) =  Extent(
+    extent.get("xmin"),
+    extent.get("ymin"),
+    extent.get("xmax"),
+    extent.get("ymax")
+  )
+
+  // ~ SpatialTiledRasterLayer in GeoPySpark but supports compression
+  def saveStitched(rdd: SRDD, path: String, compression: Compression): Unit =
+    saveStitched(rdd, path, None, None, compression)
+
+  def saveStitched(
+    rdd: SRDD,
+    path: String,
+    cropBounds: Option[util.Map[String, Double]],
+    cropDimensions: Option[util.ArrayList[Int]],
+    compression: Compression)
+  : Unit = {
+    val contextRDD = ContextRDD(rdd, rdd.metadata)
+
+    val stitched: Raster[MultibandTile] = contextRDD.stitch()
+
+    val adjusted = {
+      val cropped =
+        cropBounds match {
+          case Some(extent) => stitched.crop(toExtent(extent))
+          case None => stitched
+        }
+
+      val resampled =
+        cropDimensions.map(_.asScala.toArray) match {
+          case Some(dimensions) =>
+            cropped.resample(dimensions(0), dimensions(1))
+          case None =>
+            cropped
+        }
+
+      resampled
+    }
+
+    MultibandGeoTiff(adjusted, contextRDD.metadata.crs, GeoTiffOptions(compression)).write(path)
+  }
+}
