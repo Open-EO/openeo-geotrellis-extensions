9dd0860594c0a9790431e87dc0142374fefab192
Niels Hamers <niels.hamers@assign.be>
2019-09-18 14:35:34 +0200

    ignore testGamma0

--- geotrellis-sentinelhub/src/test/scala/org/openeo/geotrellissentinelhub/TestS1Gamma0.scala
+++ geotrellis-sentinelhub/src/test/scala/org/openeo/geotrellissentinelhub/TestS1Gamma0.scala
@@ -3,12 +3,13 @@ package org.openeo.geotrellissentinelhub
 import java.time.{LocalDate, ZoneId}
 
 import geotrellis.vector.Extent
-import org.junit.Test
+import org.junit.{Ignore, Test}
 import org.openeo.geotrellissentinelhub.Gamma0Bands._
 
 class TestS1Gamma0 {
   
   @Test
+  @Ignore
   def testGamma0(): Unit = {
     val bbox = new Extent(-5948635.289265557,-1252344.2714243263,-5792092.255337516,-1095801.2374962857)
 
