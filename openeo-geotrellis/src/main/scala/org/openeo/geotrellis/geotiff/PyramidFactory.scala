783b0b5d09129f6d862677eced7ce29cbdd42ce9
Jan Van den bosch <jan.vandenbosch@hotmail.com>
2019-10-11 08:55:49 +0200

    EP-3016: added a GeoTiff PyramidFactory
    it supports files on disk and HDFS, as well as S3 (but only public
    objects at amazonaws.com)

---
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/geotiff/PyramidFactory.scala
@@ -0,0 +1,240 @@
+package org.openeo.geotrellis.geotiff
+
+import java.time.LocalTime.MIDNIGHT
+import java.time.ZoneOffset.UTC
+import java.time.{LocalDate, ZonedDateTime}
+
+import geotrellis.contrib.vlm.geotiff.GeoTiffRasterSource
+import com.amazonaws.services.s3.{AmazonS3ClientBuilder, AmazonS3URI}
+import com.amazonaws.services.s3.model.ListObjectsRequest
+import geotrellis.contrib.vlm.spark.RasterSourceRDD.DEFAULT_PARTITION_BYTES
+import geotrellis.contrib.vlm.RasterSource
+import geotrellis.proj4.{CRS, WebMercator}
+import geotrellis.raster.MultibandTile
+import geotrellis.spark.io.hadoop.HdfsUtils
+import geotrellis.spark.io.s3.AmazonS3Client
+import geotrellis.spark.pyramid.Pyramid
+import geotrellis.spark.tiling.{LayoutDefinition, ZoomedLayoutScheme}
+import geotrellis.spark.{ContextRDD, KeyBounds, MultibandTileLayerRDD, SpaceTimeKey, SpatialKey, TemporalKey, TileLayerMetadata}
+import geotrellis.vector.{Extent, ProjectedExtent}
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+
+import scala.collection.mutable.ArrayBuilder
+import scala.reflect.ClassTag
+import scala.util.matching.Regex
+
+object PyramidFactory {
+  def from_disk(glob_pattern: String, date_regex: String): PyramidFactory = {
+    new PyramidFactory({
+      val conf = new Configuration
+
+      HdfsUtils.listFiles(new Path(glob_pattern), conf)
+        .map(path => (GeoTiffRasterSource(path.toString), deriveDate(path.getName, date_regex.r.unanchored)))
+    })
+  }
+
+  def from_s3(s3_uri: String, key_regex: String = ".*", date_regex: String): PyramidFactory = {
+    new PyramidFactory({
+      // adapted from geotrellis.spark.io.s3.geotiff.S3GeoTiffInput.list
+      val s3Uri = new AmazonS3URI(s3_uri)
+      val keyPattern = key_regex.r.unanchored
+
+      val s3Client = {
+        // pass e.g. non-AWS S3 endpoint and temporary credentials (as described at
+        // https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/prog-services-sts.html) here
+
+        AmazonS3Client(
+          AmazonS3ClientBuilder
+            .standard()
+            .build()
+        )
+      }
+
+      val objectRequest = (new ListObjectsRequest)
+        .withBucketName(s3Uri.getBucket)
+        .withPrefix(s3Uri.getKey)
+        .withDelimiter("/") // recursive
+
+      s3Client.listKeys(objectRequest)
+        .flatMap { key =>
+          key match {
+            case keyPattern(_*) => Some(new AmazonS3URI(s"s3://${s3Uri.getBucket}/${key}"))
+            case _ => None
+          }
+        }
+        .map(uri => (GeoTiffRasterSource(uri.toString), deriveDate(uri.getKey, date_regex.r.unanchored)))
+    })
+  }
+
+  private def deriveDate(filename: String, date: Regex): ZonedDateTime = {
+    filename match {
+      case date(year, month, day) => ZonedDateTime.of(LocalDate.of(year.toInt, month.toInt, day.toInt), MIDNIGHT, UTC)
+    }
+  }
+
+  private def partition[T: ClassTag](
+    chunks: Traversable[T],
+    maxPartitionSize: Long
+  )(chunkSize: T => Long = { c: T => 1l }): Array[Array[T]] = {
+    if (chunks.isEmpty) {
+      Array[Array[T]]()
+    } else {
+      val partition = ArrayBuilder.make[T]
+      partition.sizeHintBounded(128, chunks)
+      var partitionSize: Long = 0l
+      var partitionCount: Long = 0l
+      val partitions = ArrayBuilder.make[Array[T]]
+
+      def finalizePartition() {
+        val res = partition.result
+        if (res.nonEmpty) partitions += res
+        partition.clear()
+        partitionSize = 0l
+        partitionCount = 0l
+      }
+
+      def addToPartition(chunk: T) {
+        partition += chunk
+        partitionSize += chunkSize(chunk)
+        partitionCount += 1
+      }
+
+      for (chunk <- chunks) {
+        if ((partitionCount == 0) || (partitionSize + chunkSize(chunk)) < maxPartitionSize)
+          addToPartition(chunk)
+        else {
+          finalizePartition()
+          addToPartition(chunk)
+        }
+      }
+
+      finalizePartition()
+      partitions.result
+    }
+  }
+}
+
+class PyramidFactory private (rasterSources: => Seq[(RasterSource, ZonedDateTime)]) {
+  import PyramidFactory._
+
+  private val targetCrs = WebMercator
+
+  private lazy val reprojectedRasterSources =
+    rasterSources.map { case (rasterSource, date) => (rasterSource.reproject(targetCrs), date) }
+
+  private lazy val maxZoom = {
+    val (rasterSource, _) = reprojectedRasterSources.head
+    ZoomedLayoutScheme(targetCrs).zoom(rasterSource.extent.center.x, rasterSource.extent.center.y, rasterSource.cellSize)
+  }
+
+  def pyramid_seq(bbox: Extent, bbox_srs: String, from_date: String, to_date: String): Seq[(Int, MultibandTileLayerRDD[SpaceTimeKey])] = {
+    implicit val sc: SparkContext = SparkContext.getOrCreate()
+
+    val projectedExtent = ProjectedExtent(bbox, CRS.fromName(bbox_srs))
+    val from = ZonedDateTime.parse(from_date)
+    val to = ZonedDateTime.parse(to_date)
+
+    pyramid(projectedExtent, from, to).levels.toSeq
+      .sortBy { case (zoom, _) => zoom }
+      .reverse
+  }
+
+  def pyramid(boundingBox: ProjectedExtent, from: ZonedDateTime, to: ZonedDateTime)(implicit sc: SparkContext): Pyramid[SpaceTimeKey, MultibandTile, TileLayerMetadata[SpaceTimeKey]] = {
+    val layers = for (zoom <- maxZoom to 0 by -1) yield zoom -> layer(boundingBox, from, to, zoom)
+    Pyramid(layers.toMap)
+  }
+
+  def layer(boundingBox: ProjectedExtent, from: ZonedDateTime, to: ZonedDateTime, zoom: Int = maxZoom)(implicit sc: SparkContext): MultibandTileLayerRDD[SpaceTimeKey] = {
+    val reprojectedBoundingBox = ProjectedExtent(boundingBox.reproject(targetCrs), targetCrs)
+
+    val overlappingRasterSources = reprojectedRasterSources
+      .filter { case (rasterSource, date) =>
+        // FIXME: this means the driver will (partially) read the geotiff instead of the executor - on the other hand, e.g. an AccumuloRDDReader will interpret a LayerQuery both in the driver (to determine Accumulo ranges) and the executors
+        // FIXME: what's the advantage of an AttributeStore.query over comparing extents?
+        val overlaps = rasterSource.extent intersects reprojectedBoundingBox.extent
+
+        // FIXME: this is also done in the driver while a RasterSource carries a URI so an executor can do it himself
+        val withinDateRange = !(date isBefore from) && !(date isAfter to)
+
+        overlaps && withinDateRange
+      }
+
+    val layout = ZoomedLayoutScheme(targetCrs).levelForZoom(zoom).layout
+
+    rasterSourceRDD(overlappingRasterSources, layout)
+  }
+
+  // adapted from geotrellis.contrib.vlm.spark.RasterSourceRDD.apply
+  private def rasterSourceRDD(
+    sources: Seq[(RasterSource, ZonedDateTime)],
+    layout: LayoutDefinition,
+    partitionBytes: Long = DEFAULT_PARTITION_BYTES
+  )(implicit sc: SparkContext): MultibandTileLayerRDD[SpaceTimeKey] = {
+    val cellTypes = sources.map { case (source, _) => source.cellType }.toSet
+    require(cellTypes.size == 1, s"All RasterSources must have the same CellType, but multiple ones were found: $cellTypes")
+
+    val projections = sources.map { case (source, _) => source.crs }.toSet
+    require(
+      projections.size == 1,
+      s"All RasterSources must be in the same projection, but multiple ones were found: $projections"
+    )
+
+    val Some((lowerTimestamp, upperTimestamp)) = sources
+      .map { case (_, timestamp) => timestamp }
+      .foldLeft(z = None: Option[(ZonedDateTime, ZonedDateTime)]) { case (acc, timestamp) =>
+        acc match {
+          case None => Some((timestamp, timestamp))
+          case Some((lower, upper)) => Some((
+            if (timestamp isBefore lower) timestamp else lower,
+            if (timestamp isAfter upper) timestamp else upper
+          ))
+        }
+      }
+
+    val cellType = cellTypes.head
+    val crs = projections.head
+
+    val mapTransform = layout.mapTransform
+    val extent = mapTransform.extent
+    val combinedExtents = sources.map { case (source, _) => source.extent }.reduce { _ combine _ }
+
+    val spatialKeyBounds = KeyBounds(mapTransform(combinedExtents))
+    val layerKeyBounds = KeyBounds(
+      SpaceTimeKey(spatialKeyBounds.minKey, TemporalKey(lowerTimestamp)),
+      SpaceTimeKey(spatialKeyBounds.maxKey, TemporalKey(upperTimestamp))
+    )
+
+    val layerMetadata =
+      TileLayerMetadata[SpaceTimeKey](cellType, layout, combinedExtents, crs, layerKeyBounds)
+
+    val sourcesRDD: RDD[((RasterSource, ZonedDateTime), Array[SpatialKey])] =
+      sc.parallelize(sources).flatMap { case (source, timestamp) =>
+        val keys: Traversable[SpatialKey] =
+          extent.intersection(source.extent) match {
+            case Some(intersection) =>
+              layout.mapTransform.keysForGeometry(intersection.toPolygon)
+            case None =>
+              Seq.empty[SpatialKey]
+          }
+        val tileSize = layout.tileCols * layout.tileRows * cellType.bytes
+        partition(keys, partitionBytes)( _ => tileSize).map { res => ((source, timestamp), res) }
+      }
+
+    val repartitioned = sourcesRDD
+
+    val result: RDD[(SpaceTimeKey, MultibandTile)] =
+      repartitioned.flatMap { case ((source, timestamp), keys) =>
+        // might need to e.g. anonymously subclass GeoTiffReprojectRasterSource to be able to pass a custom S3 client
+        // (see org.openeo.geotrellis.geotiff.PyramidFactoryTest.anonymousInnerClass)
+        val tileSource = source.tileToLayout(layout)
+        tileSource.readAll(keys.toIterator).map { case (SpatialKey(col, row), tile) =>
+          (SpaceTimeKey(col = col, row = row, timestamp), tile)
+        }
+      }
+
+    ContextRDD(result, layerMetadata)
+  }
+}
---
+++ openeo-geotrellis/src/test/scala/org/openeo/geotrellis/geotiff/PyramidFactoryTest.scala
@@ -0,0 +1,171 @@
+package org.openeo.geotrellis.geotiff
+
+import java.time.LocalTime.MIDNIGHT
+import java.time.ZoneOffset.UTC
+import java.time.{LocalDate, ZonedDateTime}
+import java.time.format.DateTimeFormatter.{ISO_LOCAL_DATE, ISO_OFFSET_DATE_TIME}
+
+import geotrellis.proj4.{CRS, LatLng}
+import geotrellis.raster.Raster
+import geotrellis.raster.io.geotiff.MultibandGeoTiff
+import geotrellis.spark.{MultibandTileLayerRDD, SpaceTimeKey}
+import geotrellis.spark.util.SparkUtils
+import geotrellis.vector.{Extent, ProjectedExtent}
+import org.apache.spark.SparkConf
+import org.junit.{Ignore, Test}
+import org.junit.Assert._
+
+class PyramidFactoryTest {
+
+  @Test
+  def singleBandGeoTiffFromDiskForSingleDate(): Unit = {
+    val from = ZonedDateTime.of(LocalDate.of(2019, 4, 24), MIDNIGHT, UTC)
+    val to = from
+
+    singleBandGeoTiffFromDisk(
+      globPattern = "file:/data/MTDA/CGS_S2/CGS_S2_FAPAR/2019/04/24/*/*/10M/*_FAPAR_10M_V102.tif", from, to)
+  }
+
+  @Test
+  def singleBandGeoTiffFromDiskForMultipleDates(): Unit = {
+    val from = ZonedDateTime.of(LocalDate.of(2019, 4, 24), MIDNIGHT, UTC)
+    val to = from plusDays 2
+
+    singleBandGeoTiffFromDisk(
+      globPattern = "file:/data/MTDA/CGS_S2/CGS_S2_FAPAR/2019/04/2[34567]/*/*/10M/*_FAPAR_10M_V102.tif", from, to)
+  }
+
+  private def singleBandGeoTiffFromDisk(globPattern: String, from: ZonedDateTime, to: ZonedDateTime): Unit = {
+    val boundingBox = ProjectedExtent(Extent(xmin = 2.59003, ymin = 51.069, xmax = 2.8949, ymax = 51.2206), LatLng)
+
+    val pyramidFactory = PyramidFactory.from_disk(
+      globPattern,
+      date_regex = raw"_(\d{4})(\d{2})(\d{2})T"
+    )
+
+    val sparkConf = new SparkConf()
+      .set("spark.kryoserializer.buffer.max", "512m")
+      .set("spark.rdd.compress","true")
+
+    val sc = SparkUtils.createLocalSparkContext(sparkMaster = "local[*]", appName = getClass.getSimpleName, sparkConf)
+
+    try {
+      val srs = s"EPSG:${boundingBox.crs.epsgCode.get}"
+
+      val pyramid = pyramidFactory.pyramid_seq(boundingBox.extent, srs,
+        ISO_OFFSET_DATE_TIME format from, ISO_OFFSET_DATE_TIME format to)
+
+      val (maxZoom, _) = pyramid.maxBy { case (zoom, _) => zoom }
+      assertEquals(14, maxZoom)
+
+      saveLayerAsGeoTiff(pyramid, boundingBox, zoom = 10)
+    } finally sc.stop()
+  }
+
+  private def saveLayerAsGeoTiff(pyramid: Seq[(Int, MultibandTileLayerRDD[SpaceTimeKey])], boundingBox: ProjectedExtent,
+                                 zoom: Int): Unit = {
+    val layer = pyramid
+      .find { case (index, _) => index == zoom }
+      .map { case (_, layer) => layer }
+      .get.cache()
+
+    println(s"got ${layer.count()} tiles")
+
+    val timestamps = layer.keys
+      .map(_.time)
+      .distinct()
+      .collect()
+      .sortWith(_ isBefore _)
+
+    for (timestamp <- timestamps) {
+      val Raster(multibandTile, extent) = layer
+        .toSpatial(timestamp)
+        // note: the geotiff seems to move around for lower zoom levels (< 9) because of this crop operation :/
+        .crop(boundingBox.reproject(layer.metadata.crs))
+        .stitch()
+
+      MultibandGeoTiff(multibandTile, extent, layer.metadata.crs)
+        .write(s"/tmp/stitched_${ISO_LOCAL_DATE format timestamp}_$zoom.tif")
+    }
+  }
+
+  @Ignore("currently works only against amazonaws.com")
+  @Test
+  def singleBandGeoTiffFromS3ForSingleDate(): Unit = {
+    // otherwise the S3 client will keep retrying to access
+    // http://169.254.169.254/latest/meta-data/iam/security-credentials/
+    assertNotNull("aws.accessKeyId is not set", System.getProperty("aws.accessKeyId"))
+    assertNotNull("aws.secretKey is not set", System.getProperty("aws.secretKey"))
+    System.setProperty("aws.region", "eu-west-3")
+
+    val boundingBox = ProjectedExtent(Extent(xmin = 679605.00, ymin = 5667337.31, xmax = 691784.50, ymax = 5678547.98),
+      CRS.fromEpsgCode(32631))
+
+    val from = ZonedDateTime.of(LocalDate.of(2018, 4, 28), MIDNIGHT, UTC)
+    val to = from
+
+    val pyramidFactory = PyramidFactory.from_s3(
+      s3_uri = "s3://openeo-vito-test/cogs/",
+      key_regex = raw"_20180428T",
+      date_regex = raw"_(\d{4})(\d{2})(\d{2})T\d{6}\.tiff"
+    )
+
+    val sparkConf = new SparkConf()
+      .set("spark.kryoserializer.buffer.max", "512m")
+      .set("spark.rdd.compress","true")
+
+    val sc = SparkUtils.createLocalSparkContext(sparkMaster = "local[*]", appName = getClass.getSimpleName, sparkConf)
+
+    try {
+      val srs = s"EPSG:${boundingBox.crs.epsgCode.get}"
+      val pyramid = pyramidFactory.pyramid_seq(boundingBox.extent, srs,
+        ISO_OFFSET_DATE_TIME format from, ISO_OFFSET_DATE_TIME format to)
+
+      val (maxZoom, _) = pyramid.maxBy { case (zoom, _) => zoom }
+      assertEquals(14, maxZoom)
+
+      saveLayerAsGeoTiff(pyramid, boundingBox, zoom = 10)
+    } finally sc.stop()
+  }
+
+  @Ignore("not a real test but trying to pass a custom S3 client to a GeoTiffRasterSource")
+  @Test
+  def anonymousInnerClass(): Unit = {
+    import geotrellis.contrib.vlm.geotiff.GeoTiffRasterSource
+    import geotrellis.raster.io.geotiff.reader.GeoTiffReader
+    import geotrellis.util.StreamingByteReader
+    import geotrellis.spark.io.s3.util.S3RangeReader
+    import com.amazonaws.auth.AWSStaticCredentialsProvider
+    import com.amazonaws.auth.BasicAWSCredentials
+    import com.amazonaws.services.s3.AmazonS3ClientBuilder
+    import com.amazonaws.client.builder.AwsClientBuilder
+    import geotrellis.spark.io.s3.AmazonS3Client
+
+    def getByteReader(uri: String): StreamingByteReader = {
+      val endpoint = "https://oss.eu-west-0.prod-cloud-ocb.orange-business.com:443"
+      val region = "eu-west-0"
+      val accessKey = System.getProperty("aws.accessKeyId")
+      val secretKey = System.getProperty("aws.secretKey")
+
+      val s3Client = AmazonS3Client(
+        AmazonS3ClientBuilder
+          .standard()
+          .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, region))
+          .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+          .build()
+      )
+
+      val rr = S3RangeReader(uri, s3Client)
+      new StreamingByteReader(rr)
+    }
+
+    val rasterSource = new GeoTiffRasterSource("s3://s2-ndvi/cogs/S2A_MSIL1C_20180401T105031_N0206_R051_T31UES_20180401T144530.tiff") {
+      @transient override lazy val tiff: MultibandGeoTiff =
+        GeoTiffReader.readMultiband(getByteReader(uri), streaming = true)
+    }
+
+    val Some(Raster(multibandTile, extent)) = rasterSource.read(Extent(xmin = 516807.20, ymin = 5680949.70, xmax = 532827.11, ymax = 5695567.02))
+    MultibandGeoTiff(multibandTile, extent, rasterSource.crs)
+      .write(s"/tmp/s3.tif")
+  }
+}
