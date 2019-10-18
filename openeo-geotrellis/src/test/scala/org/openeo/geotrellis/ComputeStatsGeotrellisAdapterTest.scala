bc43fa99e9b001eff4e5438b81faae1eb4e5fa7f
Jeroen Dries <jeroen.dries@vito.be>
2019-10-18 14:49:30 +0200

    EP-3080 fix histogram computation for doubles

--- openeo-geotrellis/src/test/scala/org/openeo/geotrellis/ComputeStatsGeotrellisAdapterTest.scala
+++ openeo-geotrellis/src/test/scala/org/openeo/geotrellis/ComputeStatsGeotrellisAdapterTest.scala
@@ -6,7 +6,7 @@ import java.util
 
 import geotrellis.proj4.{LatLng, WebMercator}
 import geotrellis.raster.histogram.Histogram
-import geotrellis.raster.{ByteCells, ByteConstantTile, MultibandTile}
+import geotrellis.raster.{ByteCells, ByteConstantTile, DoubleConstantNoDataCellType, MultibandTile}
 import geotrellis.spark.tiling.LayoutDefinition
 import geotrellis.spark.util.SparkUtils
 import geotrellis.spark.{ContextRDD, Metadata, SpaceTimeKey, TileLayerMetadata}
@@ -138,6 +138,15 @@ object ComputeStatsGeotrellisAdapterTest {
       |            [7.022705078125007, 51.75432477678571]
       |    ]
       |  ]
+      |}
+      """.stripMargin.parseGeoJson[Polygon]()
+
+
+  private val polygon4 =
+    """
+      |{
+      |  "type": "Polygon",
+      |  "coordinates": [[[5.608215013735893, 51.032165152086264], [5.608271547800293, 51.03221788952301], [5.608276702058292, 51.03222293623101], [5.60849596808989, 51.032448502828444], [5.608716500868006, 51.032663757587436], [5.608825832560376, 51.032770514885485], [5.608945000622374, 51.032880067275485], [5.609034654329857, 51.032962466956384], [5.6108271092295565, 51.03200900180982], [5.6108243802523665, 51.032005046553195], [5.610765550277478, 51.031906904685506], [5.610730775512867, 51.03185731710986], [5.610728864439129, 51.03185450621938], [5.61068097777416, 51.031781806466725], [5.610648884176528, 51.0317343801373], [5.610647867072976, 51.03173285113396], [5.61056103219013, 51.031600028841], [5.610464592173136, 51.031620528338046], [5.610108166306372, 51.03169612090491], [5.610099694700648, 51.03169779497791], [5.6098587750999505, 51.031741960985435], [5.609583519929178, 51.03179242194306], [5.6095812925908355, 51.0317928220292], [5.609462795599489, 51.031813670192285], [5.609462557298256, 51.03181371202481], [5.6094265107381105, 51.0318200256364], [5.609420285041724, 51.03182105270213], [5.609335088450095, 51.03183424727698], [5.609264660589062, 51.03184521822665], [5.609258526965428, 51.03184611349288], [5.609158373400448, 51.03185975557609], [5.6091581558557895, 51.03185978513339], [5.609101330081307, 51.0318674864013], [5.6090929559728915, 51.031868511547074], [5.608913831137317, 51.031888110200775], [5.608906493782254, 51.031888830479645], [5.608726551315478, 51.03190448174909], [5.60871912778759, 51.031905044225965], [5.608609468745772, 51.0319121289672], [5.608538666998535, 51.031916736443236], [5.60851084566201, 51.03191739795041], [5.608487750205862, 51.0319169979628], [5.608215013735893, 51.032165152086264]]]
       |}
       """.stripMargin.parseGeoJson[Polygon]()
 }
@@ -286,9 +295,35 @@ class ComputeStatsGeotrellisAdapterTest(threshold:Int) {
     assertMedianComputedCorrectly(ndviDataCube, minDateString, minDate, maxDate, polygons)
   }
 
+  @Test
+  def compute_median_masked_ndvi_timeseries_on_accumulo_datacube(): Unit = {
+
+    val minDateString = "2018-04-21T00:00:00Z"
+    val maxDateString = "2018-04-21T02:00:00Z"
+    val minDate = ZonedDateTime.parse(minDateString)
+
+    val maxDate = ZonedDateTime.parse(maxDateString)
+
+    val polygons = Seq(polygon4)
+    val datacube= accumuloDataCube("CGS_SENTINEL2_RADIOMETRY_V102_EARLY", minDateString, maxDateString, polygons.envelope, "EPSG:4326")
+
+    val selectedBands = datacube.withContext(_.mapValues(_.subsetBands(1,3))).convert(DoubleConstantNoDataCellType)
+    val ndviProcess = TestOpenEOProcessScriptBuilder.createNormalizedDifferenceProcess
+    val processes = new OpenEOProcesses()
+    val ndviDataCube = processes.mapBandsGeneric(selectedBands, ndviProcess)//.withContext(_.mapValues(_.mapDouble(0)(pixel => 0.1)))
+
+    val mask = accumuloDataCube("S2_SCENECLASSIFICATION_PYRAMID_20190624", minDateString, maxDateString, polygons.envelope, "EPSG:4326")
+    val binaryMask = mask.withContext(_.mapValues( _.map(0)(pixel => if ( pixel < 5) 1 else 0)))
+
+    print(binaryMask.partitioner)
+
+    val maskedCube = processes.rasterMask(ndviDataCube,binaryMask,Double.NaN)
+    assertMedianComputedCorrectly(maskedCube, minDateString, minDate, maxDate, polygons)
+  }
+
   private def assertMedianComputedCorrectly(ndviDataCube: RDD[(SpaceTimeKey, MultibandTile)] with Metadata[TileLayerMetadata[SpaceTimeKey]], minDateString: String, minDate: ZonedDateTime, maxDate: ZonedDateTime, polygons: Seq[Polygon]) = {
     //alternative way to compute a histogram that works for this simple case
-    val histogram: Histogram[Double] = ndviDataCube.toSpatial(minDate).mask(polygon3.reproject(LatLng, WebMercator)).histogram(255)(0)
+    val histogram: Histogram[Double] = ndviDataCube.toSpatial(minDate).mask(polygons.map(_.reproject(LatLng, WebMercator))).histogram(1000)(0)
     print("MEDIAN: ")
     val expectedMedian = histogram.median()
     print(expectedMedian)
@@ -312,7 +347,7 @@ class ComputeStatsGeotrellisAdapterTest(threshold:Int) {
       .flatMap { case (_, dailyMeans) => dailyMeans.asScala }.filter(!_.isEmpty)
 
     assertTrue(means.exists(mean => !mean.get(0).isNaN))
-    assertEquals(stats(minDateString).get(0).get(0), expectedMedian.get, 0.1)
+    assertEquals( expectedMedian.get,stats(minDateString).get(0).get(0), 0.1)
   }
 
   @Test
