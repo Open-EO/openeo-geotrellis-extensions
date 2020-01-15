e1a3a156b6d4882d7aaca36e6ab0de3382b18310
Jan Van den bosch <jan.vandenbosch@hotmail.com>
2020-01-15 16:11:09 +0100

    EP-3190: SpacePartitioner backed by a SpaceTimeByMonthPartitioner for Accumulo RDDs + unit test

--- geotrellis-accumulo-extensions/src/main/scala/org/openeo/geotrellisaccumulo/GeotrellisRasterRDD.scala
+++ geotrellis-accumulo-extensions/src/main/scala/org/openeo/geotrellisaccumulo/GeotrellisRasterRDD.scala
@@ -1,19 +1,15 @@
 package org.openeo.geotrellisaccumulo
 
 import geotrellis.layer.{Metadata, SpaceTimeKey, TileLayerMetadata}
+import geotrellis.spark.partition.SpacePartitioner
 import geotrellis.spark.util.KryoWrapper
-import geotrellis.store.accumulo.AccumuloKeyEncoder
 import geotrellis.store.avro.codecs.KeyValueRecordCodec
 import geotrellis.store.avro.{AvroEncoder, AvroRecordCodec}
 import geotrellis.store.index.KeyIndex
-import org.apache.accumulo.core.client.mapreduce.impl.BatchInputSplit
-import org.apache.accumulo.core.data.Key
 import org.apache.avro.Schema
-import org.apache.hadoop.io.Text
 import org.apache.spark.rdd.RDD
-import org.apache.spark.{Partition, RangePartitioner, SparkContext, TaskContext}
+import org.apache.spark.{Partition, SparkContext, TaskContext}
 
-import scala.collection.JavaConverters._
 import scala.reflect.ClassTag
 
 class GeotrellisRasterRDD[V : AvroRecordCodec: ClassTag](keyIndex:KeyIndex[SpaceTimeKey],writerSchema:Schema,parent:GeotrellisAccumuloRDD,val metadata: TileLayerMetadata[SpaceTimeKey], sc : SparkContext) extends RDD[(SpaceTimeKey, V)](sc,Nil) with Metadata[TileLayerMetadata[SpaceTimeKey]] {
@@ -21,35 +17,15 @@ class GeotrellisRasterRDD[V : AvroRecordCodec: ClassTag](keyIndex:KeyIndex[Space
   val codec = KryoWrapper(KeyValueRecordCodec[SpaceTimeKey, V])
   val kwWriterSchema = KryoWrapper(Some(writerSchema))
 
-  override val partitioner: Option[org.apache.spark.Partitioner] = Some(new RangePartitioner[SpaceTimeKey,V](partitions.length,SparkContext.getOrCreate().emptyRDD[(SpaceTimeKey, V)]){
-    override def numPartitions: Int = {
-      partitions.length
-    }
-
-    override def getPartition(key: Any): Int = {
-      println(key)
-      val accumuloKey = new Key(new Text(AccumuloKeyEncoder.long2Bytes(keyIndex.toIndex(key.asInstanceOf[SpaceTimeKey]))))
-      println(accumuloKey)
-      val index = partitions.indexWhere(p => p.asInstanceOf[NewHadoopPartition].serializableHadoopSplit.value.asInstanceOf[BatchInputSplit].getRanges().asScala.exists( r => r.contains(accumuloKey)) )
-      if(index < 0) {
-        //cannot find this key in the partition, avoid breaking spark by returning -1
-        return 0
-      }
-      return index
-    }
-  })
+  override val partitioner: Option[org.apache.spark.Partitioner] = Some(SpacePartitioner(metadata.bounds))
 
   override def compute(split: Partition, context: TaskContext): Iterator[(SpaceTimeKey, V)] = {
     val parentIterator = parent.compute(split, context)
 
-    return parentIterator.map{ case (_, value) =>
+    parentIterator.map { case (_, value) =>
       AvroEncoder.fromBinary(kwWriterSchema.value.getOrElse(codec.value.schema), value.get)(codec.value)
-    }.flatMap { pairs: Vector[(SpaceTimeKey, V)] =>
-      pairs
-    }
+    }.flatten
   }
 
-  override protected def getPartitions: Array[Partition] = {
-    return parent.getPartitions
-  }
+  override protected def getPartitions: Array[Partition] = parent.getPartitions
 }
--- geotrellis-accumulo-extensions/src/main/scala/org/openeo/geotrellisaccumulo/package.scala
+++ geotrellis-accumulo-extensions/src/main/scala/org/openeo/geotrellisaccumulo/package.scala
@@ -2,7 +2,9 @@ package org.openeo
 
 import geotrellis.layer._
 import geotrellis.raster.MultibandTile
+import geotrellis.spark.partition.PartitionerIndex
 import geotrellis.spark.pyramid.Pyramid
+import geotrellis.store.index.zcurve.Z3
 import org.apache.spark.rdd.RDD
 
 import scala.collection.JavaConverters.mapAsScalaMapConverter
@@ -11,6 +13,15 @@ package object geotrellisaccumulo {
 
     def createGeotrellisPyramid(levels: java.util.Map[Integer,RDD[(SpaceTimeKey,MultibandTile)]  with Metadata[TileLayerMetadata[SpaceTimeKey]]]): Pyramid[SpaceTimeKey,MultibandTile,TileLayerMetadata[SpaceTimeKey]] ={
       val map: Map[Int, RDD[(SpaceTimeKey, MultibandTile)] with Metadata[TileLayerMetadata[SpaceTimeKey]]] = levels.asScala.toMap.map(t=>(t._1.toInt,t._2))
-      return new Pyramid(map)
+      new Pyramid(map)
     }
+
+  implicit object SpaceTimeByMonthPartitioner extends  PartitionerIndex[SpaceTimeKey] {
+    private def toZ(key: SpaceTimeKey): Z3 = Z3(key.col >> 4, key.row >> 4, 13*key.time.getYear + key.time.getMonthValue)
+
+    def toIndex(key: SpaceTimeKey): BigInt = toZ(key).z
+
+    def indexRanges(keyRange: (SpaceTimeKey, SpaceTimeKey)): Seq[(BigInt, BigInt)] =
+      Z3.zranges(toZ(keyRange._1), toZ(keyRange._2))
+  }
 }
