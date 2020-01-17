18793a370a324b23493577015d8700707dd22430
Jeroen Dries <jeroen.dries@vito.be>
2020-01-17 23:00:29 +0100

    EP-3190 try creating single spacepartitioner for everything, that also works for accumulo

--- geotrellis-accumulo-extensions/src/main/scala/org/openeo/geotrellisaccumulo/GeotrellisRasterRDD.scala
+++ geotrellis-accumulo-extensions/src/main/scala/org/openeo/geotrellisaccumulo/GeotrellisRasterRDD.scala
@@ -1,15 +1,25 @@
 package org.openeo.geotrellisaccumulo
 
+import java.time.format.DateTimeFormatter
+import java.util
+
 import geotrellis.layer.{Metadata, SpaceTimeKey, TileLayerMetadata}
 import geotrellis.spark.partition.SpacePartitioner
 import geotrellis.spark.util.KryoWrapper
+import geotrellis.store.accumulo.AccumuloKeyEncoder
 import geotrellis.store.avro.codecs.KeyValueRecordCodec
 import geotrellis.store.avro.{AvroEncoder, AvroRecordCodec}
 import geotrellis.store.index.KeyIndex
+import org.apache.accumulo.core.client.mapreduce.impl.BatchInputSplit
+import org.apache.accumulo.core.data
+import org.apache.accumulo.core.data.Key
 import org.apache.avro.Schema
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{Partition, SparkContext, TaskContext}
+import org.openeo.geotrellisaccumulo
 
+import scala.collection.JavaConverters._
+import scala.collection.mutable
 import scala.reflect.ClassTag
 
 class GeotrellisRasterRDD[V : AvroRecordCodec: ClassTag](keyIndex:KeyIndex[SpaceTimeKey],writerSchema:Schema,parent:GeotrellisAccumuloRDD,val metadata: TileLayerMetadata[SpaceTimeKey], sc : SparkContext) extends RDD[(SpaceTimeKey, V)](sc,Nil) with Metadata[TileLayerMetadata[SpaceTimeKey]] {
@@ -27,5 +37,60 @@ class GeotrellisRasterRDD[V : AvroRecordCodec: ClassTag](keyIndex:KeyIndex[Space
     }.flatten
   }
 
-  override protected def getPartitions: Array[Partition] = parent.getPartitions
+  override protected def getPartitions: Array[Partition] = {
+    val myPartitions = parent.getPartitions
+    val myRegions = partitioner.get.asInstanceOf[SpacePartitioner[SpaceTimeKey]].regions
+
+    var splitsForRegions = mutable.Seq[BatchInputSplit]()
+    for (region <- myRegions){
+      val startKey = geotrellisaccumulo.decodeIndexKey(region)
+      val endKey = geotrellisaccumulo.decodeIndexKey(region+1)
+      // assert(region+1==geotrellisaccumulo.SpaceTimeByMonthPartitioner.toIndex(endKey))
+      println("Region: " + region + " date: " + DateTimeFormatter.BASIC_ISO_DATE.format(startKey.time) + " enddate: " + DateTimeFormatter.BASIC_ISO_DATE.format(endKey.time))
+
+      var newSplit:BatchInputSplit = null
+      for(partition <- myPartitions) {
+        var rangesForRegion = mutable.Seq[data.Range]()
+        val inputSplit = partition.asInstanceOf[NewHadoopPartition].serializableHadoopSplit.value.asInstanceOf[BatchInputSplit]
+
+        var rangeIdx = 0
+        var indices = ""
+        for(range <- inputSplit.getRanges().toArray){
+
+          val theRange = range.asInstanceOf[data.Range]
+          val start = new Key(AccumuloKeyEncoder.index2RowId(keyIndex.toIndex(startKey)))
+          val end = new Key(AccumuloKeyEncoder.index2RowId(keyIndex.toIndex(endKey)))
+          if(!theRange.beforeStartKey(end) && (! theRange.afterEndKey(start) || theRange.getEndKey == start )){
+            indices += (rangeIdx + ", ")
+            rangesForRegion = rangesForRegion :+ theRange
+          }
+          rangeIdx += 1
+        }
+        if(indices.length>0){
+          println("Partition: " + partition.index + " idx: " + indices)
+          if(newSplit==null){
+            newSplit = new BatchInputSplit(inputSplit.getTable,inputSplit.getTableId,new util.ArrayList(rangesForRegion.asJavaCollection),inputSplit.getLocations)
+            newSplit.setInstanceName(inputSplit.getInstanceName)
+            newSplit.setZooKeepers(inputSplit.getZooKeepers)
+            newSplit.setMockInstance(false)
+            newSplit.setPrincipal(inputSplit.getPrincipal)
+            newSplit.setToken(inputSplit.getToken)
+            newSplit.setAuths(inputSplit.getAuths)
+            newSplit.setFetchedColumns(inputSplit.getFetchedColumns)
+            newSplit.setIterators(inputSplit.getIterators)
+            newSplit.setLogLevel(inputSplit.getLogLevel)
+            splitsForRegions = splitsForRegions :+ newSplit
+          }else{
+
+            newSplit.getRanges.addAll(rangesForRegion.asJavaCollection)
+          }
+        }
+      }
+
+    }
+    var i = -1
+    splitsForRegions.seq.map(split => {
+      i+=1
+      new NewHadoopPartition(id, i, split)}).toArray[Partition]
+  }
 }
--- geotrellis-accumulo-extensions/src/main/scala/org/openeo/geotrellisaccumulo/package.scala
+++ geotrellis-accumulo-extensions/src/main/scala/org/openeo/geotrellisaccumulo/package.scala
@@ -4,7 +4,7 @@ import geotrellis.layer._
 import geotrellis.raster.MultibandTile
 import geotrellis.spark.partition.PartitionerIndex
 import geotrellis.spark.pyramid.Pyramid
-import geotrellis.store.index.zcurve.Z3
+import geotrellis.store.index.zcurve.{Z3, ZSpaceTimeKeyIndex}
 import org.apache.spark.rdd.RDD
 
 import scala.collection.JavaConverters.mapAsScalaMapConverter
@@ -16,12 +16,60 @@ package object geotrellisaccumulo {
       new Pyramid(map)
     }
 
-  implicit object SpaceTimeByMonthPartitioner extends  PartitionerIndex[SpaceTimeKey] {
-    private def toZ(key: SpaceTimeKey): Z3 = Z3(key.col >> 4, key.row >> 4, 13*key.time.getYear + key.time.getMonthValue)
+  def decodeIndexKey(region:BigInt):SpaceTimeKey = {
+    val (x,y,t) = new Z3(region.longValue() << 8 ).decode
+
+    new SpaceTimeKey(x,y,t*1000L * 60 * 60 * 24 )
+  }
+  /*
+  def decodeIndexKey(region:BigInt):SpaceTimeKey = {
+    val (x,y,t) = new Z3(region.longValue()).decode
+    val day = t%31
+    val month = ((t-day)/31)%13
+    val year = ((t - day)/31-month)/13
+    SpaceTimeKey(x <<4,y << 4,ZonedDateTime.of(LocalDate.of(year,month,day+1),LocalTime.MIDNIGHT,ZoneId.of("UTC")))
+  }*/
+
+  /*implicit object SpaceTimeByMonthPartitioner extends  PartitionerIndex[SpaceTimeKey] {
+    private def toZ(key: SpaceTimeKey): Z3 = Z3(key.col >> 4, key.row >> 4, 31*13*key.time.getYear + 31*key.time.getMonthValue + key.time.getDayOfMonth-1)
 
     def toIndex(key: SpaceTimeKey): BigInt = toZ(key).z
 
     def indexRanges(keyRange: (SpaceTimeKey, SpaceTimeKey)): Seq[(BigInt, BigInt)] =
       Z3.zranges(toZ(keyRange._1), toZ(keyRange._2))
+  }*/
+  implicit object SpaceTimeByMonthPartitioner extends  PartitionerIndex[SpaceTimeKey] {
+
+    val keyIndex = ZSpaceTimeKeyIndex.byDay(null)
+    //private def toZ(key: SpaceTimeKey): Z3 = Z3(key.col , key.row , 31*13*key.time.getYear + 31*key.time.getMonthValue + key.time.getDayOfMonth-1)
+
+    def toIndex(key: SpaceTimeKey): BigInt = keyIndex.toIndex(key) >> 8
+
+    def indexRanges(keyRange: (SpaceTimeKey, SpaceTimeKey)): Seq[(BigInt, BigInt)] = {
+      val originalRanges = keyIndex.indexRanges(keyRange)
+      val mappedRanges = originalRanges.map(range => (range._1 >> 8,range._2 >> 8 ))
+      val distinct = mappedRanges.distinct
+      var previousEnd: BigInt = null
+      var wasSingleValue: Boolean = false
+
+      //filter out regions that only span 1 value, and are already included in another region, so basically duplicates
+      val filtered = distinct.filter(range => {
+        try{
+          if(range._1 == previousEnd && range._1 == range._2) {
+            false
+          }else if(wasSingleValue && range._1 ==previousEnd) {
+            false
+          }else{
+            true
+          }
+        }finally {
+          previousEnd = range._2
+          wasSingleValue = range._2 == range._1
+        }
+
+      })
+      return filtered
+    }
+
   }
 }
