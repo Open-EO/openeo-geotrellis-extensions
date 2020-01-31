07ec18b02aa23176ab83c287e6317e08855d2b2c
Jeroen Dries <jeroen.dries@vito.be>
2020-01-31 20:56:42 +0100

    EP-3190 merge from develop reverted some changes

--- geotrellis-accumulo-extensions/src/main/scala/org/openeo/geotrellisaccumulo/GeotrellisRasterRDD.scala
+++ geotrellis-accumulo-extensions/src/main/scala/org/openeo/geotrellisaccumulo/GeotrellisRasterRDD.scala
@@ -29,23 +29,7 @@ class GeotrellisRasterRDD[V : AvroRecordCodec: ClassTag](keyIndex:KeyIndex[Space
   val codec = KryoWrapper(KeyValueRecordCodec[SpaceTimeKey, V])
   val kwWriterSchema = KryoWrapper(Some(writerSchema))
 
-  override val partitioner: Option[org.apache.spark.Partitioner] = Some(new RangePartitioner[SpaceTimeKey,V](partitions.length,SparkContext.getOrCreate().emptyRDD[(SpaceTimeKey, V)]){
-    override def numPartitions: Int = {
-      partitions.length
-    }
-
-    override def getPartition(key: Any): Int = {
-      println(key)
-      val accumuloKey = new Key(new Text(AccumuloKeyEncoder.long2Bytes(keyIndex.toIndex(key.asInstanceOf[SpaceTimeKey]))))
-      println(accumuloKey)
-      val index = partitions.indexWhere(p => p.asInstanceOf[NewHadoopPartition].serializableHadoopSplit.value.asInstanceOf[BatchInputSplit].getRanges().asScala.exists( r => r.contains(accumuloKey)) )
-      if(index < 0) {
-        //cannot find this key in the partition, avoid breaking spark by returning -1
-        return 0
-      }
-      return index
-    }
-  })
+  override val partitioner: Option[org.apache.spark.Partitioner] = Some(SpacePartitioner(metadata.bounds))
 
   override def compute(split: Partition, context: TaskContext): Iterator[(SpaceTimeKey, V)] = {
     val parentIterator = parent.compute(split, context)
