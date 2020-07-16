6a0ffda342e8ce197e20654583d8735be72cd4c6
Jeroen Dries <jeroen.dries@vito.be>
2020-07-16 09:21:13 +0200

    make cropped tiles mutable again

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/OpenEOProcesses.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/OpenEOProcesses.scala
@@ -212,7 +212,7 @@ class OpenEOProcesses extends Serializable {
   }
 
   def remove_overlap(datacube: MultibandTileLayerRDD[SpaceTimeKey], sizeX:Int, sizeY:Int, overlapX:Int, overlapY:Int): MultibandTileLayerRDD[SpaceTimeKey] = {
-    datacube.withContext(_.mapValues(_.crop(overlapX,overlapY,sizeX,sizeY)))
+    datacube.withContext(_.mapValues(_.crop(overlapX,overlapY,sizeX,sizeY).toArrayTile()))
   }
 
   def retile(datacube: MultibandTileLayerRDD[SpaceTimeKey], sizeX:Int, sizeY:Int, overlapX:Int, overlapY:Int): MultibandTileLayerRDD[SpaceTimeKey] = {
