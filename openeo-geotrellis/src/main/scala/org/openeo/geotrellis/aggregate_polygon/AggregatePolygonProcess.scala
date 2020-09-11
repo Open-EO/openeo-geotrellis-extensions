bc209be3ce61cd500208ebbe0d71836f41101c13
Jeroen Dries <jeroen.dries@vito.be>
2020-09-11 11:56:48 +0200

    EP-3406 remove dependency on statsmeanresult

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/aggregate_polygon/AggregatePolygonProcess.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/aggregate_polygon/AggregatePolygonProcess.scala
@@ -3,9 +3,6 @@ package org.openeo.geotrellis.aggregate_polygon
 import java.time.ZonedDateTime
 import java.time.temporal.ChronoUnit.DAYS
 
-import be.vito.eodata.extracttimeseries.geotrellis.ComputeStatsGeotrellis.splitOverlappingPolygons
-import be.vito.eodata.extracttimeseries.geotrellis._
-import be.vito.eodata.processing.MaskedStatisticsProcessor.StatsMeanResult
 import geotrellis.layer.{LayoutDefinition, Metadata, SpaceTimeKey, SpatialKey, TemporalKey}
 import geotrellis.proj4.CRS
 import geotrellis.raster.{MultibandTile, Tile}
@@ -16,17 +13,17 @@ import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import org.openeo.geotrellis.SpatialToSpacetimeJoinRdd
 import org.openeo.geotrellis.aggregate_polygon.intern.PixelRateValidator.exceedsTreshold
-import org.openeo.geotrellis.aggregate_polygon.intern.{CancellationContext, MeanResult, StatisticsCallback, ZonalRunningTotal}
+import org.openeo.geotrellis.aggregate_polygon.intern._
 import org.openeo.geotrellis.layers.LayerProvider
 
 object AggregatePolygonProcess {
   private type PolygonsWithIndexMapping = (Seq[MultiPolygon], Seq[Set[Int]])
 }
 
-class AggregatePolygonProcess(val layersConfig: LayersConfig=null) {
+class AggregatePolygonProcess() {
   import AggregatePolygonProcess._
 
-  def computeAverageTimeSeries(datacube: MultibandTileLayerRDD[SpaceTimeKey], polygons: Array[MultiPolygon], crs: CRS, startDate: ZonedDateTime, endDate: ZonedDateTime, statisticsCallback: StatisticsCallback[_ >: Seq[StatsMeanResult]], cancellationContext: CancellationContext, sc: SparkContext): Unit = {
+  def computeAverageTimeSeries(datacube: MultibandTileLayerRDD[SpaceTimeKey], polygons: Array[MultiPolygon], crs: CRS, startDate: ZonedDateTime, endDate: ZonedDateTime, statisticsCallback: StatisticsCallback[_ >: Seq[MeanResult]], cancellationContext: CancellationContext, sc: SparkContext): Unit = {
 
 
     val extent = sc.parallelize(polygons).map(_.extent).reduce(_.combine(_))
@@ -72,8 +69,8 @@ class AggregatePolygonProcess(val layersConfig: LayersConfig=null) {
 
       dates.foreach(d => statisticsCallback.onComputed(d, {
         val maybeResultses: Array[Option[Array[MeanResult]]] = results.map(_.get(d))
-        val converted: Seq[Seq[_ <: StatsMeanResult]] = maybeResultses.map {
-          case Some(r) =>r.map{meanResult => new StatsMeanResult(meanResult.meanPhysical, meanResult.total, meanResult.valid)}.toSeq
+        val converted: Seq[Seq[_ <: MeanResult]] = maybeResultses.map {
+          case Some(r) =>r.map{meanResult => new MeanResult(meanResult.sum, meanResult.valid, meanResult.total,Option.empty,Option.empty)}.toSeq
           case None => Seq.empty
         }
         converted
@@ -87,7 +84,7 @@ class AggregatePolygonProcess(val layersConfig: LayersConfig=null) {
   def computeMultibandCollectionTimeSeries(datacube : MultibandTileLayerRDD[SpaceTimeKey], polygonsWithIndexMapping: PolygonsWithIndexMapping, crs: CRS, startDate: ZonedDateTime, endDate: ZonedDateTime, statisticsCallback: StatisticsCallback[_ >: Seq[StatsMeanResult]], cancellationContext: CancellationContext, sc: SparkContext): Unit =
     computeMultibandCollectionTimeSeries(datacube, polygonsWithIndexMapping, crs, startDate, endDate, statisticsCallback, sc, denseMeansMultiBand, cancellationContext)
 */
-  private def computeMultibandCollectionTimeSeries(datacube : MultibandTileLayerRDD[SpaceTimeKey], polygonsWithIndexMapping: PolygonsWithIndexMapping, crs: CRS, startDate: ZonedDateTime, endDate: ZonedDateTime, statisticsCallback: StatisticsCallback[_ >: Seq[StatsMeanResult]], sc: SparkContext,
+  private def computeMultibandCollectionTimeSeries(datacube : MultibandTileLayerRDD[SpaceTimeKey], polygonsWithIndexMapping: PolygonsWithIndexMapping, crs: CRS, startDate: ZonedDateTime, endDate: ZonedDateTime, statisticsCallback: StatisticsCallback[_ >: Seq[MeanResult]], sc: SparkContext,
                                                       cancellationContext: CancellationContext): Unit = {
     import org.apache.spark.storage.StorageLevel._
 
@@ -106,21 +103,21 @@ class AggregatePolygonProcess(val layersConfig: LayersConfig=null) {
       val zonalStats = combinedRDD.flatMap { case (date, (t1:MultibandTile, t2:Tile)) => ZonalRunningTotal(t1, t2).map(index_total => ((date.time,index_total._1),index_total._2)).filterKeys(_._2>=0) }
         .reduceByKey((a,b) =>a.zip(b).map({ case (total_a,total_b) => total_a + total_b})).collectAsMap()
 
-      val statsByDate = zonalStats.groupBy(_._1._1)
+      val statsByDate: Map[ZonedDateTime, collection.Map[(ZonedDateTime, Int), Seq[RunningTotal]]] = zonalStats.groupBy(_._1._1)
 
       for (stats <- statsByDate){
         val date = stats._1
-        val valuesForDate: collection.Map[Int, Seq[StatsMeanResult]] = stats._2.map(t=>(t._1._2,t._2.map{ meanResult => new StatsMeanResult(meanResult.mean.getOrElse(Double.NaN), meanResult.totalCount, meanResult.validCount)
+        val valuesForDate: collection.Map[Int, Seq[MeanResult]] = stats._2.map(t=>(t._1._2,t._2.map{ meanResult: RunningTotal => new MeanResult(meanResult.validSum, meanResult.validCount, meanResult.totalCount,Option.empty,Option.empty)
         }))
 
         //the map might not contain results for all features, it is sparse, so we have to make it dense
         //we also have to merge results of features that were splitted because of overlap
         val denseResults = indexMapping.map { splitIndices =>
-          val empty = new StatsMeanResult(Double.NaN, 0L, 0L)
+          val empty = new MeanResult(Double.NaN, 0L, 0L,Option.empty,Option.empty)
 
           splitIndices.flatMap(valuesForDate.get).fold(Seq.empty) { (denseMean1, denseMean2) =>
             val meansPairedByBand = denseMean1.zipAll(denseMean2, empty, empty)
-            meansPairedByBand.map { case (leftMean, rightMean) => leftMean merge rightMean }
+            meansPairedByBand.map { case (leftMean, rightMean) => leftMean + rightMean }
           }
         }
 
