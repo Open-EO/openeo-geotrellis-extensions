f1975d5ac9faa28b9e57240a71834ee008cdbe6e
Tamas Banyai <tamas.banyai@vito.be>
2020-09-26 23:51:53 +0200

    EP-3574 added scaling options

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/icor/AtmosphericCorrection.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/icor/AtmosphericCorrection.scala
@@ -3,7 +3,8 @@ package org.openeo.geotrellis.icor
 import org.apache.spark.api.java.JavaSparkContext
 import geotrellis.layer._
 import geotrellis.spark._
-import geotrellis.raster.MultibandTile
+import geotrellis.raster.{MultibandTile,NODATA}
+import java.util.ArrayList
 
 
 
@@ -15,47 +16,50 @@ class AtmosphericCorrection {
   }
   
      
-  def correct(jsc: JavaSparkContext, datacube: MultibandTileLayerRDD[SpaceTimeKey], tableId: String): ContextRDD[SpaceTimeKey, MultibandTile, TileLayerMetadata[SpaceTimeKey]]  = {
-    // TODO: mechanism to map bands properly
-    val sc = JavaSparkContext.toSparkContext(jsc)
-    val lut=LookupTableIO.readLUT(tableId)
-    val bcLUT = sc.broadcast(lut)
-    new ContextRDD(
-      datacube.map(multibandtile => (
-        multibandtile._1,
-//        multibandtile._2.mapBands((b, tile) => tile.map(i => 23 ))
-        multibandtile._2.mapBands((b, tile) => {
-          val cd=new CorrectionDescriptor()
-          tile.map(i => cd.correct(bcLUT.value, b, i.toDouble, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0).toInt) 
-        })
-      )),
-      datacube.metadata
-    )
+  def correct(
+        jsc: JavaSparkContext, 
+        datacube: MultibandTileLayerRDD[SpaceTimeKey], 
+        tableId: String, 
+        bandIds:ArrayList[String],
+        prePostMult:ArrayList[Double],
+        defParams:ArrayList[Double]
+      ): ContextRDD[SpaceTimeKey, MultibandTile, TileLayerMetadata[SpaceTimeKey]]  = {
+    
+    // TODO: get bandids by name
+    
+//    var retRDD: ContextRDD[SpaceTimeKey, MultibandTile, TileLayerMetadata[SpaceTimeKey]] = new ContextRDD(datacube,datacube.metadata)
+//    try {
+
+      val sc = JavaSparkContext.toSparkContext(jsc)
+      val lut=LookupTableIO.readLUT(tableId)
+      val bcLUT = sc.broadcast(lut)
+//      print("**** NODAT: "+NODATA.getClass().toString()+":"+NODATA)
+      new ContextRDD(
+        datacube.map(multibandtile => (
+          multibandtile._1,
+//          multibandtile._2.mapBands((b, tile) => tile.map(i => 23 ))
+          multibandtile._2.mapBands((b, tile) => {
+            val cd=new CorrectionDescriptorSentinel2()
+            val iband=cd.getBandFromName(bandIds.get(b))
+            tile.map(i => if (i!=NODATA) (prePostMult.get(1)*cd.correct(bcLUT.value, iband, multibandtile._1.instant, i.toDouble*prePostMult.get(0), defParams.get(0), defParams.get(1), defParams.get(2), defParams.get(3), defParams.get(4), defParams.get(5), defParams.get(6), 0)).toInt else NODATA ) 
+//            tile.map(i => cd.correct(bcLUT.value, b, i.toDouble, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0).toInt ) 
+          })
+        )),
+        datacube.metadata
+      )
+
+//      println("!!!!!!!!!!!!! "+retRDD.getClass().toString())
+//      retRDD
+//    } catch {
+//      case e: Exception => { 
+//        println("***********************************\n"+e+"\n"+e.getStackTrace+"\n")
+//        throw e
+//      }
+//    }
+      
+      
   }
  
 }
 
-//  def correct(datacube: MultibandTileLayerRDD[SpaceTimeKey]): ContextRDD[SpaceTimeKey, MultibandTile, TileLayerMetadata[SpaceTimeKey]]  = {
-//      println("Hello, world from inside scala correct!")
-//      val icor=new AtmosphericCorrectionICOR()
-//      new ContextRDD(
-//          datacube.map(multibandtile => (
-//              multibandtile._1,
-//              multibandtile._2.mapBands((b, tile) => icor.correct(b,tile))
-//          )),
-//          datacube.metadata
-//      )
-//  }
-
-
-        //  println("*** SCALA MAP *********************************************")
-          //println(bcICOR.getClass().toString())
-          //println(bcICOR.value.getClass().toString())
-          //bcICOR.value.correctPixel(i)
-        //  14
-        //}
-//    val bcICOR = SparkContext.getOrCreate().broadcast(new AtmosphericCorrectionICOR())
-//    println(bcICOR.getClass().toString())
-//    println(bcICOR.value.getClass().toString())
-//    println("*** SCALA ICOR DONE *********************************************")
 
