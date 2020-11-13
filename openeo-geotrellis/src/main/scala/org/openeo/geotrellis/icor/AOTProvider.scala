28d4f555608dc29a07255f0c9b1f4cf4699d1ae7
Jeroen Dries <jeroen.dries@vito.be>
2020-11-13 12:16:30 +0100

    EP-3602 EP-3603 add AOT provider that works on Terrascope, use in icor

---
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/icor/AOTProvider.scala
@@ -0,0 +1,20 @@
+package org.openeo.geotrellis.icor
+
+import geotrellis.layer.{LayoutDefinition, SpaceTimeKey}
+import geotrellis.proj4.CRS
+import geotrellis.raster.Tile
+import geotrellis.raster.geotiff.GeoTiffRasterSource
+
+class AOTProvider(basePath:String = "/data/MEP/ECMWF/cams/aod550/%1$tY/%1$tm/CAMS_NRT_aod550_%1$tY%1$tm%1$tdT120000Z.tif") {
+
+  def computeAOT(key:SpaceTimeKey, targetCRS:CRS,layoutDefinition:LayoutDefinition): Tile = {
+    val aotPath = basePath.format(key.time)
+    val rasterSource = GeoTiffRasterSource(aotPath)
+
+    val targetExtent = layoutDefinition.mapTransform.apply(key)
+    val raster = rasterSource.reprojectToGrid(targetCRS,layoutDefinition).read(targetExtent)
+    raster.get.tile.band(0)
+
+  }
+
+}
--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/icor/AtmosphericCorrection.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/icor/AtmosphericCorrection.scala
@@ -1,9 +1,9 @@
 package org.openeo.geotrellis.icor
 
-import org.apache.spark.api.java.JavaSparkContext
 import geotrellis.layer._
+import geotrellis.raster.{MultibandTile, NODATA, Tile}
 import geotrellis.spark._
-import geotrellis.raster.{MultibandTile,NODATA}
+import org.apache.spark.api.java.JavaSparkContext
 
 
 
@@ -27,16 +27,30 @@ class AtmosphericCorrection {
       val sc = JavaSparkContext.toSparkContext(jsc)
       val lut=LookupTableIO.readLUT(tableId)
       val bcLUT = sc.broadcast(lut)
+
+      val crs = datacube.metadata.crs
+      val layoutDefinition = datacube.metadata.layout
+
       new ContextRDD(
-        datacube.map(multibandtile => (
-          multibandtile._1,
-//          multibandtile._2.mapBands((b, tile) => tile.map(i => 23 ))
-          multibandtile._2.mapBands((b, tile) => {
-            val cd=new CorrectionDescriptorSentinel2()
-            val iband=cd.getBandFromName(bandIds.get(b))
-            tile.map(i => if (i!=NODATA) (prePostMult.get(1)*cd.correct(bcLUT.value, iband, multibandtile._1.instant, i.toDouble*prePostMult.get(0), defParams.get(0), defParams.get(1), defParams.get(2), defParams.get(3), defParams.get(4), defParams.get(5), defParams.get(6), 0)).toInt else NODATA ) 
-          })
-        )),
+        datacube.mapPartitions(partition => {
+          val aotProvider = new AOTProvider()
+          partition.map {
+            multibandtile =>
+              (
+                multibandtile._1,
+                //          multibandtile._2.mapBands((b, tile) => tile.map(i => 23 ))
+                multibandtile._2.mapBands((b, tile) => {
+                  val cd = new CorrectionDescriptorSentinel2()
+                  val iband = cd.getBandFromName(bandIds.get(b))
+
+                  val aotTile = aotProvider.computeAOT(multibandtile._1,crs,layoutDefinition)
+                  val resultTile:Tile = MultibandTile(tile,aotTile.convert(tile.cellType)).combine(0,1){(refl,aot) => if (refl != NODATA) (prePostMult.get(1) * cd.correct(bcLUT.value, iband, multibandtile._1.instant, refl.toDouble * prePostMult.get(0), defParams.get(0), defParams.get(1), defParams.get(2), defParams.get(3),aot, defParams.get(5),defParams.get(6),0)).toInt else NODATA}
+                  resultTile
+                })
+              )
+          }
+
+        }),
         datacube.metadata
       )
 
