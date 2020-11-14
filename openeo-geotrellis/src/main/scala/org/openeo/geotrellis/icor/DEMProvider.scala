28ea2a51bc2ea589f1251a236ecee1b65a38bfe8
Jeroen Dries <jeroen.dries@vito.be>
2020-11-14 07:53:21 +0100

    EP-3603 make dem location independent of infrastructure

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/icor/DEMProvider.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/icor/DEMProvider.scala
@@ -5,7 +5,7 @@ import geotrellis.proj4.CRS
 import geotrellis.raster.Tile
 import geotrellis.raster.geotiff.GeoTiffRasterSource
 
-class DEMProvider(layout:LayoutDefinition, crs:CRS,path:String="/data/MEP/DEM/DEM_NOAA_Globe/DEM_Globe_CS.tif") {
+class DEMProvider(layout:LayoutDefinition, crs:CRS,path:String="https://artifactory.vgt.vito.be/auxdata-public/DEM/DEM_Globe_CS.tif") {
   val rasterSource = GeoTiffRasterSource(path).reprojectToRegion(crs,layout.toRasterExtent())
 
   def computeDEM(key:SpaceTimeKey, targetCRS:CRS,layoutDefinition:LayoutDefinition): Tile = {
