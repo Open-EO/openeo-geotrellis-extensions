14ad3a0f8e45408fa315abcebbba6cbe6ec081f2
Tamas Banyai <tamas.banyai@vito.be>
2021-01-18 18:52:27 +0100

    EP-3693 getting around serialization issue

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/water_vapor/ComputeWaterVapor.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/water_vapor/ComputeWaterVapor.scala
@@ -61,6 +61,7 @@ class ComputeWaterVapor {
       datacube.mapPartitions(partition => {
 // AOT overriden        val aotProvider = new AOTProvider()
         val demProvider = new DEMProvider(layoutDefinition, crs)
+        val cwvProvider = new CWVProvider()
         
         partition.map {
           multibandtile =>
@@ -99,7 +100,7 @@ class ComputeWaterVapor {
                 val afterAuxData = System.currentTimeMillis()
                 auxDataAccum.add(afterAuxData-startMillis)
                 
-                val result=MultibandTile(compute(
+                val result=MultibandTile(cwvProvider.compute(
                   multibandtile,
                   szaTile,
                   vzaTile,
@@ -124,66 +125,7 @@ class ComputeWaterVapor {
       datacube.metadata
     )
 
-  }
- 
-  def compute(
-    multibandtile: (SpaceTimeKey, MultibandTile), // where is wv/r0/r1
-    szaTile: Tile, 
-    vzaTile: Tile, 
-    raaTile: Tile,
-    elevationTile: Tile,
-    aot: Double,
-    ozone: Double,
-    preMult: Double,
-    postMult: Double,
-    bcLUT: Broadcast[LookupTable],
-    bandIds:java.util.List[String]
-  ) : Tile = {
-
-    val wvBandId="B09"
-    val r0BandId="B8A"
-    val r1BandId="B11"
-    
-    val cd = new CorrectionDescriptorSentinel2()
-    val bp = new BlockProcessor()
-    val wvCalc = new AbdaWaterVaporCalculator()
-    
-    wvCalc.prepare(bcLUT.value,cd,wvBandId,r0BandId,r1BandId)
-            
-    // TODO: use reflToRad(double src, double sza, ZonedDateTime time, int bandToConvert)
-    val wvTile= multibandtile._2.band(wvCalc.findIndexOf(bandIds,wvBandId)).convert(FloatConstantNoDataCellType)*preMult
-    val r0Tile= multibandtile._2.band(wvCalc.findIndexOf(bandIds,r0BandId)).convert(FloatConstantNoDataCellType)*preMult
-    val r1Tile= multibandtile._2.band(wvCalc.findIndexOf(bandIds,r1BandId)).convert(FloatConstantNoDataCellType)*preMult
-                        
-    // try to get at least 1 valid value on 60m resolution
-    val mbtresult : Tile = try { 
-      val wvRawResultTile=bp.computeDoubleBlocks(
-        MultibandTile(
-          szaTile, 
-          vzaTile, 
-          raaTile,
-          elevationTile.convert(FloatConstantNoDataCellType), 
-    // AOT overriden                      aotTile.convert(FloatConstantNoDataCellType), 
-          wvTile.convert(FloatConstantNoDataCellType), 
-          r0Tile.convert(FloatConstantNoDataCellType), 
-          r1Tile.convert(FloatConstantNoDataCellType)
-        ),
-        6, // on 10m base resolution looking for a value on 60m resolution
-        aot,
-        ozone,
-        doubleNODATA,
-        bcLUT.value,
-        wvCalc
-      )*postMult
-      bp.replaceNoDataWithAverage(wvRawResultTile,doubleNODATA)
-    } catch {
-      case e: IllegalArgumentException => wvTile
-    }
-    
-    mbtresult.convert(FloatConstantNoDataCellType)
-   
- }
-  
+  }  
   
 }
 
