7ae6032bdac9fe609de57b2096b52d72bd8e7c17
Tamas Banyai <tamas.banyai@vito.be>
2021-01-18 18:36:47 +0100

    EP-3693 refactoring, no compile errors

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/water_vapor/ComputeWaterVapor.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/water_vapor/ComputeWaterVapor.scala
@@ -69,6 +69,8 @@ class ComputeWaterVapor {
               //          multibandtile._2.mapBands((b, tile) => tile.map(i => 23 ))
               {
 
+                val startMillis = System.currentTimeMillis();
+                
                 def angleTile(index: Int, fallback: Double): Tile = {
                   if (index > 0) multibandtile._2.band(index).convert(FloatConstantNoDataCellType) else FloatConstantTile(fallback.toFloat, multibandtile._2.cols, multibandtile._2.rows)
                 }
@@ -79,21 +81,41 @@ class ComputeWaterVapor {
                 val vaaIdx = bandIds.indexOf("viewAzimuthMean")
                 
                 val szaTile = angleTile(szaIdx, defParams.get(0))
-                val saaTile = angleTile(saaIdx, defParams.get(1))// 130.0) // TODO: why is the fallback hardcoded to this value?
+                val saaTile = angleTile(saaIdx, defParams.get(1))
                 val vzaTile = angleTile(vzaIdx, defParams.get(2))
-                val vaaTile = angleTile(vaaIdx, defParams.get(3))// 0.0)   // TODO: why is the fallback hardcoded to this value?
+                val vaaTile = angleTile(vaaIdx, defParams.get(3))
                 
                 val raaTileDiff = saaTile - vaaTile
                 val raaTile=raaTileDiff.mapDouble(v =>
                   ( if (v < -180.0) v+360.0 else if ( v > 180.0) v-360.0 else v ).abs  
                 )
+
+                val demTile = demProvider.compute(multibandtile._1, crs, layoutDefinition)
+
+                // AOT overriden                val aotTile = aotProvider.computeAOT(multibandtile._1, crs, layoutDefinition)
+                val aot=defParams.get(4)
+                val ozone=defParams.get(5)
+
+                val afterAuxData = System.currentTimeMillis()
+                auxDataAccum.add(afterAuxData-startMillis)
                 
-                compute(
+                val result=MultibandTile(compute(
+                  multibandtile,
                   szaTile,
                   vzaTile,
-                  raaTile
+                  raaTile,
+                  demTile,
+                  aot,
+                  ozone,
+                  prePostMult.get(0),
+                  prePostMult.get(1),
+                  bcLUT,
+                  bandIds
+                ))
                 
-                )
+                correctionAccum.add(System.currentTimeMillis() - afterAuxData)
+                
+                result
               }
             )
         }
@@ -105,9 +127,17 @@ class ComputeWaterVapor {
   }
  
   def compute(
+    multibandtile: (SpaceTimeKey, MultibandTile), // where is wv/r0/r1
     szaTile: Tile, 
     vzaTile: Tile, 
-    raaTile: Tile
+    raaTile: Tile,
+    elevationTile: Tile,
+    aot: Double,
+    ozone: Double,
+    preMult: Double,
+    postMult: Double,
+    bcLUT: Broadcast[LookupTable],
+    bandIds:java.util.List[String]
   ) : Tile = {
 
     val wvBandId="B09"
@@ -119,29 +149,20 @@ class ComputeWaterVapor {
     val wvCalc = new AbdaWaterVaporCalculator()
     
     wvCalc.prepare(bcLUT.value,cd,wvBandId,r0BandId,r1BandId)
-        
-    val startMillis = System.currentTimeMillis();
-    // AOT overriden                val aotTile = aotProvider.computeAOT(multibandtile._1, crs, layoutDefinition)
-    val aot=defParams.get(4)
-    val ozone=defParams.get(5)
-    val demTile = demProvider.compute(multibandtile._1, crs, layoutDefinition)
-    val afterAuxData = System.currentTimeMillis()
-    
+            
     // TODO: use reflToRad(double src, double sza, ZonedDateTime time, int bandToConvert)
-    val wvTile= multibandtile._2.band(wvCalc.findIndexOf(bandIds,wvBandId)).convert(FloatConstantNoDataCellType)*prePostMult.get(0)
-    val r0Tile= multibandtile._2.band(wvCalc.findIndexOf(bandIds,r0BandId)).convert(FloatConstantNoDataCellType)*prePostMult.get(0)
-    val r1Tile= multibandtile._2.band(wvCalc.findIndexOf(bandIds,r1BandId)).convert(FloatConstantNoDataCellType)*prePostMult.get(0)
-                    
-    auxDataAccum.add(afterAuxData-startMillis)
-    
+    val wvTile= multibandtile._2.band(wvCalc.findIndexOf(bandIds,wvBandId)).convert(FloatConstantNoDataCellType)*preMult
+    val r0Tile= multibandtile._2.band(wvCalc.findIndexOf(bandIds,r0BandId)).convert(FloatConstantNoDataCellType)*preMult
+    val r1Tile= multibandtile._2.band(wvCalc.findIndexOf(bandIds,r1BandId)).convert(FloatConstantNoDataCellType)*preMult
+                        
     // try to get at least 1 valid value on 60m resolution
-    val mbtresult : MultibandTile = try { 
+    val mbtresult : Tile = try { 
       val wvRawResultTile=bp.computeDoubleBlocks(
         MultibandTile(
           szaTile, 
           vzaTile, 
           raaTile,
-          demTile.convert(FloatConstantNoDataCellType), 
+          elevationTile.convert(FloatConstantNoDataCellType), 
     // AOT overriden                      aotTile.convert(FloatConstantNoDataCellType), 
           wvTile.convert(FloatConstantNoDataCellType), 
           r0Tile.convert(FloatConstantNoDataCellType), 
@@ -153,13 +174,12 @@ class ComputeWaterVapor {
         doubleNODATA,
         bcLUT.value,
         wvCalc
-      )*prePostMult.get(1)
-      MultibandTile(bp.replaceNoDataWithAverage(wvRawResultTile,doubleNODATA))
+      )*postMult
+      bp.replaceNoDataWithAverage(wvRawResultTile,doubleNODATA)
     } catch {
-      case e: IllegalArgumentException => MultibandTile(wvTile)
+      case e: IllegalArgumentException => wvTile
     }
     
-    correctionAccum.add(System.currentTimeMillis() - afterAuxData)
     mbtresult.convert(FloatConstantNoDataCellType)
    
  }
