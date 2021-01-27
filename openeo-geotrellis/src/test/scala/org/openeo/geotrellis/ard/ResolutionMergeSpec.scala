b9506674a3e3700d9c08a82b22aa95214bdf3c58
Jeroen Dries <jeroen.dries@vito.be>
2021-01-27 08:46:05 +0100

    EP-3687 enable test for resolution merge

--- openeo-geotrellis/src/test/scala/org/openeo/geotrellis/ard/ResolutionMergeSpec.scala
+++ openeo-geotrellis/src/test/scala/org/openeo/geotrellis/ard/ResolutionMergeSpec.scala
@@ -1,22 +1,27 @@
 package org.openeo.geotrellis.ard
 
 import geotrellis.raster.io.geotiff.GeoTiff
-import org.junit.{Ignore, Test}
+import org.junit.Assert.assertArrayEquals
+import org.junit.Test
 
 
 class ResolutionMergeSpec {
 
-  //TODO check in geotiff
-  @Ignore
   @Test
   def testSentinel2Merge(): Unit ={
-    val file = Thread.currentThread().getContextClassLoader.getResource("org/openeo/geotrellis/S2-bands.geotiff")
+    val file = Thread.currentThread().getContextClassLoader.getResource("org/openeo/geotrellis/S2-bands.tiff")
     val inputTiff = GeoTiff.readMultiband(file.getPath)
     val bands = inputTiff.tile.toArrayTile()
     val kSize = 5
 
     val pansharpened = Improphe.improphe(null,bands,Array(2,3),Array(4,5),3,kSize)
-    val tiff = GeoTiff(pansharpened,inputTiff.extent,inputTiff.crs)
-    tiff.write("/tmp/sharper.tiff",true)
+    //val tiff = GeoTiff(pansharpened,inputTiff.extent,inputTiff.crs)
+    //tiff.write("/tmp/sharper.tiff",true)
+
+    val refFile = Thread.currentThread().getContextClassLoader.getResource("org/openeo/geotrellis/S2-bands-resolutionmerge.tiff")
+    val refTiff = GeoTiff.readMultiband(refFile.getPath)
+
+    refTiff.tile.bands.zip(pansharpened.bands).foreach(t => assertArrayEquals(t._1.toArray(),t._2.toArray()))
+
   }
 }
