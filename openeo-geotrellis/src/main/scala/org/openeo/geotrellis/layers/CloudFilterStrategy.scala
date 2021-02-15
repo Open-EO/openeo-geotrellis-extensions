3f154dc5f3a97e5540509827e254062cb87331bc
Jeroen Dries <jeroen.dries@vito.be>
2021-02-15 12:18:06 +0100

    add full cropsar cloudmasking implementation

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/layers/CloudFilterStrategy.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/layers/CloudFilterStrategy.scala
@@ -2,7 +2,7 @@ package org.openeo.geotrellis.layers
 
 import geotrellis.raster.RasterRegion.GridBoundsRasterRegion
 import geotrellis.raster.mapalgebra.focal.Kernel
-import geotrellis.raster.{MultibandTile, Raster, RasterRegion}
+import geotrellis.raster.{MultibandTile, NODATA, Raster, RasterRegion, UByteUserDefinedNoDataCellType}
 import org.openeo.geotrellis.FFTConvolve
 
 abstract  class CloudFilterStrategy() extends Serializable {
@@ -22,12 +22,13 @@ class SCLConvolutionFilterStrategy(val sclBandIndex:Int=0) extends CloudFilterSt
   override def loadMasked(rasterRegion: RasterRegion): Option[MultibandTile] = {
     val gbRegion = rasterRegion.asInstanceOf[GridBoundsRasterRegion]
     val cloudSource = gbRegion.source.asInstanceOf[BandCompositeRasterSource].sources.toList(sclBandIndex)
-    val cloudRaster = new GridBoundsRasterRegion(cloudSource,gbRegion.bounds).raster
+
+    val cloudRaster = new GridBoundsRasterRegion(cloudSource,gbRegion.bounds.buffer(100)).raster
 
     if(cloudRaster.isDefined) {
       val maskTile = cloudRaster.get.tile.band(0)
       var allMasked = true
-      var binaryMask = maskTile.map(value => {
+      val binaryMask = maskTile.map(value => {
         if (value == 2 || value == 4 || value == 5 || value == 6 || value == 7) {
           allMasked = false
           0
@@ -36,20 +37,76 @@ class SCLConvolutionFilterStrategy(val sclBandIndex:Int=0) extends CloudFilterSt
         }
       })
       if (! allMasked) {
-        val kernel = Kernel.circle(31, 10, 15).tile
-        binaryMask = FFTConvolve(binaryMask, kernel).localIf({ d: Double => d > 0.9 }, 1.0, 0.0)
-        allMasked = !binaryMask.toArray().exists(_ == 0)
-      }
-      if (allMasked) {
-        Option.empty[MultibandTile]
-      } else {
-        val raster: Option[Raster[MultibandTile]] = rasterRegion.raster
-        if (raster.isDefined) {
-          Some(raster.get.tile.mapBands((i, t) => t.localMask(binaryMask.convert(raster.get.tile.cellType), 1, -1)))
-        } else {
-          raster.map(r => r.tile)
+
+        /**
+         *  0: nodata
+         *  1: saturated
+         *  2: dark area
+         *  3 cloud shadow
+         *  4 vegetatin
+         *  5 no vegetation
+         *  6 water
+         *  7 unclassified
+         *  8 cloud medium prob
+         *  9 cloud high prob
+         *  10 thin cirrus
+         *  11 snow
+         */
+
+        //maskTile.convert(UByteConstantNoDataCellType).renderPng(ColorMaps.IGBP).write("mask.png")
+        //binaryMask.convert(UByteConstantNoDataCellType).renderPng(ColorMaps.IGBP).write("bmask1.png")
+        val amplitude = 10000.0
+        val kernel1 = Kernel.gaussian(17,17.0/3.0,amplitude).copy()
+        val convolved = FFTConvolve(binaryMask, kernel1)
+        //first dilate, with a small kernel around everything that is not valid
+        allMasked = true
+        val convolution1 = convolved.crop(binaryMask.cols-(256+100),binaryMask.rows-(256+100),binaryMask.cols-101,binaryMask.rows-101).localIf({ d: Double => {
+          val res = d > amplitude* 0.057
+          if(!res) {
+            allMasked=false
+          }
+          res
+        } }, 1.0, 0.0)
+
+
+        if(!allMasked) {
+
+          //convolution1.convert(UByteConstantNoDataCellType).renderPng(ColorMaps.IGBP).write("conv1.png")
+          val binaryMask2 = maskTile.map(value => {
+            if (value == 3 || value == 8 || value == 9 || value == 10 || value == 11) {
+              1
+            } else {
+              0
+            }
+          })
+
+          //binaryMask2.convert(UByteConstantNoDataCellType).renderPng(ColorMaps.IGBP).write("bmask2.png")
+          val kernel2 = Kernel.gaussian(201,201.0/3.0,amplitude)
+          val convolution2 = FFTConvolve(binaryMask2, kernel2).crop(binaryMask2.cols-(256+100),binaryMask2.rows-(256+100),binaryMask2.cols-101,binaryMask2.rows-101)
+            .localIf({ d: Double => d > amplitude* 0.025 }, 1.0, 0.0)
+
+          //convolution2.convert(UByteConstantNoDataCellType).renderPng(ColorMaps.IGBP).write("conv2.png")
+          val fullMask = convolution1.localOr(convolution2).convert(UByteUserDefinedNoDataCellType(127.byteValue()))
+
+          allMasked = !fullMask.toArray().exists(_ == 0)
+          if (allMasked) {
+            Option.empty[MultibandTile]
+          } else {
+            val raster: Option[Raster[MultibandTile]] = rasterRegion.raster
+            if (raster.isDefined) {
+              Some(raster.get.tile.mapBands((i, t) => t.localMask(fullMask, 1, NODATA)))
+            } else {
+              raster.map(r => r.tile)
+            }
+          }
+        }else{
+          Option.empty[MultibandTile]
         }
+
+      }else{
+        Option.empty[MultibandTile]
       }
+
     }else{
       rasterRegion.raster.map(r => r.tile)
     }
