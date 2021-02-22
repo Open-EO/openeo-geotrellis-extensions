679677a88874bbf4addcf1fe6e64362c8e1938ff
Jan Van den bosch <jan.vandenbosch@hotmail.com>
2021-02-22 15:37:47 +0100

    EP-3682: restored test

--- geotrellis-sentinelhub/src/test/scala/org/openeo/geotrellissentinelhub/CatalogApiTest.scala
+++ geotrellis-sentinelhub/src/test/scala/org/openeo/geotrellissentinelhub/CatalogApiTest.scala
@@ -17,21 +17,20 @@ class CatalogApiTest {
   def dateTimes(): Unit = {
     val dateTimes = catalogApi.dateTimes(
       collectionId = "sentinel-1-grd",
-      ProjectedExtent(Extent(35.666439, -6.23476, 35.861576, -6.075694), LatLng),
-      from = LocalDate.of(2021, 2, 1).atStartOfDay(utc),
-      to = LocalDate.of(2021, 2, 17).atStartOfDay(utc),
+      ProjectedExtent(Extent(16.162995875210488, 48.305237663134704, 16.198050293067634, 48.328618668560985), LatLng),
+      from = LocalDate.of(2020, 11, 5).atStartOfDay(utc),
+      to = LocalDate.of(2020, 11, 7).atStartOfDay(utc),
       accessToken
     )
 
     println(dateTimes)
 
-    return
     val availableDates = dateTimes
       .sortWith(_ isBefore _)
       .map(_.toLocalDate)
       .distinct
 
-    val expectedDates = Seq(LocalDate.of(2021, 2, 3))
+    val expectedDates = Seq(LocalDate.of(2020, 11, 5), LocalDate.of(2020, 11, 6))
     assertEquals(expectedDates, availableDates)
   }
 
