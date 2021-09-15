61ffa244017a1c22aadfc153ea82aa817c606855
Jeroen Verstraelen <jeroen.verstraelen@vito.be>
2021-09-15 19:06:18 +0200

    EP-3985: ensure default maxSpatialResolution parameter is compatible with Python invocations

--- geotrellis-sentinelhub/src/main/scala/org/openeo/geotrellissentinelhub/PyramidFactory.scala
+++ geotrellis-sentinelhub/src/main/scala/org/openeo/geotrellissentinelhub/PyramidFactory.scala
@@ -46,9 +46,13 @@ object PyramidFactory {
   // convenience method for Python client
   // TODO: change name? A 429 response makes it rate-limited anyway.
   def rateLimited(endpoint: String, collectionId: String, datasetId: String, clientId: String, clientSecret: String,
-                  processingOptions: util.Map[String, Any], sampleType: SampleType, maxSpatialResolution: CellSize = CellSize(10,10)): PyramidFactory =
+                  processingOptions: util.Map[String, Any], sampleType: SampleType, maxSpatialResolution: CellSize): PyramidFactory =
     new PyramidFactory(collectionId, datasetId, new DefaultCatalogApi(endpoint), new DefaultProcessApi(endpoint),
-      clientId, clientSecret, processingOptions, sampleType, new RlGuardAdapter, maxSpatialResolution)
+                       clientId, clientSecret, processingOptions, sampleType, new RlGuardAdapter, maxSpatialResolution)
+
+  def rateLimited(endpoint: String, collectionId: String, datasetId: String, clientId: String, clientSecret: String,
+                  processingOptions: util.Map[String, Any], sampleType: SampleType): PyramidFactory =
+    rateLimited(endpoint, collectionId, datasetId, clientId, clientSecret, processingOptions, sampleType, CellSize(10,10))
 
   @deprecated("include a collectionId")
   def rateLimited(endpoint: String, datasetId: String, clientId: String, clientSecret: String,
