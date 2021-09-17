7ea4d1b84458f11b4ffaa0e60af4ba52168bd05d
Jan Van den bosch <jan.vandenbosch@hotmail.com>
2021-09-17 10:37:21 +0200

    EP-3869: aligned Elasticsearch doc ID with filenames on disk

--- geotrellis-sentinelhub/src/main/scala/org/openeo/geotrellissentinelhub/ElasticsearchRepository.scala
+++ geotrellis-sentinelhub/src/main/scala/org/openeo/geotrellissentinelhub/ElasticsearchRepository.scala
@@ -138,8 +138,10 @@ class ElasticsearchRepository(uri: String) {
     val client = elasticClient
 
     try {
+      val docId = s"${BASIC_ISO_DATE format entry.date.toLocalDate}-${entry.tileId}-${entry.bandName}"
+
       client.execute {
-        indexInto(cacheIndex).id(s"${entry.tileId}-${BASIC_ISO_DATE format entry.date.toLocalDate}-${entry.bandName}").doc(
+        indexInto(cacheIndex).id(docId).doc(
           s"""
              |{
              |  "tileId": "${entry.tileId}",
