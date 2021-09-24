099b413d47249cd5716a8e9880d61ce29b2a7e19
Jeroen Dries <jeroen.dries@vito.be>
2021-09-24 09:31:20 +0200

    EP-4016 custom S3Client was not found on the executors, try to solve with custom RangeReaderProvider

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/layers/FileLayerProvider.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/layers/FileLayerProvider.scala
@@ -15,20 +15,12 @@ import geotrellis.raster.rasterize.Rasterizer
 import geotrellis.raster.{CellSize, CellType, ConvertTargetCellType, FloatConstantNoDataCellType, FloatConstantTile, GridBounds, GridExtent, MosaicRasterSource, MultibandTile, PaddedTile, Raster, RasterExtent, RasterMetadata, RasterRegion, RasterSource, ResampleMethod, ResampleTarget, SourceName, SourcePath, TargetAlignment, TargetCellType, UByteUserDefinedNoDataCellType, UShortConstantNoDataCellType}
 import geotrellis.spark._
 import geotrellis.spark.partition.{PartitionerIndex, SpacePartitioner}
-import geotrellis.store.s3.S3ClientProducer
 import geotrellis.vector._
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import org.locationtech.proj4j.proj.TransverseMercatorProjection
 import org.openeo.geotrelliscommon.{CloudFilterStrategy, DataCubeParameters, L1CCloudFilterStrategy, MaskTileLoader, NoCloudFilterStrategy, SCLConvolutionFilterStrategy, SpaceTimeByMonthPartitioner, SparseSpaceOnlyPartitioner, SparseSpaceTimePartitioner}
 import org.slf4j.LoggerFactory
-import software.amazon.awssdk.awscore.retry.conditions.RetryOnErrorCodeCondition
-import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration
-import software.amazon.awssdk.core.retry.RetryPolicy
-import software.amazon.awssdk.core.retry.backoff.FullJitterBackoffStrategy
-import software.amazon.awssdk.core.retry.conditions.{OrRetryCondition, RetryCondition}
-import software.amazon.awssdk.regions.Region
-import software.amazon.awssdk.services.s3.S3Client
 
 import java.io.IOException
 import java.net.{URI, URL}
@@ -142,41 +134,7 @@ class MultibandCompositeRasterSource(val sourcesListWithBandIds: NonEmptyList[(R
 
 object FileLayerProvider {
 
-  {
-    val retryCondition =
-      OrRetryCondition.create(
-        RetryCondition.defaultRetryCondition(),
-        RetryOnErrorCodeCondition.create("RequestTimeout")
-      )
-    val backoffStrategy =
-      FullJitterBackoffStrategy.builder()
-        .baseDelay(Duration.ofMillis(50))
-        .maxBackoffTime(Duration.ofMillis(15))
-        .build()
-    val retryPolicy =
-      RetryPolicy.defaultRetryPolicy()
-        .toBuilder()
-        .retryCondition(retryCondition)
-        .backoffStrategy(backoffStrategy)
-        .build()
-    val overrideConfig =
-      ClientOverrideConfiguration.builder()
-        .retryPolicy(retryPolicy)
-        .build()
 
-    val endpoint = System.getenv().getOrDefault("SWIFT_URL","https://s3.waw2-1.cloudferro.com")
-    val clientBuilder = S3Client.builder()
-      .overrideConfiguration(overrideConfig)
-      .region(Region.of("RegionOne"))
-    val client = if(endpoint!="") {
-      clientBuilder.endpointOverride(URI.create(endpoint)).build()
-    }else{
-
-      clientBuilder.build()
-    }
-
-    S3ClientProducer.set(()=> client)
-  }
   private val logger = LoggerFactory.getLogger(classOf[FileLayerProvider])
 
   // important: make sure to implement object equality for CacheKey's members
