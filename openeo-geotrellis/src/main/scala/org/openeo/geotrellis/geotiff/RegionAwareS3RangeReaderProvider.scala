5d8618ebb27ffc7493bcb7cfd17fcaa2251ac202
Jan Van den bosch <jan.vandenbosch@hotmail.com>
2021-11-17 17:18:55 +0100

    added retry to be able to support larger jobs

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/geotiff/RegionAwareS3RangeReaderProvider.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/geotiff/RegionAwareS3RangeReaderProvider.scala
@@ -3,12 +3,18 @@ package org.openeo.geotrellis.geotiff
 import geotrellis.store.s3.{AmazonS3URI, S3ClientProducer}
 import geotrellis.store.s3.util.{S3RangeReader, S3RangeReaderProvider}
 import org.jboss.netty.util.internal.ConcurrentHashMap
+import software.amazon.awssdk.awscore.retry.conditions.RetryOnErrorCodeCondition
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration
+import software.amazon.awssdk.core.retry.RetryPolicy
+import software.amazon.awssdk.core.retry.backoff.FullJitterBackoffStrategy
+import software.amazon.awssdk.core.retry.conditions.{OrRetryCondition, RetryCondition}
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.model.GetBucketLocationRequest
 
 import scala.compat.java8.FunctionConverters._
 import java.net.URI
+import java.time.Duration
 
 object RegionAwareS3RangeReaderProvider {
   private type BucketName = String
@@ -25,7 +31,32 @@ object RegionAwareS3RangeReaderProvider {
       .locationConstraint()
       .toString
 
+    // TODO: reduce code duplication with S3ClientProducer/MultiClientRangeReaderProvider?
+    val s3ClientConfig = {
+      val retryCondition =
+        OrRetryCondition.create(
+          RetryCondition.defaultRetryCondition(),
+          RetryOnErrorCodeCondition.create("RequestTimeout")
+        )
+      val backoffStrategy =
+        FullJitterBackoffStrategy.builder()
+          .baseDelay(Duration.ofMillis(50))
+          .maxBackoffTime(Duration.ofMillis(15))
+          .build()
+      val retryPolicy =
+        RetryPolicy.defaultRetryPolicy()
+          .toBuilder()
+          .retryCondition(retryCondition)
+          .backoffStrategy(backoffStrategy)
+          .build()
+
+      ClientOverrideConfiguration.builder()
+        .retryPolicy(retryPolicy)
+        .build()
+    }
+
     S3Client.builder()
+      .overrideConfiguration(s3ClientConfig)
       .region(Region.of(bucketRegion))
       .build()
   }
