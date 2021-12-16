a24532be5d9e68e9451b86a5be903c9d47cdbb59
Jeroen Dries <jeroen.dries@vito.be>
2021-12-16 22:14:32 +0100

    class has to be serializable

--- geotrellis-common/src/main/scala/org/openeo/geotrelliscommon/OpenEORasterCubeMetadata.scala
+++ geotrellis-common/src/main/scala/org/openeo/geotrelliscommon/OpenEORasterCubeMetadata.scala
@@ -11,7 +11,7 @@ object OpenEORasterCubeMetadata{
  * Container object to attach metadata to OpenEO RasterCube, and provide a convenient interface towards Python.
  * @param bands
  */
-class OpenEORasterCubeMetadata(var bands: Seq[String]) {
+class OpenEORasterCubeMetadata(var bands: Seq[String]) extends Serializable {
   var inputProducts:Seq[be.vito.eodata.gwcgeotrellis.opensearch.OpenSearchResponses.Feature] = _
 
 
