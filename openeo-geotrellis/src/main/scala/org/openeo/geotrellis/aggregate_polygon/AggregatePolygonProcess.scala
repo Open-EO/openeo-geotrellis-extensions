d5300c0d3653432d1556cb2865cafae07be82fab
Jeroen Dries <jeroen.dries@vito.be>
2022-01-17 11:37:04 +0100

    set savemod to overwrite, ignore existing things

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/aggregate_polygon/AggregatePolygonProcess.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/aggregate_polygon/AggregatePolygonProcess.scala
@@ -147,7 +147,7 @@ class AggregatePolygonProcess() {
       //val expressions = bandColumns.flatMap(col => Seq((col,"sum"),(col,"max"))).toMap
       //https://spark.apache.org/docs/3.2.0/sql-ref-null-semantics.html#built-in-aggregate
       val expressionCols = bandColumns.map(df.col(_)).flatMap(col => Seq(avg(col),sum(col),count(col.isNull),count(not(col.isNull))))//,expr(s"percentile_approx(band_1,0.95)")
-      dataframe.groupBy("date","feature_index").agg(expressionCols.head,expressionCols.tail:_*).write.mode(SaveMode.Append).csv("file://" + outputPath)
+      dataframe.groupBy("date","feature_index").agg(expressionCols.head,expressionCols.tail:_*).write.mode(SaveMode.Overwrite).csv("file://" + outputPath)
 
     }finally{
       byIndexMask.unpersist()
