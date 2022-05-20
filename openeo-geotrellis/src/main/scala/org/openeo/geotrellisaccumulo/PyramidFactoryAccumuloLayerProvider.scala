9060077cf341889131afcc44a817f7adb5ac3e93
Jan Van den bosch <jan.vandenbosch@hotmail.com>
2022-05-20 10:45:05 +0200

    EP-4115: avoid OOME/hanging geotrellistimeseries integration tests

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellisaccumulo/PyramidFactoryAccumuloLayerProvider.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellisaccumulo/PyramidFactoryAccumuloLayerProvider.scala
@@ -4,7 +4,7 @@ import com.google.common.cache.{CacheBuilder, CacheLoader, LoadingCache}
 import geotrellis.layer._
 import geotrellis.proj4.LatLng
 import geotrellis.spark.{ContextRDD, MultibandTileLayerRDD, TileLayerRDD}
-import geotrellis.store.accumulo.AccumuloInstance
+import geotrellis.store.accumulo.{AccumuloAttributeStore, AccumuloInstance}
 import geotrellis.vector.ProjectedExtent
 import org.apache.spark.SparkContext
 import org.openeo.geotrellis.aggregate_polygon.intern.PixelRateValidator
@@ -24,7 +24,7 @@ class PyramidFactoryAccumuloLayerProvider(val layerName: String)(implicit accumu
     readLayer(from, to, boundingBox, zoom, sc)
 
   private def readLayer(from: ZonedDateTime, to: ZonedDateTime, boundingBox: ProjectedExtent = null, zoom: Int, sc: SparkContext): MultibandTileLayerRDD[SpaceTimeKey] = {
-    val (pyramidFactory, metadata) = readCache.getUnchecked((zoom, sc))
+    val (pyramidFactory, metadata) = readCache.getUnchecked(zoom)
 
     PixelRateValidator(boundingBox, from, to, metadata, sc)
 
@@ -45,7 +45,7 @@ class PyramidFactoryAccumuloLayerProvider(val layerName: String)(implicit accumu
   }
 
   override def readMetadata(zoom: Int, sc: SparkContext): TileLayerMetadata[SpaceTimeKey] = {
-    readCache.getUnchecked((zoom, sc))._2
+    readCache.getUnchecked(zoom)._2
   }
 
   override def loadMetadata(sc: SparkContext): Option[(ProjectedExtent, Array[ZonedDateTime])] =
@@ -56,15 +56,18 @@ class PyramidFactoryAccumuloLayerProvider(val layerName: String)(implicit accumu
 
   override def toString: String = s"${getClass.getSimpleName}($layerName)"
 
-  private val readCache: LoadingCache[(Int, SparkContext), (PyramidFactory, TileLayerMetadata[SpaceTimeKey])] = {
+  // TODO: is this caching necessary?
+  private val readCache: LoadingCache[Integer, (PyramidFactory, TileLayerMetadata[SpaceTimeKey])] = {
     CacheBuilder.newBuilder()
       .expireAfterWrite(15, TimeUnit.MINUTES)
       .build(
-        new CacheLoader[(Int, SparkContext), (PyramidFactory, TileLayerMetadata[SpaceTimeKey])] {
-          def load(key: (Int, SparkContext)): (PyramidFactory, TileLayerMetadata[SpaceTimeKey]) = {
+        new CacheLoader[Integer, (PyramidFactory, TileLayerMetadata[SpaceTimeKey])] {
+          def load(zoom: Integer): (PyramidFactory, TileLayerMetadata[SpaceTimeKey]) = {
             val pyramidFactory = new PyramidFactory("hdp-accumulo-instance", "epod-master1.vgt.vito.be:2181,epod-master2.vgt.vito.be:2181,epod-master3.vgt.vito.be:2181")
-            val pyramidSeq = pyramidFactory.pyramid_seq(layerName, LatLng.worldExtent, "EPSG:4326")
-            val metadata = pyramidSeq.find(_._1 == key._1).getOrElse(pyramidSeq.sortBy(_._1).reverse.head)._2.metadata
+
+            val attributeStore = AccumuloAttributeStore(accumuloSupplier())
+            val layerId = attributeStore.baseLayer(layerName, suggestedZoom = zoom)
+            val metadata = attributeStore.readMetadata[TileLayerMetadata[SpaceTimeKey]](layerId)
 
             (pyramidFactory, metadata)
           }
