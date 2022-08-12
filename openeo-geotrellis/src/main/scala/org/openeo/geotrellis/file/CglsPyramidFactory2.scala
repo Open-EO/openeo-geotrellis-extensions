65743b77d57297d88a37b4894471bf4116713476
Jeroen Verstraelen <jeroen.verstraelen@vito.be>
2022-08-12 14:15:20 +0200

    fix formatting in CglsPyramidFactory2

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/file/CglsPyramidFactory2.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/file/CglsPyramidFactory2.scala
@@ -6,7 +6,7 @@ import org.openeo.opensearch.backends.GlobalNetCDFSearchClient
 
 import java.util
 
-class CglsPyramidFactory2(dataGlob: String, dateRegex: String,netcdfVariables: util.List[String],  maxSpatialResolution: CellSize) extends Sentinel2PyramidFactory("http://dummy.endpoint.com","",netcdfVariables, "", maxSpatialResolution: CellSize) {
+class CglsPyramidFactory2(dataGlob: String, dateRegex: String, netcdfVariables: util.List[String], maxSpatialResolution: CellSize) extends Sentinel2PyramidFactory("http://dummy.endpoint.com","",netcdfVariables, "", maxSpatialResolution: CellSize) {
   override def createOpenSearch: OpenSearchClient = {
     new GlobalNetCDFSearchClient(dataGlob,netcdfVariables, dateRegex.r.unanchored)
   }
