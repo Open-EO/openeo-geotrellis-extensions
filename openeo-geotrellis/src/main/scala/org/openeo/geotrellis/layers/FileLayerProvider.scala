39a96a8ad5df445fd278a76c7fd1061b5b2fd4f2
Jeroen Dries <jeroen.dries@vito.be>
2022-10-13 21:26:01 +0200

    add a counter for total pixels

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/layers/FileLayerProvider.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/layers/FileLayerProvider.scala
@@ -308,12 +308,13 @@ object FileLayerProvider {
   }
 
 
-
+  private val PIXEL_COUNTER = "InputPixels"
 
   private def rasterRegionsToTiles(rasterRegionRDD: RDD[(SpaceTimeKey, (RasterRegion, SourceName))], metadata: TileLayerMetadata[SpaceTimeKey], retainNoDataTiles: Boolean, cloudFilterStrategy: CloudFilterStrategy = NoCloudFilterStrategy, partitionerOption: Option[SpacePartitioner[SpaceTimeKey]] = None) = {
     val partitioner = partitionerOption.getOrElse(SpacePartitioner(metadata.bounds))
     logger.info(s"Cube partitioner index: ${partitioner.index}")
     val totalChunksAcc = rasterRegionRDD.sparkContext.longAccumulator("ChunkCount_" + rasterRegionRDD.name)
+    BatchJobMetadataTracker.tracker("").registerCounter(PIXEL_COUNTER)
     val loadingTimeAcc = rasterRegionRDD.sparkContext.doubleAccumulator("SecondsPerChunk_" + rasterRegionRDD.name)
     val tiledRDD: RDD[(SpaceTimeKey, MultibandTile)] =
       rasterRegionRDD
@@ -414,6 +415,7 @@ object FileLayerProvider {
                   val totalPixels = mbTile.rows * mbTile.cols * mbTile.bandCount
                   totalPixelsPartition += totalPixels
                   totalChunksAcc.add(totalPixels / (256 * 256))
+                  BatchJobMetadataTracker.tracker("").add(PIXEL_COUNTER,totalPixels)
                 }
                 result
               }
