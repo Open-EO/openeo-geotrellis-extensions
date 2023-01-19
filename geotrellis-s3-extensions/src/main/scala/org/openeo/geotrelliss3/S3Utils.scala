52bcf3c20a648b5416cc962cb3517e05d927293d
Jeroen Verstraelen <jeroen.verstraelen@vito.be>
2023-01-19 13:27:27 +0100

    add logging to S3Utils

--- geotrellis-s3-extensions/src/main/scala/org/openeo/geotrelliss3/S3Utils.scala
+++ geotrellis-s3-extensions/src/main/scala/org/openeo/geotrelliss3/S3Utils.scala
@@ -1,10 +1,13 @@
 package org.openeo.geotrelliss3
 
+import org.slf4j.LoggerFactory
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.model.{Delete, DeleteObjectsRequest, ListObjectsRequest, ObjectIdentifier}
 import scala.collection.JavaConverters._
 
 object S3Utils {
+
+  val logger = LoggerFactory.getLogger(S3Utils.getClass)
 
   def deleteSubFolder(client: S3Client, bucketName: String, subfolder: String) = {
     val listObjectsRequest = ListObjectsRequest.builder
@@ -17,6 +20,9 @@ object S3Utils {
       .bucket(bucketName)
       .delete(Delete.builder.objects(keys.map(key => ObjectIdentifier.builder.key(key).build).asJavaCollection).build)
       .build
+    logger.info("Deleting objects from S3.")
+    logger.info(s"Bucket: $bucketName, Subfolder: $subfolder")
+    logger.info(s"Objects: ${keys.mkString(", ")}")
     client.deleteObjects(deleteObjectsRequest)
   }
 
