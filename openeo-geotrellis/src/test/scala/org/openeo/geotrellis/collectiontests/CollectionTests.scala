1c61a59541a3f913000b96e232435a36599c9a10
Emile Sonneveld <emile.sonneveld@vito.be>
2023-02-08 10:34:54 +0100

    CSV expected. Some tests take too long to run locally, so letting it run on CI.

--- openeo-geotrellis/src/test/scala/org/openeo/geotrellis/collectiontests/CollectionTests.scala
+++ openeo-geotrellis/src/test/scala/org/openeo/geotrellis/collectiontests/CollectionTests.scala
@@ -1,5 +1,6 @@
 package org.openeo.geotrellis.collectiontests
 
+import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
 import geotrellis.layer.SpaceTimeKey
 import geotrellis.proj4.WebMercator
 import geotrellis.raster.CellSize
@@ -8,10 +9,15 @@ import geotrellis.spark.util.SparkUtils
 import org.apache.hadoop.hdfs.HdfsConfiguration
 import org.apache.hadoop.security.UserGroupInformation
 import org.apache.spark.{SparkConf, SparkContext}
+import org.junit.Assert.{assertEquals, assertFalse, assertTrue}
 import org.junit.jupiter.api.{AfterAll, BeforeAll}
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.{Arguments, MethodSource}
 import org.junit.{AfterClass, BeforeClass, Test}
-import org.openeo.geotrellis.file.PyramidFactory
-import org.openeo.geotrellis.{ComputeStatsGeotrellisAdapterTest, LayerFixtures, ProjectedPolygons}
+import org.openeo.geotrellis.AggregateSpatialTest.assertEqualTimeseriesStats
+import org.openeo.geotrellis.file.{ProbaVPyramidFactoryTest, PyramidFactory}
+import org.openeo.geotrellis.{AggregateSpatialTest, ComputeStatsGeotrellisAdapterTest, LayerFixtures, ProjectedPolygons}
 import org.openeo.geotrelliscommon.DataCubeParameters
 import org.openeo.opensearch.OpenSearchClient
 
@@ -19,11 +25,6 @@ import java.net.URL
 import java.nio.file.{Files, Paths}
 import java.time.ZonedDateTime
 import java.util
-import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.scala.DefaultScalaModule
-import org.junit.Assert.{assertFalse, assertTrue}
-
 import scala.collection.JavaConverters._
 
 object CollectionTests {
@@ -126,32 +127,30 @@ object CollectionTests {
     val mapper = new ObjectMapper()
     mapper.readTree(s)
   }
+
+  def testLayerParams: java.util.stream.Stream[Arguments] = java.util.Arrays.stream(Array(
+    arguments("TERRASCOPE_S1_SLC_COHERENCE_V1"),
+    arguments("TERRASCOPE_S1_GAMMA0_V1"),
+    arguments("TERRASCOPE_S2_FAPAR_V2"), // Has layer
+    arguments("TERRASCOPE_S2_NDVI_V2"), // Has layer
+    arguments("TERRASCOPE_S2_LAI_V2"),
+    arguments("TERRASCOPE_S2_FCOVER_V2"),
+    arguments("TERRASCOPE_S2_TOC_V2"), // Has layer
+    arguments("S1_GRD_SIGMA0_ASCENDING"),
+    arguments("S1_GRD_SIGMA0_DESCENDING"),
+    arguments("PROBAV_L3_S5_TOC_100M"), // Has layer
+    arguments("PROBAV_L3_S10_TOC_333M"), // Has layer
+    arguments("COPERNICUS_30"),
+    arguments("COPERNICUS_90"),
+    arguments("CGLS_FAPAR_V2_GLOBAL"),
+    arguments("CGLS_LAI_V2_GLOBAL"),
+    arguments("CGLS_LAI300_V1_GLOBAL"),
+  ))
 }
 
 class CollectionTests {
 
   import CollectionTests._
-
-  private def get_layers() =
-    List(
-      //      "TERRASCOPE_S1_SLC_COHERENCE_V1",
-      //      "TERRASCOPE_S1_GAMMA0_V1",
-      "TERRASCOPE_S2_FAPAR_V2", // OK
-      //      "TERRASCOPE_S2_NDVI_V2", // OK
-      //      "TERRASCOPE_S2_LAI_V2",
-      //      "TERRASCOPE_S2_FCOVER_V2",
-      //      "TERRASCOPE_S2_TOC_V2", // OK
-      //      "S1_GRD_SIGMA0_ASCENDING",
-      //      "S1_GRD_SIGMA0_DESCENDING",
-      //      "PROBAV_L3_S5_TOC_100M",
-      //      "PROBAV_L3_S10_TOC_333M",
-      //      "COPERNICUS_30",
-      //      "COPERNICUS_90",
-      //      "CGLS_FAPAR_V2_GLOBAL",
-      //      "CGLS_LAI_V2_GLOBAL",
-      //      "CGLS_LAI300_V1_GLOBAL"
-    )
-
 
   val opensearchEndpoint = "https://services.terrascope.be/catalogue"
 
@@ -221,121 +220,120 @@ class CollectionTests {
     assertFalse(isEqual)
   }
 
-  @Test
-  def tester(): Unit = {
-    test_layer()
+  @ParameterizedTest
+  @MethodSource(Array("testLayerParams"))
+  def testLayers(layerStr: String): Unit = {
+    testLayerImpl(layerStr)
   }
 
-  private def test_layer(input_file: Option[String] = None,
-                         from_date: String = "2020-03-01T00:00:00Z",
-                         to_date: String = "2020-03-31T00:00:00Z",
-                         output_dir: Option[String] = None,
-                         expected_dir: Option[String] = None,
-                        ) = {
-
-
-    val output_dir_get = output_dir.getOrElse("tmp_collectiontests/")
+  /**
+   * This functin shares the same structure as the one of openeo-collection-tests,
+   * But for the moment has different expected results.
+   */
+  private def testLayerImpl(layerStr: String,
+                            input_file: Option[String] = None,
+                            from_date: String = "2020-03-01T00:00:00Z",
+                            to_date: String = "2020-03-31T00:00:00Z",
+                            output_dir: Option[String] = None,
+                            expected_dir: Option[String] = None,
+                           ): Unit = {
+    val output_dir_get = output_dir.getOrElse(new java.io.File("./tmp_collectiontests/").getCanonicalPath)
     Files.createDirectories(Paths.get(output_dir_get))
 
     val expected_dir_get = expected_dir.getOrElse(input_file.getOrElse(getClass.getResource("/org/openeo/geotrellis/collectiontests/expected/").getPath))
 
+    val vector_file = input_file.getOrElse(getClass.getResource("/org/openeo/geotrellis/collectiontests/"
+      + (if (layerStr.contains("CGLS")) "cgls_test.json" else "50testfields.json")).getFile)
+    var polygons = ProjectedPolygons.fromVectorFile(vector_file)
+    polygons = ProjectedPolygons.reproject(polygons, WebMercator) // LatLng gives slightly different rsults. Still not the same as the Python implmentation
+    val from_date_parsed = ZonedDateTime.parse(from_date)
+    val to_date_parsed = ZonedDateTime.parse(to_date)
 
-    get_layers().map(layerStr => {
-      val vector_file = input_file.getOrElse(getClass.getResource("/org/openeo/geotrellis/collectiontests/"
-        + (if (layerStr.contains("CGLS")) "cgls_test.json" else "50testfields.json")).getFile)
-      var polygons = ProjectedPolygons.fromVectorFile(vector_file)
-      polygons = ProjectedPolygons.reproject(polygons, WebMercator)
-      val from_date_parsed = ZonedDateTime.parse(from_date)
-      val to_date_parsed = ZonedDateTime.parse(to_date)
+    val datacubeParams = new DataCubeParameters()
+    datacubeParams.layoutScheme = "FloatingLayoutScheme"
+    datacubeParams.globalExtent = Some(polygons.extent)
 
-      val datacubeParams = new DataCubeParameters()
-      datacubeParams.layoutScheme = "FloatingLayoutScheme"
-      datacubeParams.globalExtent = Some(polygons.extent)
-
-      val layer: MultibandTileLayerRDD[SpaceTimeKey] = layerStr match {
-        case "TERRASCOPE_S2_FAPAR_V2" =>
-          val seqThing = faparPyramidFactory.datacube_seq(
-            polygons,
-            from_date,
-            to_date,
-            util.Collections.emptyMap[String, Any](),
-            "correlationid",
-            datacubeParams
-          )
-          val Seq((_, layer)) = seqThing
-          layer
-        case "TERRASCOPE_S2_NDVI_V2" =>
-          val seqThing = LayerFixtures.ClearNDVIPyramid().datacube_seq(
-            polygons,
-            from_date,
-            to_date,
-            util.Collections.emptyMap[String, Any](),
-            "correlationid",
-            datacubeParams
-          )
-          val Seq((_, layer)) = seqThing
-          layer
-        case "TERRASCOPE_S2_TOC_V2" => LayerFixtures.sentinel2TocLayerProviderUTM.readMultibandTileLayer(
-          from_date_parsed,
-          to_date_parsed,
-          polygons.extent,
-          1,
-          sc
+    val layer: MultibandTileLayerRDD[SpaceTimeKey] = layerStr match {
+      case "TERRASCOPE_S2_FAPAR_V2" =>
+        // WebMercator
+        val seqThing = faparPyramidFactory.datacube_seq(
+          polygons,
+          from_date,
+          to_date,
+          util.Collections.emptyMap[String, Any](),
+          "correlationid",
+          datacubeParams
         )
-        case _ => throw new IllegalStateException(s"Layer $layerStr not supported")
-      }
-      val file_name = s"expected_${layerStr}_${from_date_parsed.toLocalDate}_${to_date_parsed.toLocalDate}.json"
-      new ComputeStatsGeotrellisAdapterTest(1).computeStatsGeotrellisAdapter.compute_average_timeseries_from_datacube(
-        layer,
-        polygons,
-        from_date,
-        to_date,
-        band_index = 0,
-        output_file = output_dir_get + file_name
+        val Seq((_, layer)) = seqThing
+        layer
+      case "TERRASCOPE_S2_NDVI_V2" =>
+        // WebMercator
+        val seqThing = LayerFixtures.ClearNDVIPyramid().datacube_seq(
+          polygons,
+          from_date,
+          to_date,
+          util.Collections.emptyMap[String, Any](),
+          "correlationid",
+          datacubeParams
+        )
+        val Seq((_, layer)) = seqThing
+        layer
+      case "TERRASCOPE_S2_TOC_V2" => LayerFixtures.sentinel2TocLayerProviderUTM.readMultibandTileLayer(
+        from_date_parsed,
+        to_date_parsed,
+        polygons.extent,
+        1,
+        sc
       )
+      case "PROBAV_L3_S5_TOC_100M" =>
+        val seqThing = new ProbaVPyramidFactoryTest().pyramidFactoryS5.datacube_seq(
+          polygons,
+          from_date,
+          to_date,
+          util.Collections.emptyMap[String, Any](),
+          "correlationid",
+          datacubeParams,
+          Seq(1).asJava,
+        )
+        val Seq((_, layer)) = seqThing
+        layer
+      case "PROBAV_L3_S10_TOC_333M" =>
+        val seqThing = new ProbaVPyramidFactoryTest().pyramidFactoryS10.datacube_seq(
+          polygons,
+          from_date,
+          to_date,
+          util.Collections.emptyMap[String, Any](),
+          "correlationid",
+          datacubeParams,
+          Seq(1).asJava,
+        )
+        val Seq((_, layer)) = seqThing
+        layer
+      case _ => throw new IllegalStateException(s"Layer $layerStr not supported")
+    }
 
-      println("Outputted JSON: " + output_dir_get + file_name)
-      val json1 = fileToJSON(output_dir_get + file_name)
-      val json2 = fileToJSON(expected_dir_get + file_name)
+    val file_name = s"${layerStr}_${from_date_parsed.toLocalDate}_${to_date_parsed.toLocalDate}/"
+    new ComputeStatsGeotrellisAdapterTest(1).computeStatsGeotrellisAdapter.compute_generic_timeseries_from_datacube(
+      "mean",
+      layer,
+      polygons,
+      Paths.get(output_dir_get, file_name).toString,
+    )
 
-      val mapper = new ObjectMapper()
-      mapper.registerModule(DefaultScalaModule)
+    println("Outputted path: " + Paths.get(output_dir_get, file_name).toString)
 
-      // Only keeps 1 band values in JSON
-      def pruneCopyJsonObject(json: JsonNode, depth: Int = 0): JsonNode = {
-        if (!json.isContainerNode) {
-          json
-        } else {
-          if (json.isObject) {
-            val map = json.fields.asScala.flatMap { entry =>
-              if (depth >= 1)
-                List()
-              else
-                List(entry.getKey -> pruneCopyJsonObject(entry.getValue, depth + 1))
-            }.toMap
-            mapper.valueToTree(map)
-          } else {
-            val map = json.elements().asScala.zipWithIndex.flatMap { entry =>
-              if ((depth >= 2 && entry._2 >= 1) || entry._1.isNull)
-                List()
-              else
-                List(pruneCopyJsonObject(entry._1, depth + 1))
-            }.toList
-            mapper.valueToTree(map)
-          }
-        }
-      }
-
-      val prunedJson1 = pruneCopyJsonObject(json1)
-      val prunedJson2 = pruneCopyJsonObject(json2)
-      println("pruneCopyJsonObject: " + mapper.writeValueAsString(json1))
-      println("pruneCopyJsonObject: " + mapper.writeValueAsString(json2))
-      println("pruneCopyJsonObject: " + mapper.writeValueAsString(prunedJson1))
-      println("pruneCopyJsonObject: " + mapper.writeValueAsString(prunedJson2))
-
-
-      val isEqual = jsonEquals(prunedJson1, prunedJson2, 3)
-      assertTrue(isEqual)
-    })
+    if (Files.exists(Paths.get(expected_dir_get, file_name))) {
+      val groupedStats1 = AggregateSpatialTest.parseCSV(Paths.get(output_dir_get, file_name).toString)
+      val groupedStats2 = AggregateSpatialTest.parseCSV(Paths.get(expected_dir_get, file_name).toString)
+      groupedStats1.zip(groupedStats2).foreach(pair => {
+        assertEquals(pair._1._1, pair._2._1)
+        assertEqualTimeseriesStats(pair._1._2, pair._2._2)
+      })
+      println(groupedStats1.mkString("\n"))
+    } else {
+      println("Please copy output to expected path location. Example command:")
+      println("cp " + Paths.get(output_dir_get, file_name).toString + " " + Paths.get(expected_dir_get, file_name).toString)
+      assertTrue(false)
+    }
   }
 }
