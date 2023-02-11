f4632b667547568ce5b62bb9206737a8eca32102
Jeroen Dries <jeroen.dries@vito.be>
2023-02-11 20:36:15 +0100

    do not apply rounding to resolution as a general rule, stick to globalbounds which is assumed to be rounded already if needed

--- geotrellis-common/src/main/scala/org/openeo/geotrelliscommon/DatacubeSupport.scala
+++ geotrellis-common/src/main/scala/org/openeo/geotrelliscommon/DatacubeSupport.scala
@@ -52,9 +52,19 @@ object DatacubeSupport {
               logger.error(f"Trying to construct a datacube with a bounds ${boundingBox.extent} that is not entirely inside the global bounds: ${reprojected}. ")
               reprojected = reprojected.expandToInclude(boundingBox.extent)
             }
-            val x = maxSpatialResolution.width
-            val y = maxSpatialResolution.height
-            Extent(x * Math.floor(reprojected.xmin / x), y * Math.floor(reprojected.ymin / y), x * Math.ceil(reprojected.xmax / x), y * Math.ceil(reprojected.ymax / y))
+            if (p.getName == "utm") {
+              //this forces utm projection to always round to 10m, which is fine for sentinel-2, but perhaps not generally desired?
+              val x = maxSpatialResolution.width
+              val y = maxSpatialResolution.height
+              Extent(x * Math.floor(reprojected.xmin / x), y * Math.floor(reprojected.ymin / y), x * Math.ceil(reprojected.xmax / x), y * Math.ceil(reprojected.ymax / y))
+            }else{
+              if (reprojected.width < maxSpatialResolution.width || reprojected.height < maxSpatialResolution.height) {
+                Extent(reprojected.xmin, reprojected.ymin, Math.max(reprojected.xmax, reprojected.xmin + maxSpatialResolution.width), Math.max(reprojected.ymax, reprojected.ymin + maxSpatialResolution.height))
+              } else {
+                reprojected
+              }
+            }
+
           }else{
             if (p.getName == "utm") {
               //for utm, we return an extent that goes beyond the utm zone bounds, to avoid negative spatial keys
