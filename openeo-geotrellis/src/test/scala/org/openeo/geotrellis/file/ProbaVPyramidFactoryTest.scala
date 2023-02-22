b40c30f09a2e321ad9886bf51ca11caec1336ed2
Emile Sonneveld <emile.sonneveld@vito.be>
2023-02-22 09:59:27 +0100

    Testing with many partitions for #99 in groupBy.

--- openeo-geotrellis/src/test/scala/org/openeo/geotrellis/file/ProbaVPyramidFactoryTest.scala
+++ openeo-geotrellis/src/test/scala/org/openeo/geotrellis/file/ProbaVPyramidFactoryTest.scala
@@ -10,10 +10,14 @@ import geotrellis.spark.util.SparkUtils
 import geotrellis.vector.{Extent, ProjectedExtent}
 import org.apache.spark.SparkConf
 import org.junit.{Assert, Ignore, Test}
+import org.openeo.geotrellis.ProjectedPolygons
 import org.openeo.geotrellis.file.ProbaVPyramidFactory.Band._
+import org.openeo.geotrelliscommon.DataCubeParameters
 
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME
 import java.time.{LocalDate, LocalTime, ZoneOffset, ZonedDateTime}
+import java.util.Collections
 import scala.collection.JavaConverters._
 import scala.collection.mutable.ArrayBuffer
 
@@ -22,6 +26,40 @@ class ProbaVPyramidFactoryTest {
   val openSearchEndpoint = "https://services.terrascope.be/catalogue"
   val pyramidFactoryS5 = new ProbaVPyramidFactory(openSearchEndpoint, "urn:ogc:def:EOP:VITO:PROBAV_S5-TOC_100M_V001", "/data/MTDA/TIFFDERIVED/PROBAV_L3_S5_TOC_100M",CellSize(0.000992063492063, 0.000992063492063))
   val pyramidFactoryS10 =  new ProbaVPyramidFactory(openSearchEndpoint, "urn:ogc:def:EOP:VITO:PROBAV_S10-TOC_333M_V001", "/data/MTDA/TIFFDERIVED/PROBAV_L3_S10_TOC_333M",CellSize(0.000992063492063, 0.000992063492063))
+
+  @Test
+  def writeReadMultibandTileLayer(): Unit = {
+    val boundingBox = ProjectedExtent(Extent(xmin = 2.59003, ymin = 51.069, xmax = 20.8949, ymax = 51.2206), LatLng)
+    val from = ZonedDateTime.of(LocalDate.of(2020, 1, 1), LocalTime.MIDNIGHT, ZoneOffset.UTC)
+    val date = from
+
+    val sparkConf = new SparkConf()
+      .set("spark.kryoserializer.buffer.max", "512m")
+      .set("spark.rdd.compress", "true")
+
+    val sc = SparkUtils.createLocalSparkContext(sparkMaster = "local[*]", appName = getClass.getSimpleName, sparkConf)
+
+    try {
+      val srs = s"EPSG:${boundingBox.crs.epsgCode.get}"
+      val bandIndices = ArrayBuffer(RED, NIR, BLUE).map(_.id).asJava
+
+      val parameters = new DataCubeParameters
+      parameters.setLayoutScheme("FloatingLayoutScheme")
+      val pyramid = pyramidFactoryS5.datacube_seq(
+        ProjectedPolygons(Seq(boundingBox.extent.toPolygon(), boundingBox.extent.toPolygon(), boundingBox.extent.toPolygon()), srs),
+        from_date = ISO_OFFSET_DATE_TIME format date,
+        to_date = ISO_OFFSET_DATE_TIME format date,
+        metadata_properties = Collections.emptyMap[String, Any],
+        correlationId = "correlationId",
+        dataCubeParameters = parameters,
+        band_indices = bandIndices,
+      )
+      println(pyramid.size)
+    } finally {
+      Thread.sleep(999999999)
+      sc.stop()
+    }
+  }
 
   @Ignore
   @Test
