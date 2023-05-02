4cc54a2f71876835db06337f05e585e15311da47
Emile Sonneveld <emile.sonneveld@vito.be>
2023-05-02 18:00:11 +0200

    feat: added noData support in scaling with geotrellis seeder

--- geotrellis-seeder/src/main/scala/org/openeo/geotrellisseeder/Band.scala
+++ geotrellis-seeder/src/main/scala/org/openeo/geotrellisseeder/Band.scala
@@ -1,16 +1,18 @@
 package org.openeo.geotrellisseeder
 
-case class Band(name: String, id: Int, min: Int, max: Int)
+case class Band(name: String, id: Int, min: Int, max: Int, noData: Int)
 
 object Band {
-  def apply(name: String, min: Int, max: Int): Band = Band(name, 0, min, max)
+  def apply(name: String, min: Int, max: Int, noData: Int): Band = Band(name, 0, min, max, noData)
   def apply(commaSeparatedString: String): Band = {
     val split = commaSeparatedString.split(",")
 
     if (split.length == 3) {
       Band(split(0), 0, split(1).toInt, split(2).toInt)
-    } else {
+    } else if (split.length == 4) {
       Band(split(0), split(1).toInt, split(2).toInt, split(3).toInt)
+    } else {
+      Band(split(0), split(1).toInt, split(2).toInt, split(3).toInt, split(4).toInt)
     }
   }
 }
