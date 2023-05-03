934e5fd2bc1547ca9f121d9249f3053bcf2e4bcc
Jens Verrydt <jens.verrydt@vito.be>
2023-05-03 10:24:37 +0200

    Fix for nodata value in apply

--- geotrellis-seeder/src/main/scala/org/openeo/geotrellisseeder/Band.scala
+++ geotrellis-seeder/src/main/scala/org/openeo/geotrellisseeder/Band.scala
@@ -8,9 +8,9 @@ object Band {
     val split = commaSeparatedString.split(",")
 
     if (split.length == 3) {
-      Band(split(0), 0, split(1).toInt, split(2).toInt)
+      Band(split(0), 0, split(1).toInt, split(2).toInt, null)
     } else if (split.length == 4) {
-      Band(split(0), split(1).toInt, split(2).toInt, split(3).toInt)
+      Band(split(0), split(1).toInt, split(2).toInt, split(3).toInt, null)
     } else {
       Band(split(0), split(1).toInt, split(2).toInt, split(3).toInt, split(4).toInt)
     }
