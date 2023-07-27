b5f1bf194aa79edbf1e756d46d54523da83531da
Jeroen Dries <jeroen.dries@vito.be>
2023-07-27 20:18:20 +0200

    fix interpretation of AWS_HTTPS

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/MultiClientRangeReaderProvider.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/MultiClientRangeReaderProvider.scala
@@ -20,7 +20,7 @@ import java.net.URI
 class MultiClientRangeReaderProvider extends S3RangeReaderProvider {
   @transient lazy val swiftEndpoint = new URI(sys.env.getOrElse("SWIFT_URL", "https://s3.waw2-1.cloudferro.com"))
   @transient lazy val s3Endpoint = sys.env("AWS_S3_ENDPOINT")
-  @transient lazy val s3Https = sys.env.getOrElse("AWS_HTTPS","FALSE").toUpperCase.equals("TRUE")
+  @transient lazy val s3Https = sys.env.getOrElse("AWS_HTTPS","NO").toUpperCase.equals("YES")
 
   override def rangeReader(uri: URI): S3RangeReader = {
     val s3Uri = new AmazonS3URI(uri)
