f3739bbc91eee55cbd27724d67ea75f6e043321f
verstraj <jeroen.verstraelen@vito.be>
2023-08-28 18:23:05 +0200

    add warning to vectorToRaster for UTM crs with large extent Open-EO/openeo-geopyspark-driver#423

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/vector/VectorCubeMethods.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/vector/VectorCubeMethods.scala
@@ -4,6 +4,7 @@ import io.circe.{Json, JsonObject}
 import io.circe.parser
 import geotrellis.layer.{KeyBounds, LayoutDefinition, Metadata, SpaceTimeKey, SpatialComponent, SpatialKey, TileLayerMetadata}
 import geotrellis.proj4.CRS
+import geotrellis.proj4.util.UTM.getZoneCrs
 import geotrellis.raster.{ArrayTile, CellSize, DoubleArrayFiller, DoubleConstantNoDataCellType, GridExtent, MultibandTile, NODATA, PixelIsPoint, RasterExtent, Tile}
 import geotrellis.vector.{io, _}
 import org.apache.spark.SparkContext
@@ -12,10 +13,13 @@ import geotrellis.raster.rasterize.Rasterizer.foreachCellByGeometry
 import geotrellis.spark.rasterize.RasterizeRDD.fromKeyedFeature
 import geotrellis.spark.{MultibandTileLayerRDD, withFeatureClipToGridMethods}
 import org.apache.spark.rdd.RDD
+import org.slf4j.LoggerFactory
 
 import scala.reflect.ClassTag
 
 object VectorCubeMethods {
+
+  private val logger = LoggerFactory.getLogger(VectorCubeMethods.getClass)
 
   def rasterizeWithDouble(geom: Geometry, rasterExtent: RasterExtent, value: Double): Tile =
     if(geom.isValid) {
@@ -35,6 +39,10 @@ object VectorCubeMethods {
         vectorToRasterGeneric(path, rdd2.asInstanceOf[MultibandTileLayerRDD[SpaceTimeKey]])
       case _ => throw new IllegalArgumentException("Unsupported rdd type to vectorize: ${rdd}")
     }
+  }
+
+  def isUTM(crs: CRS): Boolean = {
+    crs.epsgCode.get.toString.startsWith("326") || crs.epsgCode.get.toString.startsWith("327")
   }
 
   /**
@@ -74,10 +82,22 @@ object VectorCubeMethods {
       GridExtent[Long](extent, CellSize(target_resolution, target_resolution)),
       256
     )
+    if (isUTM(target_crs) && (extent.width > 100000 || extent.height > 100000)) {
+      val geom = listJson.head.hcursor.downField("geometry").as[Geometry].getOrElse(throw new Exception("No geometry found in GeoJSON"))
+      val latlon = geom.getCentroid.getCoordinate
+      val optimalCRS = getZoneCrs(lon=latlon.x, lat=latlon.y)
+      if (optimalCRS != target_crs) {
+        logger.warn(s"WARNING VectorToRaster process:\n" +
+          s"The width and/or height of the geometries extent is very large while the target_datacube uses UTM as crs.\n" +
+          s"The optimal crs for the first geometry is ${optimalCRS} while the target_datacube uses ${target_crs}.\n"
+        )
+      }
+    }
     val keyedFeatures: RDD[(SpatialKey, Feature[Geometry, Double])] = featuresRDD.clipToGrid(layoutDefinition)
 
     val options = Rasterizer.Options(includePartial = true, sampleType = PixelIsPoint)
     val cellType = DoubleConstantNoDataCellType
+    // TODO: For very large extents fromKeyedFeature gives a OOM error on the executors. We will have to adjust the partitioner.
     val band: RDD[(SpatialKey, Tile)] with Metadata[LayoutDefinition] = fromKeyedFeature[Geometry](keyedFeatures, cellType, layoutDefinition, options)
     val datacube: RDD[(SpatialKey, MultibandTile)] = band.mapValues(tile => MultibandTile(tile))
 
