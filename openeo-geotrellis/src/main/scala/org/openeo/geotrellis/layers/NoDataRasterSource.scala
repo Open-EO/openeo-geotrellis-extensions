b034158b1c2a143f996d7cb9eccda3f5141d6880
Jan Van den bosch <jan.vandenbosch@hotmail.com>
2023-10-19 08:14:36 +0200

    WIP: NODATA RasterSource not backed by a Tile

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/layers/NoDataRasterSource.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/layers/NoDataRasterSource.scala
@@ -3,7 +3,7 @@ package org.openeo.geotrellis.layers
 import geotrellis.layer.CRSWorldExtent
 import geotrellis.proj4.{CRS, LatLng}
 import geotrellis.raster.io.geotiff.OverviewStrategy
-import geotrellis.raster.{ByteConstantTile, CellSize, CellType, GridBounds, GridExtent, MultibandTile, Raster, RasterExtent, RasterMetadata, RasterSource, ResampleMethod, ResampleTarget, SourceName, TargetCellType, byteNODATA}
+import geotrellis.raster.{ByteConstantTile, CellSize, CellType, GridBounds, GridExtent, IntConstantNoDataCellType, IntConstantTile, MultibandTile, NODATA, Raster, RasterExtent, RasterMetadata, RasterSource, ResampleMethod, ResampleTarget, SourceName, TargetCellType, byteNODATA}
 import geotrellis.vector.Extent
 
 object NoDataRasterSource {
@@ -57,4 +57,57 @@ class NoDataRasterSource private(raster: Raster[MultibandTile], override val crs
   override def cellType: CellType = raster.cellType
 
   override def gridExtent: GridExtent[Long] = GridExtent(raster.extent, cols = raster.cols, rows = raster.rows)
+}
+
+object AnotherNoDataRasterSource {
+  def instance: AnotherNoDataRasterSource = new AnotherNoDataRasterSource(IntConstantNoDataCellType,
+    GridExtent(LatLng.worldExtent, cols = 10, rows = 10), LatLng)
+}
+
+class AnotherNoDataRasterSource(override val cellType: CellType, override val gridExtent: GridExtent[Long], override val crs: CRS) extends RasterSource {
+  def targetCellType: Option[TargetCellType] = None
+
+  override def metadata: RasterMetadata = this
+
+  override protected def reprojection(targetCRS: CRS, resampleTarget: ResampleTarget, method: ResampleMethod, strategy: OverviewStrategy): RasterSource =
+    new AnotherNoDataRasterSource(cellType, gridExtent.reproject(crs, targetCRS), targetCRS)
+
+  override def resample(resampleTarget: ResampleTarget, method: ResampleMethod, strategy: OverviewStrategy): RasterSource =
+    new AnotherNoDataRasterSource(cellType, resampleTarget(gridExtent), crs)
+
+  override def read(extent: Extent, bands: Seq[Int]): Option[Raster[MultibandTile]] = {
+    require(bands == Seq(0))
+
+    extent.intersection(gridExtent.extent)
+      .map { intersection =>
+        val intersectionGridBounds = gridExtent.gridBoundsFor(intersection).toGridType[Int]
+        val noDataTile = IntConstantTile(
+          NODATA, cols = intersectionGridBounds.width, rows = intersectionGridBounds.height)
+        Raster(MultibandTile(noDataTile.convert(cellType)), intersection)
+      }
+  }
+
+  override def read(bounds: GridBounds[Long], bands: Seq[Int]): Option[Raster[MultibandTile]] = {
+    bounds.intersection(gridExtent.dimensions)
+      .map { intersection =>
+        val intersectionGridBounds = intersection.toGridType[Int]
+        val noDataTile = IntConstantTile(
+          NODATA, cols = intersectionGridBounds.width, rows = intersectionGridBounds.height)
+
+        Raster(MultibandTile(noDataTile.convert(cellType)), gridExtent.extentFor(intersection))
+      }
+  }
+
+  override def convert(targetCellType: TargetCellType): RasterSource =
+    new AnotherNoDataRasterSource(targetCellType.cellType, gridExtent, crs)
+
+  override def name: SourceName = f"${getClass.getName}($gridExtent, $crs)"
+
+  override def bandCount: Int = 1
+
+  override def resolutions: List[CellSize] = List(gridExtent.cellSize)
+
+  override def attributes: Map[String, String] = ???
+
+  override def attributesForBand(band: Int): Map[String, String] = ???
 }
--- openeo-geotrellis/src/test/scala/org/openeo/geotrellis/layers/NoDataRasterSourceTest.scala
+++ openeo-geotrellis/src/test/scala/org/openeo/geotrellis/layers/NoDataRasterSourceTest.scala
@@ -1,6 +1,6 @@
 package org.openeo.geotrellis.layers
 
-import geotrellis.proj4.WebMercator
+import geotrellis.proj4.{LatLng, WebMercator}
 import geotrellis.raster.io.geotiff.MultibandGeoTiff
 import geotrellis.raster.{FloatConstantNoDataCellType, GridBounds}
 import geotrellis.vector.Extent
@@ -11,35 +11,46 @@ class NoDataRasterSourceTest {
 
   @Test
   def test(): Unit = {
-    val noDataRasterSource = NoDataRasterSource.get
+    val noDataRasterSource = AnotherNoDataRasterSource.instance
+    assertEquals(LatLng, noDataRasterSource.crs)
     val Some(noDataRaster) = noDataRasterSource.read()
     MultibandGeoTiff(noDataRaster, noDataRasterSource.crs).write("/tmp/noDataRaster.tif")
     assertEquals(1, noDataRaster.tile.bandCount)
     assertTrue(noDataRaster.tile.band(0).isNoDataTile)
 
+    val Some(rasterCroppedByExtent) = noDataRasterSource.read(Extent(0.0, 50.0, 5.0, 55.0))
+    MultibandGeoTiff(rasterCroppedByExtent, noDataRasterSource.crs).write("/tmp/rasterCroppedByExtent.tif")
+    assertEquals(Extent(0.0, 50.0, 5.0, 55.0), rasterCroppedByExtent.extent)
+
+    val Some(rasterCroppedByGridBounds) = noDataRasterSource.read(GridBounds[Long](0, 0, 0, 0))
+    MultibandGeoTiff(rasterCroppedByGridBounds, noDataRasterSource.crs).write("/tmp/rasterCroppedByGridBounds.tif")
+    assertEquals(1, rasterCroppedByGridBounds.cols)
+    assertEquals(1, rasterCroppedByGridBounds.rows)
+
     val converted = noDataRasterSource.convert(FloatConstantNoDataCellType)
+    assertEquals(LatLng, converted.crs)
     val Some(convertedRaster) = converted.read()
     MultibandGeoTiff(convertedRaster, converted.crs).write("/tmp/convertedRaster.tif")
     assertEquals(FloatConstantNoDataCellType, convertedRaster.cellType)
     assertTrue(convertedRaster.tile.band(0).isNoDataTile)
 
     val reprojected = noDataRasterSource.reproject(targetCRS = WebMercator)
+    assertEquals(WebMercator, reprojected.crs)
     val Some(reprojectedRaster) = reprojected.read()
     MultibandGeoTiff(reprojectedRaster, reprojected.crs).write("/tmp/reprojectedRaster.tif")
     assertEquals(WebMercator, reprojected.crs)
 
     val resampled = noDataRasterSource.resample(targetCols = 100, targetRows = 100)
+    assertEquals(LatLng, resampled.crs)
     val Some(resampledRaster) = resampled.read()
     MultibandGeoTiff(resampledRaster, resampled.crs).write("/tmp/resampledRaster.tif")
     assertEquals(100, resampledRaster.cols)
     assertEquals(100, resampledRaster.rows)
 
-    val Some(rasterCroppedByExtent) = noDataRasterSource.read(Extent(0.0, 50.0, 5.0, 55.0))
-    MultibandGeoTiff(rasterCroppedByExtent, noDataRasterSource.crs).write("/tmp/rasterCroppedByExtent.tif")
+    val Some(resampledRasterCroppedByGridBounds) = resampled.read(GridBounds[Long](50, 50, 52, 51))
+    assertEquals(3, resampledRasterCroppedByGridBounds.cols)
+    assertEquals(2, resampledRasterCroppedByGridBounds.rows)
 
-    val Some(rasterCroppedByGridBounds) = noDataRasterSource.read(GridBounds[Long](5, 5, 5, 5))
-    MultibandGeoTiff(rasterCroppedByGridBounds, noDataRasterSource.crs).write("/tmp/rasterCroppedByGridBounds.tif")
-    assertEquals(1, rasterCroppedByGridBounds.cols)
-    assertEquals(1, rasterCroppedByGridBounds.rows)
+    MultibandGeoTiff(resampledRasterCroppedByGridBounds, resampled.crs).write("/tmp/resampledRasterCroppedByGridBounds.tif")
   }
 }
