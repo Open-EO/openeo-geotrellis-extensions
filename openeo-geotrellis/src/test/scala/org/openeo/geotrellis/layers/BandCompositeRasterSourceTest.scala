466c1fce13ac61c55abfb75ed74d626d6f40f464
Jan Van den bosch <jan.vandenbosch@hotmail.com>
2023-10-20 16:49:14 +0200

    address BandCompositeRasterSourceTest remarks

--- openeo-geotrellis/src/test/scala/org/openeo/geotrellis/layers/BandCompositeRasterSourceTest.scala
+++ openeo-geotrellis/src/test/scala/org/openeo/geotrellis/layers/BandCompositeRasterSourceTest.scala
@@ -3,7 +3,6 @@ package org.openeo.geotrellis.layers
 import cats.data.NonEmptyList
 import geotrellis.proj4.LatLng
 import geotrellis.raster.geotiff.GeoTiffRasterSource
-import geotrellis.raster.io.geotiff.MultibandGeoTiff
 import geotrellis.vector.{Extent, ProjectedExtent}
 import org.junit.jupiter.api.Assertions.{assertEquals, assertFalse, assertTrue}
 import org.junit.jupiter.api.Test
@@ -26,16 +25,14 @@ class BandCompositeRasterSourceTest {
     val composite = new BandCompositeRasterSource(rasterSources, crs = bbox.crs)
 
     val Some(compositeRasterByExtent) = composite.read(bbox.extent)
-    MultibandGeoTiff(compositeRasterByExtent, composite.crs).write("/tmp/singleBandGeoTiffRasterSource_extent.tif")
-
     assertEquals(1, compositeRasterByExtent.tile.bandCount)
     assertFalse(compositeRasterByExtent.tile.band(0).isNoDataTile)
+    assertTrue(compositeRasterByExtent.extent.equalsExact(bbox.extent, 0.01))
 
     val Some(compositeRasterByGridBounds) = composite.read(bounds = composite.gridExtent.gridBoundsFor(bbox.extent))
-    MultibandGeoTiff(compositeRasterByGridBounds, composite.crs).write("/tmp/singleBandGeoTiffRasterSource_gridBounds.tif")
-
     assertEquals(1, compositeRasterByGridBounds.tile.bandCount)
     assertFalse(compositeRasterByGridBounds.tile.band(0).isNoDataTile)
+    assertTrue(compositeRasterByGridBounds.extent.equalsExact(bbox.extent, 0.01))
   }
 
   @Test
@@ -52,17 +49,15 @@ class BandCompositeRasterSourceTest {
     val composite = new BandCompositeRasterSource(rasterSources, crs = bbox.crs)
 
     val Some(compositeRasterByExtent) = composite.read(bbox.extent)
-    MultibandGeoTiff(compositeRasterByExtent, composite.crs).write("/tmp/emptyBand_extent.tif")
-
     assertEquals(2, compositeRasterByExtent.tile.bandCount)
     assertFalse(compositeRasterByExtent.tile.band(0).isNoDataTile)
     assertTrue(compositeRasterByExtent.tile.band(1).isNoDataTile)
+    assertTrue(compositeRasterByExtent.extent.equalsExact(bbox.extent, 0.01))
 
     val Some(compositeRasterByGridBounds) = composite.read(bounds = composite.gridExtent.gridBoundsFor(bbox.extent))
-    MultibandGeoTiff(compositeRasterByGridBounds, composite.crs).write("/tmp/emptyBand_gridBounds.tif")
-
     assertEquals(2, compositeRasterByGridBounds.tile.bandCount)
     assertFalse(compositeRasterByGridBounds.tile.band(0).isNoDataTile)
     assertTrue(compositeRasterByGridBounds.tile.band(1).isNoDataTile)
+    assertTrue(compositeRasterByGridBounds.extent.equalsExact(bbox.extent, 0.01))
   }
 }
