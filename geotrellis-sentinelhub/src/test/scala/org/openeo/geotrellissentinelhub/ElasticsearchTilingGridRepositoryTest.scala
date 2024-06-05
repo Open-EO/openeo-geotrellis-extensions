3abdcb6f971102b21ab9d9d88f2eeb5ae4cf2165
Jeroen Dries <jeroen.dries@vito.be>
2024-06-05 09:46:21 +0200

    ignore broken test for unused feature. Probably an issue with new CI setup.

--- geotrellis-sentinelhub/src/test/scala/org/openeo/geotrellissentinelhub/ElasticsearchTilingGridRepositoryTest.scala
+++ geotrellis-sentinelhub/src/test/scala/org/openeo/geotrellissentinelhub/ElasticsearchTilingGridRepositoryTest.scala
@@ -1,12 +1,13 @@
 package org.openeo.geotrellissentinelhub
 
 import geotrellis.vector._
-import org.junit.Test
+import org.junit.{Ignore, Test}
 
 class ElasticsearchTilingGridRepositoryTest {
   private val tilingGridRepository = new ElasticsearchTilingGridRepository("https://es-apps-dev.vgt.vito.be:443")
 
   @Test
+  @Ignore("Test depends on internal service, this feature is also effectively not used.")
   def getGeometry(): Unit = {
     val geometry = tilingGridRepository.getGeometry("sentinel-hub-tiling-grid-1", "31UDS_7_2")
     println(geometry.toGeoJson())
