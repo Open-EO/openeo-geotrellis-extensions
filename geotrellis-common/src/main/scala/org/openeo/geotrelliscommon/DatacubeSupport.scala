e378dab04b90bbf334a51636e79b60dd22cf02fd
Jeroen Dries <jeroen.dries@vito.be>
2024-06-09 21:18:21 +0200

    pixel wise masking was not applying the mask for tiles where all pixels were masked

--- geotrellis-common/src/main/scala/org/openeo/geotrelliscommon/DatacubeSupport.scala
+++ geotrellis-common/src/main/scala/org/openeo/geotrelliscommon/DatacubeSupport.scala
@@ -235,13 +235,13 @@ object DatacubeSupport {
             if (logger.isDebugEnabled) {
               logger.debug(s"Spacetime mask is used to reduce input.")
             }
-            val filtered = spacetimeMask.withContext{_.filter(_._2.band(0).toArray().exists(pixel => pixel == 0))}
+
             val alignedMask: MultibandTileLayerRDD[SpaceTimeKey] =
               if(spacetimeMask.metadata.crs.equals(metadata.crs) && spacetimeMask.metadata.layout.equals(metadata.layout)) {
-                filtered
+                spacetimeMask
               }else{
                 logger.debug(s"mask: automatically resampling mask to match datacube: ${spacetimeMask.metadata}")
-                filtered.reproject(metadata.crs,metadata.layout,16,rdd.partitioner)._2
+                spacetimeMask.reproject(metadata.crs,metadata.layout,16,rdd.partitioner)._2
               }
 
             if (pixelwiseMasking) {
@@ -249,8 +249,10 @@ object DatacubeSupport {
               // maskingCube is only set from Python when replacement is not defined
               rasterMaskGeneric(spacetimeDataContextRDD, alignedMask, null, ignoreKeysWithoutMask = true)
             } else {
-              // Because we are working on Tiles here and not RasterSources, this operation can already download the pixel data:
-              rdd.join(alignedMask).mapValues(_._1)
+
+              // retain only tiles where there is at least one valid pixel (mask value == 0)
+              val filtered = alignedMask.withContext{_.filter(_._2.band(0).toArray().exists(pixel => pixel == 0))}
+              rdd.join(filtered).mapValues(_._1)
             }
           } else {
             rdd
