0bf6c11a349babeb4d1940d77c9dde36a59604a8
Jeroen Dries <jeroen.dries@vito.be>
2024-09-09 09:15:26 +0200

    message formatting

--- openeo-geotrellis/src/main/java/org/openeo/sparklisteners/BatchJobProgressListener.scala
+++ openeo-geotrellis/src/main/java/org/openeo/sparklisteners/BatchJobProgressListener.scala
@@ -30,8 +30,13 @@ class BatchJobProgressListener extends SparkListener {
         val taskMetrics = stageCompleted.stageInfo.taskMetrics
 
         if(stageCompleted.stageInfo.failureReason.isDefined){
-            logger.warn("A part of the process graph failed, and will be retried, the reason was: " + stageCompleted.stageInfo.failureReason.get);
-            logger.info("Your job may still complete if the failure was caused by a transient error, but will take more time. A common cause of transient errors is too little executor memory (overhead). Too low executor-memory can be seen by a high 'garbage collection' time, which was: " + Duration.ofMillis(taskMetrics.jvmGCTime).toSeconds/1000.0 + " seconds.");
+            val message =
+              f"""
+                |"A part of the process graph failed, and will be retried, the reason was: ${stageCompleted.stageInfo.failureReason.get}
+                |"Your job may still complete if the failure was caused by a transient error, but will take more time. A common cause of transient errors is too little executor memory (overhead). Too low executor-memory can be seen by a high 'garbage collection' time, which was: ${Duration.ofMillis(taskMetrics.jvmGCTime).toSeconds/1000.0} seconds."
+                |""".stripMargin
+            logger.warn(message);
+
         }else{
           println(taskMetrics.jvmGCTime)
           val duration = Duration.ofMillis(taskMetrics.executorRunTime)
@@ -41,7 +46,7 @@ class BatchJobProgressListener extends SparkListener {
             duration.toMillis.toFloat / 1000.0 + " seconds"
           }
           val megabytes = taskMetrics.shuffleWriteMetrics.bytesWritten.toFloat/(1024.0*1024.0)
-          logger.info(f"Finished part ${stageCompleted.stageInfo.stageId}  of the process graph: $stageCompleted.stageInfo.name.\n The total computing time was: $timeString. It produced: $megabytes%.2f MB of data.");
+          logger.info(f"Finished part ${stageCompleted.stageInfo.stageId}  of the process graph: ${stageCompleted.stageInfo.name}.\n The total computing time was: $timeString. It produced: $megabytes%.2f MB of data.");
 
           val accumulators = stageCompleted.stageInfo.accumulables;
           val chunkCounts = accumulators.filter(_._2.name.get.startsWith("ChunkCount"));
@@ -49,7 +54,7 @@ class BatchJobProgressListener extends SparkListener {
             val totalChunks = chunkCounts.head._2.value
             val megapixel = totalChunks.get.asInstanceOf[Long] * 256 * 256 / (1024 * 1024)
             if(taskMetrics.executorRunTime > 0) {
-              logger.info(f"load_collection: data was loaded with an average speed of: ${megapixel.toFloat/ duration.toSeconds().toFloat}%.3f Megapixel per second.")
+              logger.info(f"load_collection: data was loaded with an average speed of: ${megapixel.toFloat/ duration.toSeconds.toFloat}%.3f Megapixel per second.")
             };
           }
         }
