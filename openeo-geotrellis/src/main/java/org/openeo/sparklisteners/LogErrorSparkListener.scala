b7e7492f05daaa32eb48689ee6266cac75ecf2c7
Jeroen Dries <jeroen.dries@vito.be>
2024-10-07 20:22:06 +0200

    reduce logging

--- openeo-geotrellis/src/main/java/org/openeo/sparklisteners/LogErrorSparkListener.scala
+++ openeo-geotrellis/src/main/java/org/openeo/sparklisteners/LogErrorSparkListener.scala
@@ -1,7 +1,7 @@
 package org.openeo.sparklisteners
 
 import org.apache.spark.scheduler._
-import org.apache.spark.{SparkContext, Success}
+import org.apache.spark.{SparkContext, Success, TaskEndReason}
 import org.slf4j.LoggerFactory
 
 import java.util.concurrent.atomic.{AtomicInteger, AtomicLong}
@@ -85,12 +85,20 @@ class LogErrorSparkListener extends SparkListener {
     }
   }
 
+  def handleTaskFailure(r: TaskEndReason): Unit = {
+    if(r.toString.contains("another attempt succeeded") || r.toString.contains("TaskKilled(Stage finished")){
+      logger.debug(r.toString)
+    }else{
+      logger.warn("Task error: " + r)
+    }
+  }
+
   override def onTaskEnd(taskEnd: SparkListenerTaskEnd): Unit = {
     val newValue = tasksCompleted.incrementAndGet()
     if (debug) println("LogErrorSparkListener.onTaskEnd(...) tasksCompleted: " + newValue + " taskEnd.reason: " + taskEnd.reason)
     taskEnd.reason match {
       case Success => // Ignore
-      case r => logger.warn("Task error: " + r)
+      case r => handleTaskFailure(r)
     }
 
     // taskMetrics may be null if the task has failed
