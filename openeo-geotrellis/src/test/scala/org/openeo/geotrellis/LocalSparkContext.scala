a7cb3fd29c881c81bd2906eb336bc3bedbc0cf10
Emile Sonneveld <emile.sonneveld@vito.be>
2024-11-21 14:13:27 +0100

    Modernized AgEra5FileLayerProviderTest while checking for pixel shift. No functional changes.

--- openeo-geotrellis/src/test/scala/org/openeo/geotrellis/LocalSparkContext.scala
+++ openeo-geotrellis/src/test/scala/org/openeo/geotrellis/LocalSparkContext.scala
@@ -3,21 +3,46 @@ package org.openeo.geotrellis
 import geotrellis.spark.util.SparkUtils
 import org.apache.spark.{SparkConf, SparkContext}
 import org.junit.ClassRule
+import org.junit.jupiter.api.{AfterAll, BeforeAll}
 import org.junit.rules.ExternalResource
 
-trait LocalSparkContext {
-  def appName: String = getClass.getName
-  def sparkConf: SparkConf = new SparkConf
-  def sparkMaster: String = "local[*]"
+protected trait LocalSparkContextBase {
+  protected var _sc: Option[SparkContext] = None
 
-  var sc: SparkContext = _
+  implicit def sc: SparkContext = {
+    if (_sc.isEmpty) {
+      val conf = new SparkConf()
+        .set("spark.kryoserializer.buffer.max", "512m")
+        .set("spark.rdd.compress", "true")
+      _sc = Some(SparkUtils.createLocalSparkContext(sparkMaster = "local[*]", appName = getClass.getSimpleName, conf))
+    }
+    _sc.get
+  }
 
+  def tearDownSpark(): Unit = {
+    if (_sc.isDefined) {
+      _sc.get.stop()
+      _sc = None
+    }
+  }
+}
+
+//noinspection JUnitMalformedDeclaration
+trait LocalSparkContext extends LocalSparkContextBase {
   @ClassRule
   def sparkContext: ExternalResource = new ExternalResource {
-    override def before(): Unit =
-      sc = SparkUtils.createLocalSparkContext(sparkMaster, appName, sparkConf)
+    override def before(): Unit = sc
 
-    override def after(): Unit =
-      sc.stop()
+    override def after(): Unit = tearDownSpark()
   }
+}
+
+//noinspection JUnitMalformedDeclaration
+trait LocalSparkContextJupyter extends LocalSparkContextBase {
+
+  @BeforeAll
+  def setupSpark(): Unit = sc
+
+  @AfterAll
+  def after(): Unit = tearDownSpark()
 }
