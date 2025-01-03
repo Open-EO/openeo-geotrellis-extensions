9adad55d18774d5f5c382f37be96fca8bdc2e524
Jeroen Dries <jeroen.dries@vito.be>
2025-01-03 15:18:57 +0100

    define old type for backwards compatibility

---
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/layers/package.scala
@@ -0,0 +1,9 @@
+package org.openeo.geotrellis
+
+package object layers {
+
+  /**
+   * @deprecated Use LoadCollectionFromAssets instead
+   */
+  type FileLayerProvider = LoadCollectionFromAssets
+}
