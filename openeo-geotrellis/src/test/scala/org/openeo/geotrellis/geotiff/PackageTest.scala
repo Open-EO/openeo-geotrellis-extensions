89f1e39c927cdbeb55ad57a13ed1b16749a32f3a
Jan Van den bosch <jan.vandenbosch@hotmail.com>
2025-04-01 15:58:27 +0200

    test code path for more than 3 bands #405

--- openeo-geotrellis/src/test/scala/org/openeo/geotrellis/geotiff/PackageTest.scala
+++ openeo-geotrellis/src/test/scala/org/openeo/geotrellis/geotiff/PackageTest.scala
@@ -1,13 +1,23 @@
 package org.openeo.geotrellis.geotiff
 
-import geotrellis.proj4.LatLng
+import geotrellis.proj4.{CRS, LatLng, WebMercator}
 import geotrellis.raster.io.geotiff.{GeoTiff, Tiled}
-import org.junit.jupiter.api.Assertions.{assertEquals, assertThrows, assertTrue}
+import org.junit.jupiter.api.Assertions.{assertEquals, assertNotEquals, assertThrows, assertTrue}
 import org.junit.jupiter.api.{Disabled, Test}
 import org.junit.jupiter.api.io.TempDir
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.{Arguments, MethodSource}
 
 import java.io.IOException
 import java.nio.file.{Files, Path, Paths}
+import java.util.stream.{Stream => JStream}
+
+object PackageTest {
+  def testConvertToCogParameters: JStream[Arguments] = JStream.of(
+    Arguments.of("/org/openeo/geotrellis/cgls_ndvi300.tif", LatLng),
+    Arguments.of("/org/openeo/geotrellis/Sentinel2FileLayerProvider_multiband_reference_average.tif", WebMercator),
+  )
+}
 
 class PackageTest {
 
@@ -42,24 +52,26 @@ class PackageTest {
   private def processingSoftware(geotiff: Path): Option[String] =
     GeoTiff.readSingleband(geotiff.toString).tags.headTags.get("PROCESSING_SOFTWARE")
 
-  @Test
-  def testConvertToCog(@TempDir tempDir: Path): Unit = {
+  @ParameterizedTest
+  @MethodSource(Array("testConvertToCogParameters"))
+  def testConvertToCog(geoTiffResource: String, expectedCrs: CRS, @TempDir tempDir: Path): Unit = {
     val geotiffCopy = tempDir.resolve("copy.tif")
-    Files.copy(getClass.getResourceAsStream("/org/openeo/geotrellis/cgls_ndvi300.tif"), geotiffCopy)
+    Files.copy(getClass.getResourceAsStream(geoTiffResource), geotiffCopy)
     val originalFilePermissions = Files.getPosixFilePermissions(geotiffCopy)
+    val targetBlockSize = 128
 
-    val tiffBefore = GeoTiff.readSingleband(geotiffCopy.toString)
+    val tiffBefore = GeoTiff.readMultiband(geotiffCopy.toString)
 
-    assertEquals(LatLng, tiffBefore.crs)
-    assertEquals(Tiled(512, 512), tiffBefore.options.storageMethod)
+    assertEquals(expectedCrs, tiffBefore.crs)
+    assertNotEquals(Tiled(targetBlockSize, targetBlockSize), tiffBefore.options.storageMethod)
 
-    convertToCog(geotiffCopy, tiffBefore.bandCount, blockSize = 256)
+    convertToCog(geotiffCopy, tiffBefore.bandCount, blockSize = targetBlockSize)
     assertEquals(originalFilePermissions, Files.getPosixFilePermissions(geotiffCopy))
 
-    val tiffAfter = GeoTiff.readSingleband(geotiffCopy.toString)
+    val tiffAfter = GeoTiff.readMultiband(geotiffCopy.toString)
 
-    assertEquals(LatLng, tiffAfter.crs)
-    assertEquals(Tiled(256, 256), tiffAfter.options.storageMethod)
+    assertEquals(expectedCrs, tiffAfter.crs)
+    assertEquals(Tiled(targetBlockSize, targetBlockSize), tiffAfter.options.storageMethod)
     assertEquals(tiffBefore.getOverviewsCount, tiffAfter.getOverviewsCount)
     // additional COG checks here
   }
