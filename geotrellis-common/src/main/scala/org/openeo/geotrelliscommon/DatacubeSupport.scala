d20f43517ddd47f2556face7ecb4fc2fb8e13946
dsamaey <diether.samaey@gmail.com>
2025-05-05 17:44:31 +0200

    Issue #1128 wrong spatial extent after resampling (#437)
    Co-authored-by: dsamaey <diether.samaey@vito.be>

--- geotrellis-common/src/main/scala/org/openeo/geotrelliscommon/DatacubeSupport.scala
+++ geotrellis-common/src/main/scala/org/openeo/geotrelliscommon/DatacubeSupport.scala
@@ -40,7 +40,7 @@ object DatacubeSupport {
   }
 
   // note: make sure to express boundingBox and maxSpatialResolution in the same units
-  def getLayout(layoutScheme: LayoutScheme, boundingBox: ProjectedExtent, zoom: Int, maxSpatialResolution: CellSize, globalBounds:Option[ProjectedExtent] = Option.empty, multiple_polygons_flag: Boolean = false) = {
+  def getLayout(layoutScheme: LayoutScheme, boundingBox: ProjectedExtent, zoom: Int, maxSpatialResolution: CellSize, globalBounds: Option[ProjectedExtent] = Option.empty, multiple_polygons_flag: Boolean = false) = {
     val LayoutLevel(_, worldLayout) = layoutScheme match {
       case scheme: ZoomedLayoutScheme => scheme.levelForZoom(zoom)
       case scheme: FloatingLayoutScheme => {
@@ -48,12 +48,12 @@ object DatacubeSupport {
         val layoutExtent: Extent = {
           val p = boundingBox.crs.proj4jCrs.getProjection
           if (globalBounds.isDefined) {
-            var inputBounds = globalBounds.get
+            val inputBounds = globalBounds.get
 
             var reprojected: Extent =
-              if(!inputBounds.extent.isEmpty) {
+              if (!inputBounds.extent.isEmpty) {
                 inputBounds.reprojectAsPolygon(boundingBox.crs).getEnvelopeInternal
-              }  else{
+              } else {
                 inputBounds.reproject(boundingBox.crs)
               }
 
@@ -63,11 +63,11 @@ object DatacubeSupport {
             }
             val x = maxSpatialResolution.width
             val y = maxSpatialResolution.height
-            if (p.getName == "utm" && x < 100 && y < 100) {
+            if (p.getName == "utm" && x < 50 && y < 50) {
               // TODO: This statement is mostly for Sentinel-2. Can we remove this if-branch?
               //this forces utm projection to always round to 10m, which is fine for sentinel-2, but perhaps not generally desired?
               Extent(x * Math.floor(reprojected.xmin / x), y * Math.floor(reprojected.ymin / y), x * Math.ceil(reprojected.xmax / x), y * Math.ceil(reprojected.ymax / y))
-            }else{
+            } else {
               if (reprojected.width < maxSpatialResolution.width || reprojected.height < maxSpatialResolution.height) {
                 Extent(reprojected.xmin, reprojected.ymin, Math.max(reprojected.xmax, reprojected.xmin + maxSpatialResolution.width), Math.max(reprojected.ymax, reprojected.ymin + maxSpatialResolution.height))
               } else {
@@ -75,11 +75,11 @@ object DatacubeSupport {
               }
             }
 
-          }else{
+          } else {
             if (p.getName == "utm") {
               //for utm, we return an extent that goes beyond the utm zone bounds, to avoid negative spatial keys
               if (p.getSouthernHemisphere)
-              //official extent: Extent(166021.4431, 1116915.0440, 833978.5569, 10000000.0000) -> round to 10m + extend
+                //official extent: Extent(166021.4431, 1116915.0440, 833978.5569, 10000000.0000) -> round to 10m + extend
                 Extent(0.0, 1000000.0, 833970.0 + 100000.0, 10000000.0000 + 100000.0)
               else {
                 //official extent: Extent(166021.4431, 0.0000, 833978.5569, 9329005.1825) -> round to 10m + extend
