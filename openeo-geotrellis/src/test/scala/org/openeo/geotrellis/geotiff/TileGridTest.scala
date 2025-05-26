66a7c3bfb356f23c2526d3ba98e7aaabf6ce710f
elienVandermaesenVITO <elien.vandermaesen@vito.be>
2025-05-26 11:22:16 +0200

    issue 419 change checks tests for overview tiles for inconsistent sizes

--- openeo-geotrellis/src/test/scala/org/openeo/geotrellis/geotiff/TileGridTest.scala
+++ openeo-geotrellis/src/test/scala/org/openeo/geotrellis/geotiff/TileGridTest.scala
@@ -143,12 +143,14 @@ class TileGridTest {
       val tile = GeoTiff.readMultiband(path)
       Assert.assertEquals(3,tile.overviews.size)
       Assert.assertEquals(Tiled(128,128),tile.overviews.head.options.storageMethod)
-      Assert.assertEquals(421,tile.overviews(0).tile.cols)
-      Assert.assertEquals(421,tile.overviews(0).tile.rows)
-      Assert.assertEquals(211,tile.overviews(1).tile.cols)
-      Assert.assertEquals(211,tile.overviews(1).tile.rows)
-      Assert.assertEquals(106,tile.overviews(2).tile.cols)
-      Assert.assertEquals(106,tile.overviews(2).tile.rows)
+      val colSize = tile.tile.cols
+      val rowSize = tile.tile.rows
+      Assert.assertEquals(math.ceil(colSize.toDouble/4).toInt,tile.overviews(0).tile.cols)
+      Assert.assertEquals(math.ceil(rowSize.toDouble/4).toInt,tile.overviews(0).tile.rows)
+      Assert.assertEquals(math.ceil(colSize.toDouble/8).toInt,tile.overviews(1).tile.cols)
+      Assert.assertEquals(math.ceil(rowSize.toDouble/8).toInt,tile.overviews(1).tile.rows)
+      Assert.assertEquals(math.ceil(colSize.toDouble/16).toInt,tile.overviews(2).tile.cols)
+      Assert.assertEquals(math.ceil(rowSize.toDouble/16).toInt,tile.overviews(2).tile.rows)
     }
   }
 
