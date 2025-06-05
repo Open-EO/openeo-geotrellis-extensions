b26362301fbfa8a9b61117ac7a9dbf8123491c1b
dsamaey <diether.samaey@vito.be>
2025-06-05 18:13:29 +0200

    Issue #451: add basic process graph test

---
+++ geotrellis-common/src/test/scala/org/openeo/geotrelliscommon/DockerTest.scala
@@ -0,0 +1,29 @@
+package org.openeo.geotrelliscommon
+
+import org.junit.Test
+
+import scala.sys.process._
+
+class DockerTest {
+
+  @Test
+  def testPs(): Unit = {
+    val cmd = "docker ps"
+    val output = cmd.!!
+    println(output)
+  }
+
+  @Test
+  def testPull(): Unit = {
+    val cmd = "docker pull hello-world"
+    val output = cmd.!!
+    println(output)
+  }
+
+  @Test
+  def testRun(): Unit = {
+    val cmd = "docker run hello-world"
+    val output = cmd.!!
+    println(output)
+  }
+}
