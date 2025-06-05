fa48aa1504b26789321d3be086f3c76473dea366
dsamaey <diether.samaey@vito.be>
2025-06-05 12:15:10 +0200

    Issue #451: add basic process graph test

--- geopyspark-geotrellis/src/test/scala/geopyspark/geotrellis/ProcessGraphRunner.scala
+++ geopyspark-geotrellis/src/test/scala/geopyspark/geotrellis/ProcessGraphRunner.scala
@@ -36,9 +36,12 @@ object ProcessGraphRunner {
     val jars = classPath.split(":").filter(_.endsWith(".jar")).filter(_.contains(hostM2RepositoryFolder)).map(f => f.replaceFirst(hostM2RepositoryFolder, dockerM2RepositoryFolder)).reduce((acc, e) => acc + ":" + e)
     val folders = classPath.split(":").filter(!_.endsWith(".jar")).map(f => f.replaceFirst(hostCodeFolder, dockerCodeFolder)).reduce((acc, e) => acc + ":" + e)
 
-    val dockerClassPath = jars + ":" + folders
+    val dockerClassPath = "/code/geotrellis-extensions/target/geotrellis-extensions-2.4.0_2.12-SNAPSHOT.jar:" + folders + ":" + jars
 
     val debug = ManagementFactory.getRuntimeMXBean().getInputArguments().stream().anyMatch(_.contains("-agentlib:jdwp"))
+
+    val dockerImage = "vito-docker.artifactory.vgt.vito.be/geotrellis_process_graph_test_helper"
+//    val dockerImage = "run_process_graph_locally2"
 
     val cmd =
       if (debug) {
@@ -46,11 +49,11 @@ object ProcessGraphRunner {
         val sparkUIPort = findFirstOpenPort(4040)
         println(f"Waiting for remote debugger on port ${debugPort}")
         println(f"SparkUI will be available at http://localhost:${sparkUIPort}")
-        f"docker run -p ${debugPort}:5005 -p ${sparkUIPort}:4040 -v ${outputDir}:/out -v ${hostGraphFolder}:/graphs -v ${hostM2RepositoryFolder}:${dockerM2RepositoryFolder} -v ${hostCodeFolder}:${dockerCodeFolder} vito-docker.artifactory.vgt.vito.be/geotrellis_process_graph_test_helper /graphs/${processGraphName} /out ${dockerClassPath} DEBUG"
+        f"docker run -p ${debugPort}:5005 -p ${sparkUIPort}:4040 -v ${outputDir}:/out -v ${hostGraphFolder}:/graphs -v ${hostM2RepositoryFolder}:${dockerM2RepositoryFolder} -v ${hostCodeFolder}:${dockerCodeFolder} ${dockerImage} /graphs/${processGraphName} /out ${dockerClassPath} DEBUG"
       } else {
-        f"docker run -v ${outputDir}:/out -v ${hostGraphFolder}:/graphs -v ${hostM2RepositoryFolder}:${dockerM2RepositoryFolder} -v ${hostCodeFolder}:${dockerCodeFolder} vito-docker.artifactory.vgt.vito.be/geotrellis_process_graph_test_helper /graphs/${processGraphName} /out ${dockerClassPath}"
+        f"docker run -v ${outputDir}:/out -v ${hostGraphFolder}:/graphs -v ${hostM2RepositoryFolder}:${dockerM2RepositoryFolder} -v ${hostCodeFolder}:${dockerCodeFolder} ${dockerImage} /graphs/${processGraphName} /out ${dockerClassPath}"
       }
-
+    println(cmd)
     val output = cmd.!!
     println(output)
   }
--- geopyspark-geotrellis/src/test/scala/geopyspark/geotrellis/TestProcessGraphJson.scala
+++ geopyspark-geotrellis/src/test/scala/geopyspark/geotrellis/TestProcessGraphJson.scala
@@ -1,6 +1,7 @@
 package geopyspark.geotrellis
 
 import org.junit.Test
+import org.junit.jupiter.api.Disabled
 
 class TestProcessGraphJson {
 
@@ -8,4 +9,11 @@ class TestProcessGraphJson {
   def loadSyntheticData(): Unit = {
     ProcessGraphRunner.run("/processgraphs/load_synthetic_data.json")
   }
+
+  @Test
+  @Disabled
+  def bug(): Unit = {
+    ProcessGraphRunner.run("/processgraphs/bug.json")
+  }
+
 }
