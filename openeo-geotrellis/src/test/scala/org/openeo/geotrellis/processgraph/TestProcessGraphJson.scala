b6224d35906477a0b0b989b11ab9b41606dcf75b
dsamaey <diether.samaey@vito.be>
2025-06-10 13:41:54 +0200

    Issue #451: add basic process graph test

--- openeo-geotrellis/src/test/scala/org/openeo/geotrellis/processgraph/TestProcessGraphJson.scala
+++ openeo-geotrellis/src/test/scala/org/openeo/geotrellis/processgraph/TestProcessGraphJson.scala
@@ -18,4 +18,9 @@ class TestProcessGraphJson {
   def reduceTApplyNeighbourhoodUdf(): Unit = {
     ProcessGraphRunner.run("/org/openeo/geotrellis/processgraph/reduce_t_apply_neighbourhood_udf.json")
   }
+
+  @Test
+  def applyNeighbourhoodUdfCheckDims(): Unit = {
+    ProcessGraphRunner.run("/org/openeo/geotrellis/processgraph/apply_neighbourhood_udf_check_dims.json")
+  }
 }
