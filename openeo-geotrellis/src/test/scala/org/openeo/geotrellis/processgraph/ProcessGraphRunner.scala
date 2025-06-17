1f45a1493e0237a3d37792483f721c88186afbba
dsamaey <diether.samaey@vito.be>
2025-06-17 10:13:48 +0200

    Issue #451: add basic process graph test

--- openeo-geotrellis/src/test/scala/org/openeo/geotrellis/processgraph/ProcessGraphRunner.scala
+++ openeo-geotrellis/src/test/scala/org/openeo/geotrellis/processgraph/ProcessGraphRunner.scala
@@ -5,9 +5,7 @@ import org.slf4j.{Logger, LoggerFactory}
 import java.io.File
 import java.lang.management.ManagementFactory
 import java.net.ServerSocket
-import java.nio.file.{FileSystems, Files, Path, Paths}
 import scala.annotation.tailrec
-import scala.jdk.CollectionConverters.asScalaIteratorConverter
 import scala.sys.process._
 import scala.util.Using
 
@@ -19,7 +17,7 @@ object ProcessGraphRunner {
 
   def run(processGraph: File): Unit = {
 
-    val logger: Logger = LoggerFactory.getLogger("docker-issues")
+    val logger: Logger = LoggerFactory.getLogger(ProcessGraphRunner.getClass)
 
     val hostGraphFolder = processGraph.getParent
     val processGraphName = processGraph.getName
@@ -27,19 +25,11 @@ object ProcessGraphRunner {
     val currentDir = System.getProperty("user.dir")
     val outputDir = currentDir + "/target/processgraph/results/" + processGraphName.replaceFirst(".json", "")
 
-    logger.error(f"Output dir: ${outputDir}")
-
     new File(outputDir).mkdirs()
 
-    logger.error(f"Output dir: ${outputDir} (OK)")
+    logger.info(f"Output dir: ${outputDir}")
 
     val classPath = System.getProperty("java.class.path")
-    logger.error(f"full Classpath: $classPath")
-
-
-    classPath.split(":").foreach(
-      cpe => println(f"Check ${cpe}: ${new File(cpe).exists}")
-    )
 
     def findCommonPrefix(strings: Array[String]): String = {
       if (strings.length < 2) {
@@ -57,16 +47,14 @@ object ProcessGraphRunner {
       }
     }
 
-
     val jarParts = classPath.split(":").filter(_.endsWith(".jar")).groupBy(s => s.substring(0, s.indexOf("/", 1))).map(e => findCommonPrefix(e._2))
-
     val folderParts = classPath.split(":").filter(!_.endsWith(".jar")).groupBy(s => s.substring(0, s.indexOf("/", 1))).map(e => findCommonPrefix(e._2))
 
-    var modifiedClassPath = classPath.split(":")
 
     val jarMapping = jarParts.zipWithIndex.map { case (jarPart, i) => (jarPart, f"/jars${i}/") }
     val folderMapping = folderParts.zipWithIndex.map { case (folderPart, i) => (folderPart, f"/code${i}/") }
 
+    var modifiedClassPath = classPath.split(":")
     jarMapping.foreach {
       case (jarPart, replacement) =>
         modifiedClassPath = modifiedClassPath.map(mcpe => if (mcpe.endsWith(".jar") && mcpe.startsWith(jarPart)) {
@@ -87,27 +75,13 @@ object ProcessGraphRunner {
     }
     modifiedClassPath = modifiedClassPath.filter(f => !f.startsWith("/opt"))
 
-    Stream(jarMapping, folderMapping).flatten
-      .foreach(f => {
-        val file = new File(f._1)
-        val bool = file.canExecute()
-        println(bool)
-      })
-
     val classPathMappings = Stream(jarMapping, folderMapping).flatten
       .filter(f => !f._1.startsWith("/opt"))
       .map { case (a, b) => f"-v ${a}:${b}" }.mkString(" ")
 
     val dockerClassPath = modifiedClassPath.mkString(":")
-    logger.error(f"Docker classpath: $dockerClassPath")
 
-    logger.error("Checking if running in debug mode")
     val debug = ManagementFactory.getRuntimeMXBean().getInputArguments().stream().anyMatch(_.contains("-agentlib:jdwp"))
-    if (debug) {
-      logger.error("Running in debug mode")
-    } else {
-      logger.error("Not running in debug mode")
-    }
 
     val dockerImage = "vito-docker.artifactory.vgt.vito.be/geotrellis_process_graph_test_helper"
 
@@ -121,9 +95,9 @@ object ProcessGraphRunner {
       } else {
         f"docker run -v ${outputDir}:/out -v ${hostGraphFolder}:/graphs ${classPathMappings} ${dockerImage} /graphs/${processGraphName} /out ${dockerClassPath}"
       }
-    logger.error(f"Prepared command: $cmd")
+    logger.debug(f"Prepared command: $cmd")
     val output = cmd.!!
-    println(output)
+    logger.info(output)
   }
 
   @tailrec
