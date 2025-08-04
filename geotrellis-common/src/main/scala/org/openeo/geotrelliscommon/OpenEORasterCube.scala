8e689cd2a616cb7cf2b4114ca2d19764080e80b2
Emile Sonneveld <emile.sonneveld@vito.be>
2025-08-04 21:18:49 +0200

    Test TileLayerMetadataOpenEO.

--- geotrellis-common/src/main/scala/org/openeo/geotrelliscommon/OpenEORasterCube.scala
+++ geotrellis-common/src/main/scala/org/openeo/geotrelliscommon/OpenEORasterCube.scala
@@ -1,13 +1,43 @@
 package org.openeo.geotrelliscommon
 
-import geotrellis.layer.TileLayerMetadata
-import geotrellis.raster.MultibandTile
+import geotrellis.layer.{Bounds, LayoutDefinition, TileLayerMetadata}
+import geotrellis.proj4.CRS
+import geotrellis.raster.{CellType, MultibandTile}
 import geotrellis.spark.ContextRDD
+import geotrellis.vector.Extent
 import org.apache.spark.rdd._
 
+class TileLayerMetadataOpenEO[K](
+                                  override val cellType: CellType,
+                                  override val layout: LayoutDefinition,
+                                  override val extent: Extent,
+                                  override val crs: CRS,
+                                  override val bounds: Bounds[K],
+                                  bandsArg: Seq[String],
+                                ) extends TileLayerMetadata[K](cellType, layout, extent, crs, bounds) {
+  val bands: Seq[String] = bandsArg
+}
 
+object TileLayerMetadataOpenEO {
+  def apply[K](metadata: TileLayerMetadata[K], openEOMetadata: OpenEORasterCubeMetadata): TileLayerMetadataOpenEO[K] = {
+    metadata match {
+      case m: TileLayerMetadataOpenEO[K] =>
+        assert(m.bands == openEOMetadata.bands, "Bands in metadata should match.")
+        m
+      case _ =>
+        new TileLayerMetadataOpenEO(
+          metadata.cellType,
+          metadata.layout,
+          metadata.extent,
+          metadata.crs,
+          metadata.bounds,
+          openEOMetadata.bands
+        )
+    }
+  }
+}
 
-class OpenEORasterCube[K](rdd: RDD[(K, MultibandTile)], metadata: TileLayerMetadata[K],val openEOMetadata:OpenEORasterCubeMetadata) extends ContextRDD[K,MultibandTile,TileLayerMetadata[K]](rdd, metadata){
+class OpenEORasterCube[K](rdd: RDD[(K, MultibandTile)], metadata: TileLayerMetadata[K], val openEOMetadata: OpenEORasterCubeMetadata) extends ContextRDD[K, MultibandTile, TileLayerMetadata[K]](rdd, TileLayerMetadataOpenEO(metadata, openEOMetadata)) {
 
 
 }
