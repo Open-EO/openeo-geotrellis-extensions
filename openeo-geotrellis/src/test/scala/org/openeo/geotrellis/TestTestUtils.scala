9e7821a1591db20c00e4fee42875d60e98dcd7d7
Emile Sonneveld <emile.sonneveld@vito.be>
2025-08-04 20:05:24 +0200

    Clean up testBuildPlainSpatioTemporalDataCube

--- openeo-geotrellis/src/test/scala/org/openeo/geotrellis/TestTestUtils.scala
+++ openeo-geotrellis/src/test/scala/org/openeo/geotrellis/TestTestUtils.scala
@@ -6,22 +6,24 @@ import org.openeo.geotrellis.geotiff.{GTiffOptions, saveRDDTemporal}
 import org.openeo.geotrelliscommon.DataCubeParameters
 
 import java.nio.file.{Files, Paths}
+import scala.reflect.io.Directory
 
 object TestTestUtils extends LocalSparkContextJupyter {}
 
 class TestTestUtils {
   @Test def testBuildPlainSpatioTemporalDataCube(): Unit = {
     val path = "tmp/testBuildPlainSpatioTemporalDataCube/"
+    new Directory(Paths.get(path).toFile).deleteRecursively()
     Files.createDirectories(Paths.get(path))
 
     val pe = geotrellis.vector.ProjectedExtent(
-      geotrellis.vector.Extent(256000.0, 5376000.0, 768000.0, 5888000.0),
+      geotrellis.vector.Extent(256000.0, 5376000.0, 757000.0, 5377000.0),
       geotrellis.proj4.CRS.fromEpsgCode(32631)
     )
     val dcParams = new DataCubeParameters()
     dcParams.layoutScheme = "FloatingLayoutScheme"
 
-    val tileLayerRDD_10M = buildPlainSpatioTemporalDataCube(pe, 100, dcParams)
+    val tileLayerRDD_10M = buildPlainSpatioTemporalDataCube(pe, 10, dcParams)
     val partitions_10m = tileLayerRDD_10M.partitions
     println("10m partitions: " + partitions_10m.length)
 
