4f143374a7a1e1accd388697027966802616e03b
Jeroen Dries <jeroen.dries@vito.be>
2025-08-07 16:11:09 +0200

    move geocoding into a class https://github.com/cloudinsar/s1-workflows/issues/20

---
+++ snap-integration/src/main/scala/org/openeo/geotrellis/geocoding/GeoCodingProcess.scala
@@ -0,0 +1,97 @@
+package org.openeo.geotrellis.geocoding
+
+import geotrellis.layer.{LayoutDefinition, Metadata, SpaceTimeKey, TemporalProjectedExtent, TileLayerMetadata}
+import geotrellis.proj4.{CRS, LatLng, Transform}
+import geotrellis.raster.resample.NearestNeighbor
+import geotrellis.raster.{CellSize, DoubleArrayTile, FloatConstantNoDataCellType, MultibandTile, Raster, RasterExtent}
+import geotrellis.spark.tiling._
+import geotrellis.spark._
+import geotrellis.spark.{ContextRDD, MultibandTileLayerRDD}
+import geotrellis.vector.{Extent, ProjectedExtent}
+import org.apache.spark.rdd.RDD
+import org.esa.snap.core.dataio.geocoding.GeoCodingFactory
+import org.esa.snap.core.datamodel.{Band, GeoPos, PixelPos, ProductData}
+import org.openeo.geotrelliscommon.DatacubeSupport
+import org.slf4j.{Logger, LoggerFactory}
+
+object GeoCodingProcess {
+  private implicit val logger: Logger = LoggerFactory.getLogger(classOf[GeoCodingProcess])
+}
+
+class GeoCodingProcess extends Serializable {
+
+  def geocode(inputTile: MultibandTile, crs:CRS): Option[Raster[MultibandTile]] = {
+
+    val latitudes = inputTile.band(2).toArrayDouble()
+
+    val longitudes = inputTile.band(3).toArrayDouble()
+    val latBand: Band = new Band("latitude", ProductData.TYPE_FLOAT64, inputTile.cols, inputTile.rows)
+    latBand.setData(new ProductData.Double(latitudes))
+    val lonBand: Band = new Band("latitude", ProductData.TYPE_FLOAT64, inputTile.cols, inputTile.rows)
+    lonBand.setData(new ProductData.Double(longitudes))
+    //for automatic estimation of pixel size, we need to avoid NaN values in lat/lon bands
+    val geoCoder = GeoCodingFactory.createPixelGeoCoding(latBand,lonBand,0.05)
+
+
+    val pixelPos = new PixelPos()
+
+    val minLon = longitudes.min
+    val maxLon = longitudes.max
+    val minLat = latitudes.min
+    val maxLat = latitudes.max
+
+    if (minLat.isNaN || maxLat.isNaN || minLon.isNaN || maxLon.isNaN) {
+
+      return None
+    }
+
+    val reprojected = Extent(minLon, minLat, maxLon, maxLat).reproject(LatLng, crs) //.buffer(-10000.0) //.buffer(-20000.0,0.0)
+
+    val re = RasterExtent(reprojected, CellSize(20.0, 20.0))
+    val tile = DoubleArrayTile.ofDim(re.cols, re.rows)
+    val coordTransform = Transform(crs, LatLng)
+
+    val geoCoded = tile.mapDouble { (x, y, d) => {
+
+      val (xCoord, yCoord) = re.gridToMap(x, y)
+      val (lon, lat) = coordTransform(xCoord, yCoord)
+
+      val resultPos = geoCoder.getPixelPos(new GeoPos(lat, lon), pixelPos)
+      //println(resultPos)
+      if (resultPos.isValid) {
+        try {
+
+          inputTile.band(0).getDouble(resultPos.x.round.toInt, resultPos.y.round.toInt)
+        } catch {
+          case e: ArrayIndexOutOfBoundsException =>
+            GeoCodingProcess.logger.error(s"Error retrieving value for pixel position $resultPos: ${e.getMessage}")
+            Double.NaN
+        }
+      } else {
+        Double.NaN
+      }
+    }
+    }
+    Some(Raster(MultibandTile(geoCoded), re.extent))
+
+  }
+
+  def geoCode(cube: MultibandTileLayerRDD[SpaceTimeKey], targetExtent: Extent, targetCRS: CRS) = {
+    val rasters: RDD[(TemporalProjectedExtent, MultibandTile)] = cube.flatMap { case (key: SpaceTimeKey, tile: MultibandTile) => {
+
+      val raster = geocode(tile, targetCRS)
+      raster.map(r=>(TemporalProjectedExtent(r.extent, targetCRS, key.time), r.tile))
+    }
+    }
+    val targetLayout: LayoutDefinition = LayoutDefinition(RasterExtent(targetExtent, CellSize(20.0, 20.0)), 256, 256)
+    val origBounds = cube.metadata.bounds.get
+
+    val md = DatacubeSupport.tileLayerMetadata(targetLayout, ProjectedExtent(targetExtent, targetCRS), origBounds.minKey.time, origBounds.maxKey.time, FloatConstantNoDataCellType)
+
+    val tiled: RDD[(SpaceTimeKey, MultibandTile)] = rasters.tileToLayout(FloatConstantNoDataCellType, targetLayout, Tiler.Options(NearestNeighbor))
+    val tiledRDD: RDD[(SpaceTimeKey, MultibandTile)] with Metadata[TileLayerMetadata[SpaceTimeKey]] = ContextRDD(tiled.groupByKey().mapValues(_.reduce(_ merge (_))), md)
+    tiledRDD
+  }
+
+}
+
--- snap-integration/src/test/scala/org/openeo/geotrellis/geocoding/TestGeoCoding.scala
+++ snap-integration/src/test/scala/org/openeo/geotrellis/geocoding/TestGeoCoding.scala
@@ -7,17 +7,16 @@ import geotrellis.raster.{CellSize, DoubleArrayTile, FloatConstantNoDataCellType
 import geotrellis.raster.io.geotiff.GeoTiff
 import geotrellis.raster.resample.NearestNeighbor
 import geotrellis.spark.{ContextRDD, MultibandTileLayerRDD, withTilerMethods}
-import geotrellis.spark.tiling._
+
 import geotrellis.spark.util.SparkUtils
 import geotrellis.vector.{Extent, ProjectedExtent}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
-import org.esa.snap.core.dataio.geocoding.GeoRaster
-import org.esa.snap.core.dataio.geocoding.inverse.PixelQuadTreeInverse
-import org.esa.snap.core.datamodel.{GeoPos, PixelPos}
+
 import org.junit.jupiter.api.Test
 import org.openeo.geotrellis.geotiff.{saveRDD, saveRDDTemporal}
 import org.openeo.geotrelliscommon.DatacubeSupport
+
 import org.slf4j.{Logger, LoggerFactory}
 
 import java.time.{ZoneOffset, ZonedDateTime}
@@ -39,72 +38,6 @@ object TestGeoCoding{
     _sc.get
   }
 
-  private def geocode(inputTile: MultibandTile, crs:CRS): Option[Raster[MultibandTile]] = {
-    val geoCoder: PixelQuadTreeInverse = new PixelQuadTreeInverse.Plugin(false).create().asInstanceOf[PixelQuadTreeInverse]
-
-    val latitudes = inputTile.band(2).toArrayDouble()
-    val longitudes = inputTile.band(3).toArrayDouble()
-    val geoRaster = new GeoRaster(longitudes, latitudes, "lon", "lat", inputTile.cols, inputTile.rows, 0.1)
-    geoCoder.initialize(geoRaster, false, Array.empty[PixelPos])
-    val pixelPos = new PixelPos()
-
-    val minLon = longitudes.min
-    val maxLon = longitudes.max
-    val minLat = latitudes.min
-    val maxLat = latitudes.max
-
-    if(minLat.isNaN || maxLat.isNaN || minLon.isNaN || maxLon.isNaN) {
-
-      return None
-    }
-
-    val reprojected = Extent(minLon, minLat, maxLon, maxLat).reproject(LatLng, crs)//.buffer(-10000.0) //.buffer(-20000.0,0.0)
-
-    val re = RasterExtent(reprojected, CellSize(20.0, 20.0))
-    val tile = DoubleArrayTile.ofDim(re.cols, re.rows)
-    val coordTransform = Transform(crs, LatLng)
-
-    val geoCoded = tile.mapDouble { (x, y, d) => {
-
-      val (xCoord, yCoord) = re.gridToMap(x, y)
-      val (lon, lat) = coordTransform(xCoord, yCoord)
-
-      val resultPos = geoCoder.getPixelPos(new GeoPos(lat, lon), pixelPos)
-      //println(resultPos)
-      if (resultPos.isValid) {
-        try{
-
-          inputTile.band(0).getDouble(resultPos.x.round.toInt, resultPos.y.round.toInt)
-        } catch {
-          case e: ArrayIndexOutOfBoundsException =>
-            logger.error(s"Error retrieving value for pixel position $resultPos: ${e.getMessage}")
-            Double.NaN
-        }
-      } else {
-        Double.NaN
-      }
-    }
-    }
-    Some(Raster(MultibandTile(geoCoded),re.extent))
-
-  }
-
-  private def geoCode(cube: MultibandTileLayerRDD[SpaceTimeKey], targetExtent: Extent, targetCRS: CRS) = {
-    val rasters: RDD[(TemporalProjectedExtent, MultibandTile)] = cube.flatMap { case (key: SpaceTimeKey, tile: MultibandTile) => {
-
-      val raster = geocode(tile, targetCRS)
-      raster.map(r=>(TemporalProjectedExtent(r.extent, targetCRS, key.time), r.tile))
-    }
-    }
-    val targetLayout: LayoutDefinition = LayoutDefinition(RasterExtent(targetExtent, CellSize(20.0, 20.0)), 256, 256)
-    val origBounds = cube.metadata.bounds.get
-
-    val md = DatacubeSupport.tileLayerMetadata(targetLayout, ProjectedExtent(targetExtent, targetCRS), origBounds.minKey.time, origBounds.maxKey.time, FloatConstantNoDataCellType)
-
-    val tiled: RDD[(SpaceTimeKey, MultibandTile)] = rasters.tileToLayout(FloatConstantNoDataCellType, targetLayout, Tiler.Options(NearestNeighbor))
-    val tiledRDD: RDD[(SpaceTimeKey, MultibandTile)] with Metadata[TileLayerMetadata[SpaceTimeKey]] = ContextRDD(tiled.groupByKey().mapValues(_.reduce(_ merge (_))), md)
-    tiledRDD
-  }
 }
 
 class TestGeoCoding {
@@ -122,7 +55,7 @@ class TestGeoCoding {
     val masterTiff = GeoTiff.readMultiband(resource.toString.stripPrefix("file:")).raster
     val inputTile = masterTiff.tile
     val utm = CRS.fromEpsgCode(32631)
-    val raster = TestGeoCoding.geocode(inputTile,utm).get
+    val raster = new GeoCodingProcess().geocode(inputTile,utm).get
 
     GeoTiff(raster, utm).write("/tmp/geocoded.tif")
 
@@ -147,7 +80,7 @@ class TestGeoCoding {
     val cube: MultibandTileLayerRDD[SpaceTimeKey] = ContextRDD(tiledInput,inputMetadata)
     val targetExtent = Extent(1078161.262, 5197478.538, 1176612.520, 5228026.100)
     val targetCRS = CRS.fromEpsgCode(32631)
-    val tiledRDD: RDD[(SpaceTimeKey, MultibandTile)] with Metadata[TileLayerMetadata[SpaceTimeKey]] = TestGeoCoding.geoCode(cube, targetExtent, targetCRS)
+    val tiledRDD: RDD[(SpaceTimeKey, MultibandTile)] with Metadata[TileLayerMetadata[SpaceTimeKey]] = new GeoCodingProcess().geoCode(cube, targetExtent, targetCRS)
 
     saveRDDTemporal(tiledRDD, "/tmp/geocoded_cube.tif")
 
