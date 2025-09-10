95f7b283f1ff8b240595e9f58fb75ee61e0eb8b6
Jeroen Dries <jeroen.dries@vito.be>
2025-09-10 10:19:00 +0200

    geocoding: higher resolution to close gaps

--- snap-integration/src/main/scala/org/openeo/geotrellis/geocoding/GeoCodingProcess.scala
+++ snap-integration/src/main/scala/org/openeo/geotrellis/geocoding/GeoCodingProcess.scala
@@ -3,7 +3,6 @@ package org.openeo.geotrellis.geocoding
 import geotrellis.layer.{LayoutDefinition, Metadata, SpaceTimeKey, TemporalProjectedExtent, TileLayerMetadata}
 import geotrellis.proj4.{CRS, LatLng, Transform}
 import geotrellis.raster.buffer.BufferedTile
-import geotrellis.raster.io.geotiff.GeoTiff
 import geotrellis.raster.resample.NearestNeighbor
 import geotrellis.raster.{CellSize, DoubleArrayTile, FloatConstantNoDataCellType, MultibandTile, Raster, RasterExtent}
 import geotrellis.spark.tiling._
@@ -13,7 +12,10 @@ import geotrellis.vector.{Extent, ProjectedExtent}
 import org.apache.spark.rdd.RDD
 import org.esa.snap.core.dataio.geocoding.GeoRaster
 import org.esa.snap.core.dataio.geocoding.inverse.PixelQuadTreeInverse
+import org.esa.snap.core.dataio.geocoding.util.XYInterpolator
+import org.esa.snap.core.dataio.geocoding.util.XYInterpolator.SYSPROP_GEOCODING_INTERPOLATOR
 import org.esa.snap.core.datamodel.{GeoPos, PixelPos}
+import org.esa.snap.runtime.Config
 import org.openeo.geotrelliscommon.DatacubeSupport
 import org.slf4j.{Logger, LoggerFactory}
 
@@ -23,18 +25,21 @@ object GeoCodingProcess {
 
 class GeoCodingProcess extends Serializable {
 
-  def geocode(inputTile: MultibandTile, crs:CRS, resolution:CellSize = CellSize(20.0,20.0), lonIndex:Int =3, latIndex:Int = 2): Option[Raster[MultibandTile]] = {
 
-    val latitudes = inputTile.band(latIndex).toArrayDouble()
+
+  def geoCode(inputTile: MultibandTile, crs:CRS, resolution:CellSize = CellSize(20.0,20.0), lonIndex:Int =3, latIndex:Int = 2): Option[Raster[MultibandTile]] = {
+
+    val latitudes  = inputTile.band(latIndex).toArrayDouble()
     val longitudes = inputTile.band(lonIndex).toArrayDouble()
 
-    val geoCoder: PixelQuadTreeInverse = new PixelQuadTreeInverse.Plugin(false).create().asInstanceOf[PixelQuadTreeInverse]
-    val geoRaster = new GeoRaster(longitudes, latitudes, "lon", "lat", inputTile.cols, inputTile.rows, 0.05)
-    geoCoder.initialize(geoRaster, false, Array.empty[PixelPos])
-    //for automatic estimation of pixel size, we need to avoid NaN values in lat/lon bands
-    //the method below is cleaner and shorter, but forces dependency on older geotools
-    //val geoCoder = GeoCodingFactory.createPixelGeoCoding(latBand,lonBand,0.05)
 
+    Config.instance("snap").preferences.put(SYSPROP_GEOCODING_INTERPOLATOR, XYInterpolator.Type.GEODETIC.name)
+    //val estimatedRes = estimateGroundResolutionInKm(latitudes,longitudes,croppedInput.cols,croppedInput.rows)
+    val geoCoder = new PixelQuadTreeInverse.Plugin(true).create().asInstanceOf[PixelQuadTreeInverse]
+    // 0.15 is estimated distance between pixels in km, the expected value for Sentinel1 would be around 0.02 (20m)
+    // if we set it to such a lower value however, gaps appear in the output
+    val geoRaster = new GeoRaster(longitudes, latitudes, "lon", "lat", inputTile.cols, inputTile.rows, 0.15)
+    geoCoder.initialize(geoRaster, false, Array.empty[PixelPos])
 
     val pixelPos = new PixelPos()
 
@@ -59,14 +64,13 @@ class GeoCodingProcess extends Serializable {
       val (lon, lat) = coordTransform(xCoord, yCoord)
 
       val resultPos = geoCoder.getPixelPos(new GeoPos(lat, lon), pixelPos)
-      //println(resultPos)
       if (resultPos.isValid) {
         try {
 
           inputTile.band(0).getDouble(resultPos.x.round.toInt, resultPos.y.round.toInt)
         } catch {
           case e: ArrayIndexOutOfBoundsException =>
-            GeoCodingProcess.logger.error(s"resample_spatial - geocode: Error retrieving value for pixel position $resultPos: ${e.getMessage}")
+            GeoCodingProcess.logger.error(s"resample_spatial - geocode: Error retrieving value for pixel position  ($lon, $lat) - ($xCoord, $yCoord ) : ${e.getMessage}")
             Double.NaN
         }
       } else {
@@ -78,7 +82,7 @@ class GeoCodingProcess extends Serializable {
 
   }
 
-  def geoCode(cube: MultibandTileLayerRDD[SpaceTimeKey], targetExtent: Extent, targetCRS: CRS, targetResolution: CellSize) = {
+  def geoCode(cube: MultibandTileLayerRDD[SpaceTimeKey], targetExtent: Extent, targetCRS: CRS, targetResolution: CellSize): MultibandTileLayerRDD[SpaceTimeKey] = {
 
     val bandLabels = DatacubeSupport.maybeBandLabels(cube).getOrElse{throw new IllegalArgumentException("Band labels missing from input cube, cannot proceed with geocoding.")}
 
@@ -98,7 +102,7 @@ class GeoCodingProcess extends Serializable {
     val bufferedCube: RDD[(SpaceTimeKey, BufferedTile[MultibandTile])] = cube.bufferTiles(16)
     val rasters: RDD[(TemporalProjectedExtent, MultibandTile)] = bufferedCube.flatMap { case (key: SpaceTimeKey, tile: BufferedTile[MultibandTile]) => {
 
-      val raster = geocode(tile.tile, targetCRS,targetResolution, lonIndex,latIndex)
+      val raster = geoCode(tile.tile, targetCRS,targetResolution, lonIndex,latIndex)
       //if(raster.isDefined) {
       //  GeoTiff(raster.get, targetCRS).write(s"/tmp/geocoded_${key.time}_${key.spatialKey.col}_${key.spatialKey.row}.tif")
       //}
