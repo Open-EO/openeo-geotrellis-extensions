c2c97abb7785335d25aef69c7b5ea273ca708ccc
dsamaey <diether.samaey@vito.be>
2025-09-17 20:30:06 +0200

    chore: upgrade to scala 2.13 # https://github.com/Open-EO/openeo-geotrellis-kubernetes/issues/35

--- openeo-geotrellis/src/test/scala/org/openeo/geotrellis/processgraph/TestProcessGraphJson.scala
+++ openeo-geotrellis/src/test/scala/org/openeo/geotrellis/processgraph/TestProcessGraphJson.scala
@@ -3,25 +3,28 @@ package org.openeo.geotrellis.processgraph
 import org.junit.Test
 import org.junit.jupiter.api.Disabled
 
-@Disabled
 class TestProcessGraphJson {
 
   @Test
+  @Disabled
   def loadSyntheticData(): Unit = {
     ProcessGraphRunner.run("/org/openeo/geotrellis/processgraph/load_synthetic_data.json")
   }
 
   @Test
+  @Disabled
   def reduceT(): Unit = {
     ProcessGraphRunner.run("/org/openeo/geotrellis/processgraph/reduce_t.json")
   }
 
   @Test
+  @Disabled
   def reduceTApplyNeighbourhoodUdf(): Unit = {
     ProcessGraphRunner.run("/org/openeo/geotrellis/processgraph/reduce_t_apply_neighbourhood_udf.json")
   }
 
   @Test
+  @Disabled
   def applyNeighbourhoodUdfCheckDims(): Unit = {
     ProcessGraphRunner.run("/org/openeo/geotrellis/processgraph/apply_neighbourhood_udf_check_dims.json")
   }
