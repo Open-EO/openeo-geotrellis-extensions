ac6cf5ef07bcef1fbba6f96afdde002b1db02ddd
Jan Van den bosch <jan.vandenbosch@hotmail.com>
2025-10-03 09:56:53 +0200

    debug, support load_per_product (#547)
    * quickly build jar with "debug" logging #446
    
    * fixup! quickly build jar with "debug" logging #446
    
    * load_per_product: apply NODATA of 0 to integral cell types #446
    
    * load_per_product: add test and run #446
    
    * cleanup #446

--- openeo-geotrellis/src/test/scala/org/openeo/geotrellis/layers/FileLayerProviderTest.scala
+++ openeo-geotrellis/src/test/scala/org/openeo/geotrellis/layers/FileLayerProviderTest.scala
@@ -1723,8 +1723,9 @@ class FileLayerProviderTest extends RasterMatchers{
       f"$outDir/testMultibandCOGViaSTACResampledCubic.nc", referenceFile)
   }
 
-  @Test
-  def testMultibandNoNoDataCOGViaSTAC(@TempDir outDir: Path): Unit = {
+  @ParameterizedTest
+  @ValueSource(booleans = Array(false, true))
+  def testMultibandNoNoDataCOGViaSTAC(loadPerProduct: Boolean, @TempDir outDir: Path): Unit = {
     val pyramidFactory = LayerFixtures.stacCogNoNoDataCollection
 
     val projectedPolygons = ProjectedPolygons.fromExtent(
@@ -1735,6 +1736,7 @@ class FileLayerProviderTest extends RasterMatchers{
     val dataCubeParameters = new DataCubeParameters
     dataCubeParameters.layoutScheme = "FloatingLayoutScheme"
     dataCubeParameters.globalExtent = Some(projectedPolygons.extent)
+    dataCubeParameters.loadPerProduct = loadPerProduct
 
     writeToNetCDFAndCompare(
       projectedPolygons,
