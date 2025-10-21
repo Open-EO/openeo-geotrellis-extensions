3b536f312f0f1fb576d57d6e7b57755f71b4760f
dsamaey <diether.samaey@vito.be>
2025-10-21 12:10:00 +0200

    chore: fix test

--- geotrellis-sentinelhub/src/test/scala/org/openeo/geotrellissentinelhub/PyramidFactoryTest.scala
+++ geotrellis-sentinelhub/src/test/scala/org/openeo/geotrellissentinelhub/PyramidFactoryTest.scala
@@ -1168,7 +1168,7 @@ class PyramidFactoryTest {
     val datacubeParams = new DataCubeParameters()
     datacubeParams.tileSize = 256
     datacubeParams.layoutScheme = "FloatingLayoutScheme"
-    datacubeParams.partitionerIndexReduction = 7
+    datacubeParams.setPartitionerIndexReduction(7)
 
     // Should not throw any of the 2 following errors after #128 fix:
     // - "Cannot create a polygon with exterior with fewer than 4 points: LINEARRING EMPTY"
