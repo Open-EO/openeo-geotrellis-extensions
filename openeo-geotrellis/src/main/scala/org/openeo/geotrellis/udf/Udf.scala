f58dd4de58afe9a6af199d6974c12755771cb341
dsamaey <diether.samaey@vito.be>
2025-10-28 15:47:38 +0100

    chore: fix tests

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/udf/Udf.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/udf/Udf.scala
@@ -221,12 +221,21 @@ object Udf {
   }
 
   private def defaultCodeBlock() = {
-    val context = SparkContext.getOrCreate();
-    context.getConf
-    logger.error(context.toString)
-    val conf = SparkContext.getOrCreate().getConf
-    logger.error(conf.toDebugString)
-    val memoryLimitBytes = conf.getSizeAsBytes("spark.executor.pyspark.memory", -1)
+    val memoryLimitBytes =
+      try {
+        val context = SparkContext.getOrCreate();
+        val conf = SparkContext.getOrCreate().getConf
+        conf.getSizeAsBytes("spark.executor.pyspark.memory", -1)
+      } catch {
+        case t: Throwable => {
+          logger.error(f"Failed to get spark context (${t.getMessage}) to determine spark.executor.pyspark.memory")
+          -1
+        }
+        case e: Error => {
+          logger.error(s"Failed to get spark context (${e.getMessage}) to determine spark.executor.pyspark.memory")
+          -1
+        }
+      }
     if (memoryLimitBytes > 0) {
       logger.debug(s"Limiting JEP UDF memory to $memoryLimitBytes bytes.")
       val memoryLimitingCode = f"""
