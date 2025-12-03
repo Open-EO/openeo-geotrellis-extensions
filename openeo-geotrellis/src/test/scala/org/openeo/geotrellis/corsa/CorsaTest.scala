546df201bbf8f02dc1b7ae4f95b5fcd60dc96278
Jan Van den bosch <jan.vandenbosch@hotmail.com>
2025-12-03 15:33:53 +0100

    fill in some blanks #563

--- openeo-geotrellis/src/test/scala/org/openeo/geotrellis/corsa/CorsaTest.scala
+++ openeo-geotrellis/src/test/scala/org/openeo/geotrellis/corsa/CorsaTest.scala
@@ -1,62 +1,63 @@
 package org.openeo.geotrellis.corsa
 
-import ai.onnxruntime.{OnnxTensor, OrtEnvironment, OrtSession}
+import ai.onnxruntime.{OnnxJavaType, OnnxTensor, OrtEnvironment, OrtSession, OrtUtil, TensorInfo}
 import ai.onnxruntime.OrtSession.SessionOptions
 import geotrellis.proj4.CRS
-import geotrellis.raster.{FloatConstantNoDataCellType, GridBounds, MultibandTile, Raster, Tile, isData}
+import geotrellis.raster.{FloatArrayTile, FloatConstantNoDataCellType, GridBounds, MultibandTile, Raster, Tile, UShortArrayTile, isData}
 import geotrellis.raster.geotiff.GeoTiffRasterSource
-import geotrellis.raster.io.geotiff.SinglebandGeoTiff
+import geotrellis.raster.io.geotiff.{MultibandGeoTiff, SinglebandGeoTiff}
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 import java.nio.file.{Files, Path, Paths}
+import java.util
 import scala.jdk.CollectionConverters._
 import scala.jdk.StreamConverters._
 
+object CorsaTest {
+  private val TileSize = 120
+  private val Bands = Seq("B02", "B03", "B04", "B05", "B06", "B07", "B08", "B8A", "B11", "B12")
+}
+
 class CorsaTest {
+  import CorsaTest._
 
   @Test
   def test(): Unit = {
-    val bands = Seq("B02", "B03", "B04", "B05", "B06", "B07", "B08", "B8A", "B11", "B12")
     val modelDir = {
       // copied from /data/users/Public/luytsa/corsa-compression/pretrain_BEN_10-20mbands_bicubic_512-128_onnx
       Paths.get("/home/bossie/Documents/VITO/openeo-geotrellis-extensions/CORSA encode process #563/pretrain_BEN_10-20mbands_bicubic_512-128_onnx")
     }
     val modelPath = modelDir.resolve("encoder.onnx")
 
-    // TODO: set up ONNX dependencies (~ load_onnx_deps)
-
-    val (Raster(cubeArray, extent), crs) = sentinel2Tile(bands)
-    // MultibandGeoTiff(cubeArray, extent, crs).write("/tmp/sample.tif")
+    val (Raster(cubeArray, extent), crs) = sentinel2Tile
+    MultibandGeoTiff(cubeArray, extent, crs).write("/tmp/sample.tif")
 
     // TODO: replace NaNs with 0; in this case there are none
     cubeArray foreach { (_, value) => assertTrue(isData(value)) }
 
-    // TODO: normalize data
-    val cubeArrayNormalized = preprocessDataCube(cubeArray, bands)
+    val cubeArrayNormalized = preprocessDataCube(cubeArray)
 
     val (level0, level1) = processWindowOnnx(cubeArrayNormalized, modelPath)
 
-    // TODO: resample to 20m and 40m respectively
+    // already 20m and 40m resolution, see comment below
     SinglebandGeoTiff(level0, extent, crs).write("/tmp/level0_20m.tif")
     SinglebandGeoTiff(level1, extent, crs).write("/tmp/level1_40m.tif")
   }
 
-  private def sentinel2Tile(bands: Seq[String]): (Raster[MultibandTile], CRS) = {
+  private def sentinel2Tile: (Raster[MultibandTile], CRS) = {
     val files = Files.list(Paths.get("/data/MTDA/TERRASCOPE_Sentinel2/TOC_V2/2021/09/07/S2B_20210907T104619_31UFS_TOC_V210")).toScala(Seq)
 
     val bandRasterSources = for {
-      band <- bands
+      band <- Bands
       bandFile <- files.find(_.toString contains band)
     } yield GeoTiffRasterSource(bandFile.toString)
 
     val crs = bandRasterSources.head.crs
 
-    val tileSize: Long = 120
-
     val rasters = for {
       rs <- bandRasterSources
-      raster <- rs.read(GridBounds(0, 0, tileSize - 1, tileSize - 1))
+      raster <- rs.read(GridBounds(0L, 0L, TileSize - 1, TileSize - 1))
     } yield raster
 
     val extent = rasters.head.extent
@@ -65,35 +66,58 @@ class CorsaTest {
     (Raster(multibandTile, extent), crs)
   }
 
-  private def preprocessDataCube(cubeArray: MultibandTile, orderedBands: Seq[String]): MultibandTile = {
+  private def preprocessDataCube(cubeArray: MultibandTile): MultibandTile = {
     // TODO: scale; original does this by unpickling some objects from disk and applying them to the input
     cubeArray.convert(FloatConstantNoDataCellType)
-    // TODO: original adds a new axis in addition to bands/y/x; do it in processWindowOnnx instead?
+    // UDF adds a new dimension in addition to bands/y/x; this is done processWindowOnnx instead
   }
 
   private def processWindowOnnx(cubeArrayNormalized: MultibandTile, modelPath: Path): (Tile, Tile)  = {
-    // TODO: apply model encoder.onnx and return level0 and level1
-    assert(Files.exists(modelPath))
+    require(Files.exists(modelPath))
 
-    val env = OrtEnvironment.getEnvironment()
-    val tensor = OnnxTensor.createTensor(env, cubeArrayNormalized) // TODO: load cubeArrayNormalized to tensor
+    val env = OrtEnvironment.getEnvironment
     val (ortSession, ortInputName) = loadOrtSession(modelPath, env)
+    val tensor = OnnxTensor.createTensor(env, reshape(cubeArrayNormalized))
     val ortInputs = Map(ortInputName -> tensor).asJava
 
     val result = ortSession.run(ortInputs)
-    // TODO: get stuff from result
 
-    ???
+    val ortL0Ids = OrtUtil.reshape(result.get(2).getValue.asInstanceOf[Array[Array[Long]]].flatten, Array(1, 1, 60, 60)).asInstanceOf[Array[Array[Array[Array[Long]]]]]
+    val ortL1Ids = OrtUtil.reshape(result.get(3).getValue.asInstanceOf[Array[Array[Long]]].flatten, Array(1, 1, 30, 30)).asInstanceOf[Array[Array[Array[Array[Long]]]]]
+
+    // resample by means of "repeat" in the UDF's process_window_onnx() is undone by the resample_spatial in the UDP
+    val level0 = UShortArrayTile(ortL0Ids.flatten.flatten.flatten.map(_.toShort), cols = 60, rows = 60)//.resample(120, 120)
+    val level1 = UShortArrayTile(ortL1Ids.flatten.flatten.flatten.map(_.toShort), cols = 30, rows = 30)//.resample(120, 120)
+
+    (level0, level1)
   }
 
-  private def loadOrtSession(modelPath: Path, env: OrtEnvironment): (OrtSession, String) = {
-    // TODO: start ONNX runtime inference session from model
-    // TODO: set intra_op_num_threads to 1
+  private def reshape(cubeArrayNormalized: MultibandTile): Array[Array[Array[Array[Float]]]] = {
+    def unflatten(floats: Array[Float]): Array[Array[Float]] =
+      floats.sliding(size = TileSize, step = TileSize).toArray // 1D -> 2D
 
+    val yxBands = for {
+      bandTile <- cubeArrayNormalized.bands.toArray
+      xy = unflatten(bandTile.asInstanceOf[FloatArrayTile].array)
+      yx = xy.transpose
+    } yield yx
+
+    Array(yxBands) // some additional dimension
+  }
+
+  // TODO: eventually cache this
+  private def loadOrtSession(modelPath: Path, env: OrtEnvironment): (OrtSession, String) = {
     val options = new SessionOptions
     options.setIntraOpNumThreads(1)
     val session = env.createSession(modelPath.toString, options)
 
-    (session, session.getInputNames.iterator().next())
+    require(session.getNumInputs == 1)
+    val inputName = session.getInputNames.iterator().next()
+
+    val inputInfo: TensorInfo = session.getInputInfo.get(inputName).getInfo.asInstanceOf[TensorInfo]
+    require(inputInfo.`type` == OnnxJavaType.FLOAT)
+    require(util.Arrays.equals(inputInfo.getShape, Array(1L, Bands.size, TileSize, TileSize))) // [???, bands, y, x]
+
+    (session, inputName)
   }
 }
