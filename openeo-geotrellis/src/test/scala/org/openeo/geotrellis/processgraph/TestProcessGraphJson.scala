0435d4ffa2aa70087a88ca8b6dfac3dd26975deb
dsamaey <diether.samaey@vito.be>
2026-01-20 14:43:05 +0100

    fix: removed incorrect assertion

--- openeo-geotrellis/src/test/scala/org/openeo/geotrellis/processgraph/TestProcessGraphJson.scala
+++ openeo-geotrellis/src/test/scala/org/openeo/geotrellis/processgraph/TestProcessGraphJson.scala
@@ -15,6 +15,12 @@ class TestProcessGraphJson {
     ProcessGraphRunner.run("/org/openeo/geotrellis/processgraph/load_sentinel_5p_data.json")
   }
 
+  @Test
+  def issue(): Unit = {
+    ProcessGraphRunner.run("/org/openeo/geotrellis/processgraph/issue_.json")
+  }
+
+
 
   @Test
   @Disabled
