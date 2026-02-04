f9067030a03c7cc4242b18e8ca853f6ccebfb0af
Jan Van den bosch <jan.vandenbosch@hotmail.com>
2026-02-04 11:52:28 +0100

    corsaDecompress: retile to 60x60 automatically #630

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/OpenEOProcesses.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/OpenEOProcesses.scala
@@ -1175,16 +1175,22 @@ class OpenEOProcesses extends Serializable {
     }
 
   def corsaDecompressGeneric[K: SpatialComponent: ClassTag, M: Component[*, Bounds[K]]](datacube: MultibandTileLayerRDD[K]): MultibandTileLayerRDD[K] = {
-    val newTileLayout = datacube.metadata.tileLayout.copy(tileCols = 120, tileRows = 120)
-    val newBounds = datacube.metadata.bounds.flatMap { keyBounds =>
-      keyBounds.rekey(datacube.metadata.layout, datacube.metadata.layout.copy(tileLayout = newTileLayout))
+    val expectedTileSize = 60
+
+    val retiled =
+      if (datacube.metadata.tileCols == expectedTileSize && datacube.metadata.tileRows == expectedTileSize) datacube
+      else retileGeneric(datacube, sizeX = expectedTileSize, sizeY = expectedTileSize, overlapX = 0, overlapY = 0)
+
+    val newTileLayout = retiled.metadata.tileLayout.copy(tileCols = 120, tileRows = 120)
+    val newBounds = retiled.metadata.bounds.flatMap { keyBounds =>
+      keyBounds.rekey(retiled.metadata.layout, retiled.metadata.layout.copy(tileLayout = newTileLayout))
     }
 
     val modelDir = corsa.modelDir
 
     ContextRDD(
-      datacube.mapValues(tile => corsa.decompress(modelDir, tile)),
-      datacube.metadata.copy(layout = datacube.metadata.layout.copy(tileLayout = newTileLayout), bounds = newBounds)
+      retiled.mapValues(tile => corsa.decompress(modelDir, tile)),
+      retiled.metadata.copy(layout = retiled.metadata.layout.copy(tileLayout = newTileLayout), bounds = newBounds)
     )
   }
 
