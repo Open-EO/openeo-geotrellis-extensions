95ebd4da0862298bd9566c557e076b24b0e27d5a
elienVandermaesenVITO <elien.vandermaesen@vito.be>
2026-02-24 13:44:09 +0100

    remove histogram and use own calculations

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/netcdf/NetCDFRDDWriter.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/netcdf/NetCDFRDDWriter.scala
@@ -197,7 +197,7 @@ object NetCDFRDDWriter {
       }else{
         path
       }
-    val bandHistograms = collection.mutable.Map[String,(Histogram[Double],Int)]()
+    val bandStatistics = collection.mutable.Map[String,(Double,Double,Option[Double],Int,Int)]() // min, max, mean, valid data count, total data count
     var netcdfFile: NetcdfFileWriter = null
     for(tuple <- cachedRDD.toLocalIterator){
 
@@ -257,7 +257,7 @@ object NetCDFRDDWriter {
               tile = tile.crop(rasterExtent.cols-gridExtent.colMin,rasterExtent.rows-gridExtent.rowMin,raster.CropOptions(force=true))
               logger.debug(s"Cropping output tile to avoid going out of variable (${variable}) bounds ${gridExtent}.")
             }
-            if (addBandsStatistics) bandsStatistics(tile, bandHistograms, variable)
+            if (addBandsStatistics) bandsStatistics(tile, bandStatistics, variable)
             try{
               writeTile(variable, origin, tile, netcdfFile)
             }catch {
@@ -274,7 +274,7 @@ object NetCDFRDDWriter {
         netcdfFile.flush()
       }
     }
-    val assetsMetadata = setupAssetMetadata(rdd.metadata, dates, bandNames, preProcessResult._1, extent, addBandsStatistics, bandHistograms)
+    val assetsMetadata = setupAssetMetadata(rdd.metadata, dates, bandNames, preProcessResult._1, extent, addBandsStatistics, bandStatistics)
     if(dates.nonEmpty) {
       val timeDimName = if(dimensionNames!=null) dimensionNames.getOrDefault(TIME,TIME) else TIME
       writeTime(timeDimName, netcdfFile, dates)
@@ -887,16 +887,15 @@ object NetCDFRDDWriter {
     if (bandsMetadata.containsKey("OFFSET")) netcdfFile.addVariableAttribute(variableName,"add_offset",bandsMetadata.get("OFFSET").toFloat)
   }
 
-  private def setupAssetMetadata[K: SpatialComponent : Boundable : ClassTag](metadata: TileLayerMetadata[K], dates: List[Int], bandNames: ArrayList[String], gridBounds: GridBounds[Int], bbox: Extent, addBandsStats: Boolean,  histograms:scala.collection.mutable.Map[String,(Histogram[Double],Int)]): java.util.Map[String, Any] = {
+  private def setupAssetMetadata[K: SpatialComponent : Boundable : ClassTag](metadata: TileLayerMetadata[K], dates: List[Int], bandNames: ArrayList[String], gridBounds: GridBounds[Int], bbox: Extent, addBandsStats: Boolean,  bandStatistics:scala.collection.mutable.Map[String,(Double,Double,Option[Double],Int,Int)]): java.util.Map[String, Any] = {
     val assetMetadata = if (dates.nonEmpty) {
       new util.HashMap[String,Any](util.Map.of("time", new util.HashMap[String,Any](util.Map.of("type", "temporal", "extent",Array(dates.head, dates.last), "values", dates.toArray))))
     } else new java.util.HashMap[String,Any]()
     val bands = if (addBandsStats) {
       val maps = new util.ArrayList[util.Map[String,Any]]()
-      histograms.foreach {case (bandName,(histogram,size)) => {
-        val statistics = histogram.statistics()
-        val mapStatistics = statistics.fold(new util.HashMap[String, Any](util.Map.of("valid_percent", 0.0))){ statistics =>
-          new util.HashMap[String, Any](util.Map.of("maximum", statistics.zmax, "minimum", statistics.zmin, "mean", statistics.mean,"stddev",statistics.stddev, "valid_percent", statistics.dataCells.toDouble/size*100))
+      bandStatistics.foreach {case (bandName,(min,max,optionMean,validCount,size)) => {
+        val mapStatistics = optionMean.fold(new util.HashMap[String, Any](util.Map.of("valid_percent", 0.0))){ mean =>
+          new util.HashMap[String, Any](util.Map.of("maximum", max, "minimum", min, "mean", mean, "valid_percent", validCount.toDouble/size*100))
         }
         val band = new util.HashMap[String,Any](util.Map.of("name", bandName, "statistics", mapStatistics))
         maps.add(band)
@@ -942,33 +941,49 @@ object NetCDFRDDWriter {
     assetMetadata
   }
 
-  private def bandsStatistics(tile:Tile, bandHistograms:collection.mutable.Map[String,(Histogram[Double],Int)], bandName:String): Unit = {
-    logger.info(s"tile $bandName min&max = ${tile.findMinMaxDouble}")
-    val result = if (bandHistograms.contains(bandName)) {
-      val (histogram,size) = bandHistograms(bandName)
-      (histogram.merge(tile.histogramDouble()),size+tile.size)
-    } else (tile.histogramDouble(),tile.size)
-    bandHistograms.update(bandName,result)
+  private def bandsStatistics(tile:Tile, bandStatistics:collection.mutable.Map[String,(Double,Double,Option[Double],Int,Int)], bandName:String): Unit = {
+    val (tempMin,tempMax, tempMean, tempValidCount) = tile.cellType match {
+      case _:FloatCells => statsDouble(tile)
+      case _:DoubleCells => statsDouble(tile)
+      case _:ShortCells => statsInt(tile)
+      case _:UShortCells => statsInt(tile)
+      case _:IntCells => statsInt(tile)
+    }
+    val result = if (bandStatistics.contains(bandName)) {
+      val (curMin,curMax,curMean, curValidCount,size) = bandStatistics(bandName)
+      val newMean = if (tempValidCount+curValidCount > 0){
+        Some((tempMean.getOrElse(0.0)*tempValidCount + curMean.getOrElse(0.0)*curValidCount)/(tempValidCount+curValidCount))
+      } else None
+      (Math.min(tempMin,curMin), Math.max(tempMax,curMax),newMean,tempValidCount+curValidCount,size+tile.size)
+    } else (tempMin,tempMax,tempMean,tempValidCount,tile.size)
+    bandStatistics.update(bandName,result)
   }
 
   private def bandsStatistics(rasters:Seq[Raster[MultibandTile]], bandNames: ArrayList[String]): java.util.ArrayList[java.util.HashMap[String,Any]] = {
     val stats = new java.util.ArrayList[java.util.HashMap[String,Any]]()
     for (bandId <- 0 until bandNames.size()){
-      val minmax = rasters.foldLeft((Double.MaxValue,Double.MinValue)){case ((min,max),raster) =>
-        val (minTemp,maxTemp) = raster.tile.band(bandId).findMinMaxDouble
-        (Math.min(minTemp,min),Math.max(maxTemp,max))
-      }
-      logger.info(s"raster ${bandNames.get(bandId)} min&max = $minmax")
-      val histogramsAndSizes = rasters.map(raster => {
-        (raster.tile.band(bandId).histogramDouble(),raster.tile.size)
+      val bandStatistics = rasters.map(raster => {
+        val tile = raster.tile.band(bandId)
+        val (min, max, mean, validCount) = tile.cellType match {
+          case _: FloatCells => statsDouble(tile)
+          case _: DoubleCells => statsDouble(tile)
+          case _: ShortCells => statsInt(tile)
+          case _: UShortCells => statsInt(tile)
+          case _: IntCells => statsInt(tile)
+        }
+        (min, max, mean, validCount, raster.tile.size)
       })
-      val (result,size )= histogramsAndSizes.reduce{ (accHistogramSize, histogramSize) => {
-        (accHistogramSize._1.merge(histogramSize._1), accHistogramSize._2 + histogramSize._2)
+      val (min,max,maybeMean,validCount,size)= bandStatistics.reduce{(accumulated, temporary) => {
+        val (accMin, accMax, accMean, accValidCount, accSize) = accumulated
+        val (tempMin, tempMax, tempMean, tempValidCount, tempSize) = temporary
+        val newMean = if (accValidCount + tempValidCount > 0) {
+          Some((accMean.getOrElse(0.0) * accValidCount + tempMean.getOrElse(0.0) * tempValidCount) / (accValidCount + tempValidCount))
+        } else None
+        (Math.min(accMin, tempMin), Math.max(accMax, tempMax), newMean, accValidCount + tempValidCount, accSize + tempSize)
       }}
-      val statistics = result.statistics()
       val rasterBands = new java.util.HashMap[String,Any]()
-      val bandStats = statistics.fold(new java.util.HashMap[String,Any](java.util.Map.of("valid_percent", 0.0)))(statistics => {
-        new java.util.HashMap[String, Any](java.util.Map.of("mean", statistics.mean, "maximum", statistics.zmax, "minimum", statistics.zmin, "stddev", statistics.stddev, "valid_percent", statistics.dataCells.toDouble / size*100))
+      val bandStats = maybeMean.fold(new java.util.HashMap[String,Any](java.util.Map.of("valid_percent", 0.0)))(mean => {
+        new java.util.HashMap[String, Any](java.util.Map.of("mean", mean, "maximum", max, "minimum", min, "valid_percent", validCount.toDouble / size*100))
       })
       logger.info(s"computed statistics for band ${bandNames.get(bandId)}: $bandStats")
       rasterBands.put("statistics",bandStats)
@@ -976,6 +991,53 @@ object NetCDFRDDWriter {
       stats.add(rasterBands)
     }
     stats
+  }
+  private def statsDouble(tile: Tile): (Double,Double,Option[Double],Int) = {
+    var zmin = Double.NaN
+    var zmax = Double.NaN
+    var sum = 0.0
+    var validCount = 0
+    tile.foreachDouble { z =>
+      if (isData(z)) {
+        validCount+=1
+        if(isNoData(zmin)) {
+          zmin = z
+          zmax = z
+        } else {
+          zmin = math.min(zmin, z)
+          zmax = math.max(zmax, z)
+          sum += z
+        }
+      }
+    }
+    val mean:Option[Double] = if(validCount == 0) {
+      None
+    }else{
+      Some(sum/validCount)
+    }
+    (zmin,zmax,mean,validCount)
+  }
+  private def statsInt(tile:Tile): (Double,Double,Option[Double],Int) = {
+    var zmin = Int.MaxValue
+    var zmax = Int.MinValue
+    var sum = 0
+    var validCount = 0
+
+    tile.foreach { z =>
+      if (isData(z)) {
+        validCount +=1
+        zmin = math.min(zmin, z)
+        zmax = math.max(zmax, z)
+        sum += z
+      }
+    }
+
+    val mean:Option[Double] = if(validCount == 0) {
+      None
+    }else{
+      Some(sum.toDouble/validCount)
+    }
+    (zmin,zmax,mean,validCount)
   }
 
   private def getNoDataValue(cellType: CellType): (DataType,Option[Number]) = {
--- openeo-geotrellis/src/test/scala/org/openeo/geotrellis/netcdf/NetCDFRDDWriterTest.scala
+++ openeo-geotrellis/src/test/scala/org/openeo/geotrellis/netcdf/NetCDFRDDWriterTest.scala
@@ -204,14 +204,14 @@ class NetCDFRDDWriterTest extends RasterMatchers {
     )
     val arrayDim = 512
     val arrayTile0 = IntArrayTile(Array.fill(arrayDim * arrayDim / 4)(0) ++ Array.fill(arrayDim * arrayDim / 2)(30) ++ Array.fill(arrayDim * arrayDim / 4)(256), arrayDim, arrayDim, noDataValue = 256)
-    testStatistics(arrayTile = arrayTile0, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 75, "minimum", 0.0, "maximum", 30.0, "mean", 20.0, "stddev", 14.142135623730951)))
+    testStatistics(arrayTile = arrayTile0, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 75, "minimum", 0.0, "maximum", 30.0, "mean", 20.0))) // , "stddev", 14.142135623730951
     val arrayTile1 = IntArrayTile(Array.fill(arrayDim * arrayDim)(256), arrayDim, arrayDim, noDataValue = 256)
     val imageTile1 = arrayTile1.convert(DoubleUserDefinedNoDataCellType(256)).mutable
     testStatistics(arrayTile = imageTile1, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 0.0)))
     val arrayTile2 = IntArrayTile(Array.fill(arrayDim * arrayDim / 2)(256) ++ Array.fill(arrayDim * arrayDim / 8)(30) ++ Array.fill(arrayDim * arrayDim / 8)(10) ++ Array.fill(arrayDim * arrayDim / 4)(256), arrayDim, arrayDim, noDataValue = 256)
-    testStatistics(arrayTile = arrayTile2, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 25, "minimum", 10.0, "maximum", 30.0, "mean", 20.0, "stddev", 10)))
+    testStatistics(arrayTile = arrayTile2, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 25, "minimum", 10.0, "maximum", 30.0, "mean", 20.0))) // , "stddev", 10
     testStatistics(arrayTile = arrayTile0, addStatistics = false)
-    testStatistics(arrayTile = arrayTile0, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 100, "minimum", 0.0, "maximum", 30.0, "mean", 15.0, "stddev", 15.0)), polygon = polygon1, expectedShape = Array(86, 52))
+    testStatistics(arrayTile = arrayTile0, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 100, "minimum", 0.0, "maximum", 30.0, "mean", 15.0)), polygon = polygon1, expectedShape = Array(86, 52)) // , "stddev", 15.0
     testStatistics(arrayTile = arrayTile2, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 0.0)), polygon = polygon1, expectedShape = Array(86, 52))
   }
 
@@ -401,17 +401,17 @@ class NetCDFRDDWriterTest extends RasterMatchers {
     val arrayDim = 512
     val arrayTile0 = IntArrayTile(Array.fill(arrayDim * arrayDim / 4)(0) ++ Array.fill(arrayDim * arrayDim / 2)(30) ++ Array.fill(arrayDim * arrayDim / 4)(256), arrayDim, arrayDim)
     val imageTile0 = arrayTile0.convert(UShortUserDefinedNoDataCellType(256)).mutable
-    testStatistics(imageTile = imageTile0, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 75, "minimum", 0.0, "maximum", 30.0, "mean", 20.0, "stddev", 14.142135623730951)))
+    testStatistics(imageTile = imageTile0, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 75, "minimum", 0.0, "maximum", 30.0, "mean", 20.0))) // , "stddev", 14.142135623730951
     val arrayTile1 = IntArrayTile(Array.fill(arrayDim * arrayDim)(256), arrayDim, arrayDim)
     val imageTile1 = arrayTile1.convert(DoubleUserDefinedNoDataCellType(256)).mutable
     testStatistics(imageTile = imageTile1, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 0.0)))
     val arrayTile2 = IntArrayTile(Array.fill(arrayDim * arrayDim / 2)(256) ++ Array.fill(arrayDim * arrayDim / 8)(30) ++ Array.fill(arrayDim * arrayDim / 8)(10) ++ Array.fill(arrayDim * arrayDim / 4)(256), arrayDim, arrayDim)
     val imageTile2 = arrayTile2.convert(UShortUserDefinedNoDataCellType(256)).mutable
-    testStatistics(imageTile = imageTile2, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 25, "minimum", 10.0, "maximum", 30.0, "mean", 20.0, "stddev", 10)))
+    testStatistics(imageTile = imageTile2, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 25, "minimum", 10.0, "maximum", 30.0, "mean", 20.0))) // , "stddev", 10
     val imageTile3 = arrayTile2.convert(UShortCellType).mutable
-    testStatistics(imageTile = imageTile3, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 100, "minimum", 10, "maximum", 256, "mean", 197.0, "stddev", 102.3132444994293)))
+    testStatistics(imageTile = imageTile3, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 100, "minimum", 10, "maximum", 256, "mean", 197.0))) // , "stddev", 102.3132444994293
     testStatistics(imageTile = imageTile0, addStatistics = false)
-    testStatistics(imageTile = imageTile0, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 100, "minimum", 0.0, "maximum", 30.0, "mean", 15.0, "stddev", 15.0)), polygon = polygon1, expectedShape = Array(86, 52))
+    testStatistics(imageTile = imageTile0, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 100, "minimum", 0.0, "maximum", 30.0, "mean", 15.0)), polygon = polygon1, expectedShape = Array(86, 52)) // , "stddev", 15.0
     val imageTile4 = arrayTile2.convert(UShortUserDefinedNoDataCellType(256)).mutable
     testStatistics(imageTile = imageTile4, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 0.0)), polygon = polygon1, expectedShape = Array(86, 52))
   }
@@ -623,19 +623,19 @@ class NetCDFRDDWriterTest extends RasterMatchers {
     val arrayDim = 512
     val arrayTile0 = IntArrayTile(Array.fill(arrayDim * arrayDim / 4)(0) ++ Array.fill(arrayDim * arrayDim / 2)(30) ++ Array.fill(arrayDim * arrayDim / 4)(256), arrayDim, arrayDim)
     val imageTile0 = arrayTile0.convert(UShortUserDefinedNoDataCellType(256)).mutable
-    testStatistics(imageTile = imageTile0, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 75, "minimum", 0.0, "maximum", 30.0, "mean", 20.0, "stddev", 14.142135623730951)))
+    testStatistics(imageTile = imageTile0, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 75, "minimum", 0.0, "maximum", 30.0, "mean", 20.0))) // , "stddev", 14.142135623730951
     val arrayTile1 = IntArrayTile(Array.fill(arrayDim * arrayDim)(256), arrayDim, arrayDim)
     val imageTile1 = arrayTile1.convert(DoubleUserDefinedNoDataCellType(256)).mutable
     testStatistics(imageTile = imageTile1, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 0.0)))
     val arrayTile2 = IntArrayTile(Array.fill(arrayDim * arrayDim / 2)(256) ++ Array.fill(arrayDim * arrayDim / 8)(30) ++ Array.fill(arrayDim * arrayDim / 8)(10) ++ Array.fill(arrayDim * arrayDim / 4)(256), arrayDim, arrayDim)
     val imageTile2 = arrayTile2.convert(UShortUserDefinedNoDataCellType(256)).mutable
-    testStatistics(imageTile = imageTile2, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 25, "minimum", 10.0, "maximum", 30.0, "mean", 20.0, "stddev", 10)))
+    testStatistics(imageTile = imageTile2, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 25, "minimum", 10.0, "maximum", 30.0, "mean", 20.0))) // , "stddev", 10
     val arrayTile3 = IntArrayTile(Array.fill(arrayDim * arrayDim / 2)(256) ++ Array.fill(arrayDim * arrayDim / 8)(30) ++ Array.fill(arrayDim * arrayDim / 8)(10) ++ Array.fill(arrayDim * arrayDim / 4)(256), arrayDim, arrayDim)
-    testStatistics(imageTile = arrayTile3, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 100, "minimum", 10, "maximum", 256, "mean", 197.0 , "stddev", 102.3132444994293)))
+    testStatistics(imageTile = arrayTile3, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 100, "minimum", 10, "maximum", 256, "mean", 197.0))) //  , "stddev", 102.3132444994293
     val imageTile3 = arrayTile3.convert(UShortCellType).mutable
-    testStatistics(imageTile = imageTile3, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 100, "minimum", 10, "maximum", 256, "mean", 197.0 , "stddev", 102.3132444994293)))
+    testStatistics(imageTile = imageTile3, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 100, "minimum", 10, "maximum", 256, "mean", 197.0))) //  , "stddev", 102.3132444994293
     testStatistics(imageTile = imageTile0, addStatistics = false)
-    testStatistics(imageTile = imageTile0, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 100, "minimum", 0.0, "maximum", 30.0, "mean", 15.0 , "stddev", 15.0)), cropBounds = Some(Extent(-18.0, 30.0, 18.0, 60)), expectedShape = Array(86, 52))
+    testStatistics(imageTile = imageTile0, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 100, "minimum", 0.0, "maximum", 30.0, "mean", 15.0)), cropBounds = Some(Extent(-18.0, 30.0, 18.0, 60)), expectedShape = Array(86, 52)) // , "stddev", 15.0
     val imageTile4 = arrayTile2.convert(UShortUserDefinedNoDataCellType(256)).mutable
     testStatistics(imageTile = imageTile4, expectedStatistics = new util.HashMap[String, Any](util.Map.of("valid_percent", 0.0)), cropBounds = Some(Extent(-18.0, 30.0, 18.0, 60)), expectedShape = Array(86, 52))
   }
