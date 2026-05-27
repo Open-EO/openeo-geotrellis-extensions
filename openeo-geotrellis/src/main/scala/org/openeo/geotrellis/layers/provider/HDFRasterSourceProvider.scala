26c30802fe221cb40c83a9de608d648b00ed34c5
elienVandermaesenVITO <elien.vandermaesen@vito.be>
2026-05-27 15:32:14 +0200

    issue #716 add cellType if not present

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/layers/provider/HDFRasterSourceProvider.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/layers/provider/HDFRasterSourceProvider.scala
@@ -1,6 +1,6 @@
 package org.openeo.geotrellis.layers.provider
 
-import geotrellis.raster.RasterSource
+import geotrellis.raster.{CellType, ConvertTargetCellType, RasterSource}
 import geotrellis.raster.gdal.{GDALPath, GDALRasterSource, GDALWarpOptions}
 import org.slf4j.{Logger, LoggerFactory}
 
@@ -16,7 +16,12 @@ class HDFRasterSourceProvider extends RasterSourceProvider {
 
   override def rasterSource(definition: RasterSourceDefinition): RasterSource = {
     val dataPath = definition.dataPath.replace("/vsis3/EODATA/", "/vsis3/eodata/").replace("https", "/vsicurl/https")
+    val cellType = if (definition.targetCellType.nonEmpty){
+      definition.targetCellType
+    } else {
+      Some(ConvertTargetCellType(CellType.fromName("float64")))
+    }
     val warpOptions = GDALWarpOptions(cellSize = Some(definition.theResolution), targetCRS=Some(definition.targetExtent.crs), resampleMethod = Some(definition.resampleMethod),te = Some(definition.targetExtent.extent))
-    GDALRasterSource(GDALPath(dataPath),options = warpOptions, targetCellType = definition.targetCellType)
+    GDALRasterSource(GDALPath(dataPath),options = warpOptions, targetCellType = cellType)
   }
 }
