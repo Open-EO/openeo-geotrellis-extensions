be1cff40d7b62a152ed09624571f7ac0974f4043
dsamaey <diether.samaey@vito.be>
2026-06-02 18:41:15 +0200

    fix: consider nominalDate when merging features

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/layers/OpenSearchClientMerger.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/layers/OpenSearchClientMerger.scala
@@ -16,26 +16,27 @@ object OpenSearchClientMerger {
         ) {
           if (features.map(_.crs).distinct.size > 1) {
             logger.warn(s"Multiple features with different CRS found in OpenSearch client, cannot merge into single feature client")
-            client
+            return client
           }
           if (features.map(_.resolution).distinct.size > 1) {
             logger.warn(s"Multiple features with different resolution found in OpenSearch client, cannot merge into single feature client")
-            client
+            return client
           }
           if (features.map(_.bbox).distinct.size > 1) {
             logger.warn(s"Multiple features with different bbox found in OpenSearch client, cannot merge into single feature client")
-            client
+            return client
           }
           if (features.map(_.geometry).distinct.size > 1) {
             logger.warn(s"Multiple features with different geometry found in OpenSearch client, cannot merge into single feature client")
-            client
+            return client
           }
           logger.warn(s"Multiple features found in OpenSearch client, merging into single feature client with combined links")
           val singleFeatureClient = new FixedFeaturesOpenSearchClient()
-          val f1 = features.head
-          val links: Array[OpenSearchResponses.Link] = features.flatMap(_.links).toArray
-          val feature = OpenSearchResponses.Feature("merged", f1.bbox, f1.nominalDate, links, f1.resolution, f1.tileID, f1.geometry, f1.crs, f1.generalProperties, f1.rasterExtent, f1.deduplicationOrderValue, f1.cloudCover, f1.selfUrl)
-          singleFeatureClient.addFeature(feature)
+          features.groupBy(_.nominalDate).map(dateFeature => {
+            val f1 = dateFeature._2.head
+            val links: Array[OpenSearchResponses.Link] = dateFeature._2.flatMap(_.links).toArray
+            OpenSearchResponses.Feature("merged", f1.bbox, f1.nominalDate, links, f1.resolution, f1.tileID, f1.geometry, f1.crs, f1.generalProperties, f1.rasterExtent, f1.deduplicationOrderValue, f1.cloudCover, f1.selfUrl)
+          }).foreach(f => singleFeatureClient.addFeature(f))
           singleFeatureClient
         } else {
           client
