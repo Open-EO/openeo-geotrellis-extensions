7db63c26ba56294d4be364e2cac2c0a5bb46b3a4
Jan Van den bosch <jan.vandenbosch@hotmail.com>
2026-06-12 06:33:37 +0200

    "log_level": "debug" drops executor logs #678

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/geotiff/package.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/geotiff/package.scala
@@ -1513,7 +1513,7 @@ package object geotiff {
     val outputBufferString = outputBuffer.toString().trim
     val cerrBufferString = cerrBuffer.toString().trim
 
-    logger.debug(s"${args mkString " "} returned exit code $exitCode; stdout was: $outputBufferString; stderr was $cerrBufferString")
+    logger.info(s"${args mkString " "} returned exit code $exitCode; stdout was: $outputBufferString; stderr was $cerrBufferString")
 
     if (cerrBufferString.nonEmpty) {
       logger.info(s"gdalinfo warnings: $cerrBufferString") // Mostly harmless messages
