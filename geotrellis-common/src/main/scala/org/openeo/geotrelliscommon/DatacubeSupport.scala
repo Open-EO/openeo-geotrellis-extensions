640acb5667cef023725bf83a746ff7ea0438623d
elienVandermaesenVITO <elien.vandermaesen@vito.be>
2026-06-23 13:27:01 +0200

    OOM aggregate spatial make it the temporal influence smaller

--- geotrellis-common/src/main/scala/org/openeo/geotrelliscommon/DatacubeSupport.scala
+++ geotrellis-common/src/main/scala/org/openeo/geotrelliscommon/DatacubeSupport.scala
@@ -332,10 +332,10 @@ object DatacubeSupport {
   }
 
   def optimalReductionForSparseKeys(sparseKeys: Seq[SpaceTimeKey], maxPartitionSizeInMb: Int, tileSize: Int, cellTypeBits: Int, bandCount: Int) = {
-    val temporalSteps = sparseKeys.map(_.time).distinct.length
-    val tileSizeInMb: Double = (bandCount * tileSize * cellTypeBits * temporalSteps).toDouble / (8 * 1024 * 1024)
+    val temporalWeight = math.max(sparseKeys.map(_.time).distinct.length.toDouble/20, 1)
+    val tileSizeInMb: Double = (bandCount * tileSize * cellTypeBits * temporalWeight).toDouble / (8 * 1024 * 1024)
     val maxRecordsPerPartition: Double = math.min(math.min(maxPartitionSizeInMb / tileSizeInMb, 1024),sparseKeys.length)
-    logger.info(s"Computing optimal reduction for ${sparseKeys.length} sparse keys and $temporalSteps, maxPartitionSizeInMb $maxPartitionSizeInMb, tilSizeInMb $tileSizeInMb, maxRecordsPerPartition $maxRecordsPerPartition .")
+    logger.info(s"Computing optimal reduction for ${sparseKeys.length} sparse keys and $temporalWeight, maxPartitionSizeInMb $maxPartitionSizeInMb, tilSizeInMb $tileSizeInMb, maxRecordsPerPartition $maxRecordsPerPartition .")
     var indexReduction = math.max(math.ceil(math.log(maxRecordsPerPartition) / math.log(2)).toInt - 1, 1)
 
     def computeIndices(cartesian: Seq[SpaceTimeKey], indexReduction: Int): (Array[BigInt], Int) = {
