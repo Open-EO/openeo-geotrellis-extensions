c1a6bf0d129878e2142983b28c15ca33d2b3d4b5
elienVandermaesenVITO <elien.vandermaesen@vito.be>
2026-07-24 11:12:51 +0200

    issue #658 remove adding nodata to nonodata celltypes

--- openeo-geotrellis/src/main/scala/org/openeo/geotrellis/layers/FileLayerProvider.scala
+++ openeo-geotrellis/src/main/scala/org/openeo/geotrellis/layers/FileLayerProvider.scala
@@ -755,11 +755,7 @@ class FileLayerProvider private(openSearch: OpenSearchClient, openSearchCollecti
       })
 
       logger.debug(s"Determined common cell type of rasterSources is $commonCellType, from cell types: ${cellTypes.mkString(", ")}.")
-      commonCellType match {
-        case integralNoNoData: NoNoData if !integralNoNoData.isFloatingPoint => commonCellType.withNoData(Some(0))
-        case _: NoNoData => commonCellType.withDefaultNoData()
-        case _ => commonCellType
-      }
+      commonCellType
     } catch {
       case e: Exception => {
         // Geotrellis GDALException errors are not descriptive enough. Attempt to add some more useful information.
